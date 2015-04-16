ad158eff0c6968acb98293f269ffb949bc60b6d4
Jeff Sittler <mindblender@gmail.com>
2015-04-16 00:39:55 -0700

    changed css path

--- themes/default/header.php
+++ themes/default/header.php
@@ -22,7 +22,7 @@ var gFlashOK = true;
 </script>
 <script type='text/javascript' src='../javascript/general.js'></script>
 <script type='text/JavaScript' src='../javascript/overlib_mini.js'></script>
-<link rel='stylesheet' href='../themes/".THEME."/stylesheet.css' type='text/css' />";
+<link rel='stylesheet' href='../themes/".THEME."/css/stylesheet.css' type='text/css' />";
 if ($includeCalendar && $includeCalendar == true) {
 	include("../includes/calendar.php");
 }
