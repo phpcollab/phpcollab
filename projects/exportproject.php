cb41474427dc48a997c517e4d7baaeca500125f8
Jeff Sittler <mindblender@gmail.com>
2015-08-12 01:34:09 -0700

    Cleaned up require statement

--- projects/exportproject.php
+++ projects/exportproject.php
@@ -8,7 +8,7 @@ $export = "true";
 $checkSession = "false";
 include_once '../includes/library.php';
 
-require("../includes/phpmyadmin/defines.lib.php");
+require '../includes/phpmyadmin/defines.lib.php';
     function which_crlf()
     {
         $the_crlf = "\n";
