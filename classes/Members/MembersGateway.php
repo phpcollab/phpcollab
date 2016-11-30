b68b9968536fde10e94895453f626f6bfe1a6e16
Jeff Sittler <mindblender@gmail.com>
2016-11-29 20:28:18 -0700

    Adding getMemberByLogin method

--- classes/Members/MembersGateway.php
+++ classes/Members/MembersGateway.php
@@ -20,10 +20,31 @@ class MembersGateway
         $this->initrequest = $GLOBALS['initrequest'];
     }
 
+    public function getMemberByLogin($loginData)
+    {
+        if ($loginData['demo'] != true) {
+            if ($loginData['ssl']) {
+                $whereStatement = "WHERE mem.email_work = :ssl_email AND mem.login != 'demo' AND mem.profil != 4";
+            } else {
+                $whereStatement = "WHERE mem.login = :member_login AND mem.login != 'demo' AND mem.profil != 4";
+            }
+        } else {
+            $whereStatement = "WHERE mem.login = :member_login AND mem.profil != 4";
+        }
+
+        $this->db->query($this->initrequest["members"] . ' ' . $whereStatement);
+
+        $this->db->bind(':member_login', $loginData['login']);
+
+        if ($loginData['ssl']) {
+            $this->db->bind(':ssl_email', $loginData['ssl_email']);
+        }
+
+        return $this->db->single();
+    }
+
     public function getAllMembers()
     {
-//        $sorting = 'name';
-
         $this->db->query($this->initrequest["members"]);
 
         return $this->db->resultset();
