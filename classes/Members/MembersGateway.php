aaa8bf4b4a71bc6f28d8a04225361536eb81c01f
Jeff Sittler <mindblender@gmail.com>
2016-12-14 23:53:34 -0700

    changed the getMemberByLogin to accept an array or a string.

--- classes/Members/MembersGateway.php
+++ classes/Members/MembersGateway.php
@@ -22,23 +22,41 @@ class MembersGateway
 
     public function getMemberByLogin($loginData)
     {
-        if ($loginData['demo'] != true) {
-            if ($loginData['ssl']) {
-                $whereStatement = "WHERE mem.email_work = :ssl_email AND mem.login != 'demo' AND mem.profil != 4";
+        if (is_array($loginData)) {
+            if ($loginData['demo'] != true) {
+                if ($loginData['ssl']) {
+                    $whereStatement = "WHERE mem.email_work = :ssl_email AND mem.login != 'demo' AND mem.profil != 4";
+                } else {
+                    $whereStatement = "WHERE mem.login = :member_login AND mem.login != 'demo' AND mem.profil != 4";
+                }
             } else {
-                $whereStatement = "WHERE mem.login = :member_login AND mem.login != 'demo' AND mem.profil != 4";
+                $whereStatement = "WHERE mem.login = :member_login AND mem.profil != 4";
+            }
+
+            $this->db->query($this->initrequest["members"] . ' ' . $whereStatement);
+
+            $this->db->bind(':member_login', $loginData['login']);
+
+            if ($loginData['ssl']) {
+                $this->db->bind(':ssl_email', $loginData['ssl_email']);
             }
         } else {
             $whereStatement = "WHERE mem.login = :member_login AND mem.profil != 4";
+            $this->db->query($this->initrequest["members"] . ' ' . $whereStatement);
+            $this->db->bind(':member_login', $loginData);
         }
+
+
+        return $this->db->single();
+    }
+
+    public function getMemberById($memberId)
+    {
+        $whereStatement = "WHERE mem.id = :member_id";
 
         $this->db->query($this->initrequest["members"] . ' ' . $whereStatement);
 
-        $this->db->bind(':member_login', $loginData['login']);
-
-        if ($loginData['ssl']) {
-            $this->db->bind(':ssl_email', $loginData['ssl_email']);
-        }
+        $this->db->bind(':member_id', $memberId);
 
         return $this->db->single();
     }
