ce3dd9d12f7f494aa3aca788109bcd63f6286df7
Jeff Sittler <mindblender@gmail.com>
2016-12-14 19:47:15 -0700

    Adding Organizations and OrganizationsGateway classes

---
+++ classes/Organizations/Organizations.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace phpCollab\Organizations;
+
+use phpCollab\Database;
+
+class Organizations
+{
+    protected $organizations_gateway;
+    protected $db;
+
+    /**
+     * Organizations constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->organizations_gateway = new OrganizationsGateway($this->db);
+    }
+
+    public function checkIfClientExistsByName($clientName)
+    {
+        $clientName = filter_var($clientName, FILTER_SANITIZE_STRING);
+
+        $data = $this->organizations_gateway->getClientByName($clientName);
+
+        return $data;
+
+    }
+
+}
---
+++ classes/Organizations/OrganizationsGateway.php
@@ -0,0 +1,32 @@
+<?php
+namespace phpCollab\Organizations;
+
+use phpCollab\Database;
+
+class OrganizationsGateway
+{
+    protected $db;
+    protected $initrequest;
+
+    /**
+     * OrganizationsGateway constructor.
+     * @param Database $db
+     */
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+        $this->initrequest = $GLOBALS['initrequest'];
+    }
+
+    public function getClientByName($clientName)
+    {
+        $whereStatement = ' WHERE org.name = :client_name';
+
+        $this->db->query($this->initrequest["organizations"] . $whereStatement);
+
+        $this->db->bind(':client_name', $clientName);
+
+        return $this->db->single();
+    }
+
+}
