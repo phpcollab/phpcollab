16ed096b83bc763be616168ba778fabfd0fb594d
Jeff Sittler <mindblender@gmail.com>
2017-01-11 00:12:06 -0700

    modified to use class methods Added check for $_GET params are present, otherwise redirect

--- users/viewclientuser.php
+++ users/viewclientuser.php
@@ -31,38 +31,44 @@
 $checkSession = "true";
 include_once '../includes/library.php';
 
-$tmpquery = "WHERE mem.id = '$id'";
-$userDetail = new phpCollab\Request();
-$userDetail->openMembers($tmpquery);
-$comptUserDetail = count($userDetail->mem_id);
+$members = new \phpCollab\Members\Members();
+$organizations = new \phpCollab\Organizations\Organizations();
 
-if ($comptUserDetail == "0") {
-    phpCollab\Util::headerFunction("../clients/viewclient.php?msg=blankUser&id=$organization");
+$orgId = $_GET['organization'];
+$id = $_GET['id'];
+
+if (empty($id) || empty($orgId)) {
+    phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
 }
-$organization = $userDetail->mem_organization[0];
+
+$userDetail = $members->getMemberById($id);
+
+if (empty($userDetail)) {
+    phpCollab\Util::headerFunction("../clients/viewclient.php?msg=blankUser&id=$orgId");
+}
+$memberOrganization = $userDetail['mem_organization'];
+
+$idSession = \phpCollab\Util::returnGlobal('idSession', 'SESSION');
+$profilSession = \phpCollab\Util::returnGlobal('profilSession', 'SESSION');
 
 if ($clientsFilter == "true" && $profilSession == "2") {
+    $teams = new \phpCollab\Teams\Teams();
     $teamMember = "false";
-    $tmpquery = "WHERE tea.member = '$idSession' AND org2.id = '$organization'";
-    $memberTest = new phpCollab\Request();
-    $memberTest->openTeams($tmpquery);
-    $comptMemberTest = count($memberTest->tea_id);
-    if ($comptMemberTest == "0") {
+
+    $memberTest = $teams->getTeamByTeamMemberAndOrgId($idSession, $memberOrganization);
+
+    if (empty($memberTest)) {
         phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
-    } else {
     }
 } else if ($clientsFilter == "true" && $profilSession == "1") {
-    $tmpquery = "WHERE org.owner = '$idSession' AND org.id = '$organization'";
+    $detailClient = $organizations->getOrganizationByIdAndOwner($idSession, $orgId);
 } else {
-    $tmpquery = "WHERE org.id = '$organization'";
+    $detailClient = $organizations->getOrganizationById($orgId);
 }
 
 $comptDetailClient = "0";
-$detailClient = new phpCollab\Request();
-$detailClient->openOrganizations($tmpquery);
-$comptDetailClient = count($detailClient->org_id);
 
-if ($comptDetailClient == "0") {
+if (empty($detailClient)) {
     phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
 }
 
@@ -71,8 +77,8 @@ include '../themes/' . THEME . '/header.php';
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
 $blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/listclients.php?", $strings["clients"], in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=$organization", $detailClient->org_name[0], in));
-$blockPage->itemBreadcrumbs($userDetail->mem_login[0]);
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=$orgId", $detailClient['org_name'], in));
+$blockPage->itemBreadcrumbs($userDetail['mem_login']);
 $blockPage->closeBreadcrumbs();
 
 $block1 = new phpCollab\Block();
@@ -98,28 +104,28 @@ $block1->closePaletteIcon();
 $block1->openContent();
 $block1->contentTitle($strings["user_details"]);
 
-$block1->contentRow($strings["user_name"], $userDetail->mem_login[0]);
-$block1->contentRow($strings["full_name"], $userDetail->mem_name[0]);
-$block1->contentRow($strings["organization"], $userDetail->mem_org_name[0]);
-$block1->contentRow($strings["email"], $userDetail->mem_email_work[0]);
-$block1->contentRow($strings["work_phone"], $userDetail->mem_phone_work[0]);
-$block1->contentRow($strings["home_phone"], $userDetail->mem_phone_home[0]);
-$block1->contentRow($strings["mobile_phone"], $userDetail->mem_mobile[0]);
-$block1->contentRow($strings["fax"], $userDetail->mem_fax[0]);
-$block1->contentRow($strings["comments"], nl2br($userDetail->mem_comments[0]));
-$block1->contentRow($strings["account_created"], phpCollab\Util::createDate($userDetail->mem_created[0], $timezoneSession));
-$block1->contentRow($strings["last_page"], $userDetail->mem_last_page[0]);
+$block1->contentRow($strings["user_name"], $userDetail['mem_login']);
+$block1->contentRow($strings["full_name"], $userDetail['mem_name']);
+$block1->contentRow($strings["organization"], $userDetail['org_name']);
+$block1->contentRow($strings["email"], $userDetail['mem_email_work']);
+$block1->contentRow($strings["work_phone"], $userDetail['mem_phone_work']);
+$block1->contentRow($strings["home_phone"], $userDetail['mem_phone_home']);
+$block1->contentRow($strings["mobile_phone"], $userDetail['mem_mobile']);
+$block1->contentRow($strings["fax"], $userDetail['mem_fax']);
+$block1->contentRow($strings["comments"], nl2br($userDetail['mem_comments']));
+$block1->contentRow($strings["account_created"], phpCollab\Util::createDate($userDetail['mem_created'], $timezoneSession));
+$block1->contentRow($strings["last_page"], $userDetail['mem_last_page']);
 
 $block1->contentTitle($strings["information"]);
 
-$tmpquery = "SELECT tas.id FROM " . $tableCollab["tasks"] . " tas LEFT OUTER JOIN " . $tableCollab["projects"] . " pro ON pro.id = tas.project WHERE tas.assigned_to = '" . $userDetail->mem_id[0] . "' AND tas.status IN(0,2,3) AND pro.status IN(0,2,3)";
-phpCollab\Util::computeTotal($tmpquery);
-$valueTasks = $countEnregTotal;
+// Refactor
+$tmpquery = "SELECT tas.id FROM {$tableCollab["tasks"]} tas LEFT OUTER JOIN {$tableCollab["projects"]} pro ON pro.id = tas.project WHERE tas.assigned_to = '" . $userDetail['mem_id'] . "' AND tas.status IN(0,2,3) AND pro.status IN(0,2,3)";
+$valueTasks = phpCollab\Util::computeTotal($tmpquery);
 
 $block1->contentRow($strings["tasks"], $valueTasks);
 
 $z = "(Client on project site)";
-if ($userDetail->mem_log_connected[0] > $dateunix - 5 * 60) {
+if ($userDetail['mem_log_connected'] > $dateunix - 5 * 60) {
     $connected_result = $strings["yes"] . " " . $z;
 } else {
     $connected_result = $strings["no"];
@@ -131,11 +137,10 @@ $block1->closeForm();
 
 $block1->openPaletteScript();
 if ($profilSession == "0" || $profilSession == "1") {
-    $block1->paletteScript(0, "remove", "../users/deleteclientusers.php?id=$id&organization=$organization", "true,true,true", $strings["delete"]);
-    $block1->paletteScript(1, "edit", "../users/updateclientuser.php?id=$id&organization=$organization", "true,true,true", $strings["edit"]);
+    $block1->paletteScript(0, "remove", "../users/deleteclientusers.php?id=$id&organization=$orgId", "true,true,true", $strings["delete"]);
+    $block1->paletteScript(1, "edit", "../users/updateclientuser.php?id=$id&organization=$orgId", "true,true,true", $strings["edit"]);
 }
 $block1->paletteScript(2, "export", "../users/exportuser.php?id=$id", "true,true,true", $strings["export"]);
 $block1->closePaletteScript("", "");
 
 include '../themes/' . THEME . '/footer.php';
-?>
