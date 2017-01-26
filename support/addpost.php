b921ad38d99a3b73d35a0f0ee0c0924455f423ad
Jeff Sittler <mindblender@gmail.com>
2017-01-26 00:00:23 -0700

    fix syntax error, code cleanup and getting variables from $_GET, $_POST & $_SESSION

--- support/addpost.php
+++ support/addpost.php
@@ -5,6 +5,10 @@
 $checkSession = "true";
 include_once '../includes/library.php';
 
+$id = $_GET["id"];
+$action = $_GET["action"];
+$idSession = $_SESSION["idSession"];
+
 if ($enableHelpSupport != "true") {
     phpCollab\Util::headerFunction('../general/permissiondenied.php');
 }
@@ -20,16 +24,15 @@ $requestDetail = new phpCollab\Request();
 $requestDetail->openSupportRequests($tmpquery);
 
 if ($action == "edit") {
-        $dbParams = [];
-        $dbParams["status"] = $sta;
-        $dbParams["date_close"] = ($sta == 2) ? $dateheure : '--' ;
-        $dbParams["support_request_id"] = $id;
-        phpCollab\Util::newConnectSql("UPDATE {$tableCollab["support_requests"]} SET status=:status,date_close=:date_close WHERE id = :support_request_id", $dbParams);
-        unset($dbParams);
-    }
+    $dbParams = [];
+    $dbParams["status"] = $_POST["sta"];
+    $dbParams["date_close"] = ($_POST["sta"] == 2) ? $dateheure : '--' ;
+    $dbParams["support_request_id"] = $id;
+    phpCollab\Util::newConnectSql("UPDATE {$tableCollab["support_requests"]} SET status=:status,date_close=:date_close WHERE id = :support_request_id", $dbParams);
+    unset($dbParams);
 
     if ($notifications == "true") {
-        if ($requestDetail->sr_status[0] != $sta) {
+        if ($requestDetail->sr_status[0] != $_POST["sta"]) {
             $num = $id;
             include '../support/noti_statusrequestchange.php';
         }
@@ -39,8 +42,6 @@ if ($action == "edit") {
 }
 
 if ($action == "add") {
-    $mes = phpCollab\Util::convertData($mes);
-
     $num = phpCollab\Util::newConnectSql(
         "INSERT INTO {$tableCollab["support_posts"]} (request_id,message,date,owner,project) VALUES(:request_id, :message, :date, :owner, :project)",
         ["request_id" => $id, "message" => $mes, "date" => $dateheure, "owner" => $idSession, "project" => $requestDetail->sr_project[0]]);
@@ -61,20 +62,20 @@ $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
 
 if ($supportType == "team") {
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $requestDetail->sr_project[0], $requestDetail->sr_pro_name[0], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], in));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $requestDetail->sr_project[0], $requestDetail->sr_pro_name[0], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], "in"));
     if ($action == "status") {
         $blockPage->itemBreadcrumbs($strings["edit_status"]);
     } else {
         $blockPage->itemBreadcrumbs($strings["add_support_response"]);
     }
 } else if ($supportType == "admin") {
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/support.php?", $strings["support_management"], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], in));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], in));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/support.php?", $strings["support_management"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], "in"));
     if ($action == "status") {
         $blockPage->itemBreadcrumbs($strings["edit_status"]);
     } else {
