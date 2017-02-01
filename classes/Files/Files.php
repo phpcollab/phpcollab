c406c1ff64efee1cd66c8111d8e9640cf4af0cd3
Jeff Sittler <mindblender@gmail.com>
2017-01-31 20:45:53 -0700

    Creating Files and FilesGateway classes Added initial methods

---
+++ classes/Files/Files.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace phpCollab\Files;
+
+use phpCollab\Database;
+
+/**
+ * Class Files
+ * @package phpCollab\Files
+ */
+class Files
+{
+    protected $files_gateway;
+    protected $db;
+
+    /**
+     * Files constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->files_gateway = new FilesGateway($this->db);
+    }
+
+    /**
+     * @param $fileId
+     * @return mixed
+     */
+    public function getFiles($fileId)
+    {
+        return $this->files_gateway->getFiles($fileId);
+    }
+
+    /**
+     * @param $fileId
+     * @return mixed
+     */
+    public function getFileById($fileId)
+    {
+        return $this->files_gateway->getFileById($fileId);
+    }
+
+    /**
+     *
+     */
+    public function getPublishedFiles()
+    {
+        $this->files_gateway->getFiles();
+        return;
+    }
+
+    /**
+     *
+     */
+    public function getUnPublishedFiles()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function publishFile()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function unPublishFile()
+    {
+
+    }
+
+    /**
+     * @param $fileId
+     * @return mixed
+     */
+    public function deleteFile($fileId)
+    {
+        $fileId = filter_var((string)$fileId, FILTER_SANITIZE_STRING);
+
+        $response = $this->files_gateway->deleteFiles($fileId);
+        return $response;
+    }
+
+}
---
+++ classes/Files/FilesGateway.php
@@ -0,0 +1,106 @@
+<?php
+
+
+namespace phpCollab\Files;
+
+use phpCollab\Database;
+
+/**
+ * Class FilesGateway
+ * @package phpCollab\Files
+ */
+class FilesGateway
+{
+    protected $db;
+    protected $initrequest;
+    protected $tableCollab;
+
+    /**
+     * FilesGateway constructor.
+     * @param Database $db
+     */
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+        $this->initrequest = $GLOBALS['initrequest'];
+        $this->tableCollab = $GLOBALS['tableCollab'];
+    }
+
+    public function getFiles($fileId)
+    {
+        if ( strpos($fileId, ',') ) {
+            $ids = explode(',', $fileId);
+            $placeholders = str_repeat ('?, ', count($ids)-1) . '?';
+            $sql = $this->initrequest["files"] . " WHERE fil.id IN ($placeholders) OR vc_parent IN ($placeholders)";
+            $this->db->query($sql);
+
+            $this->db->execute(array_merge($ids, $ids));
+
+            return $this->db->fetchAll();
+        } else {
+            $query = $this->initrequest["files"] . " WHERE fil.id IN(:file_id) OR fil.vc_parent IN(:file_id) ORDER BY fil.name";
+
+            $this->db->query($query);
+
+            $this->db->bind(':file_id', $fileId);
+
+            return $this->db->resultset();
+        }
+    }
+
+    public function getFileById($fileId)
+    {
+        $query = $this->initrequest["files"] . " WHERE fil.id IN(:file_id) OR fil.vc_parent IN(:file_id) ORDER BY fil.name";
+        $this->db->query($query);
+        $this->db->bind(':file_id', $fileId);
+        return $this->db->single();
+    }
+
+    public function deleteFiles($fileId)
+    {
+
+        if ( strpos($fileId, ',') ) {
+            $ids = explode(',', $fileId);
+            $placeholders = str_repeat ('?, ', count($ids)-1) . '?';
+            $sql = "DELETE FROM {$this->tableCollab['files']} WHERE id IN ($placeholders) OR vc_parent IN($placeholders)";
+            $this->db->query($sql);
+
+            $this->db->execute(array_merge($ids, $ids));
+
+            return $this->db->fetchAll();
+        } else {
+            $query = "DELETE FROM {$this->tableCollab['files']} WHERE id IN (:file_id) OR vc_parent IN(:file_id)";
+
+            $this->db->query($query);
+
+            $this->db->bind(':file_id', $fileId);
+
+            return $this->db->execute();
+        }
+    }
+
+    /**
+     * @param string $sorting
+     * @return string
+     */
+/*
+    private function orderBy($sorting)
+    {
+        if (!is_null($sorting)) {
+            $allowedOrderedBy = ["boo.name", "boo.category", "mem.login"];
+            $pieces = explode(' ', $sorting);
+
+            if ($pieces) {
+                $key = array_search($pieces[0], $allowedOrderedBy);
+
+                if ($key !== false) {
+                    $order = $allowedOrderedBy[$key];
+                    return " ORDER BY $order $pieces[1]";
+                }
+            }
+        }
+
+        return '';
+    }
+*/
+}
