9d3496f39c3d30dab245a01bc57861b07021640a
Jeff Sittler <mindblender@gmail.com>
2017-01-31 20:49:35 -0700

    Refactored to use the new Files class Code cleanup

--- linkedcontent/deletefiles.php
+++ linkedcontent/deletefiles.php
@@ -1,74 +1,76 @@
 <?php
-#Application name: PhpCollab
-#Status page: 0
-#Path by root: ../linkedcontent/deletefiles.php
-
 $checkSession = "true";
 include_once '../includes/library.php';
 
+$files = new \phpCollab\Files\Files();
+
+$action = $_GET["action"];
+$task = $_GET["task"];
+$project = $_GET["project"];
+$sendto = $_POST["sendto"];
+$id = $_GET["id"];
+$strings = $GLOBALS["strings"];
+
 if ($task == "") {
-	$task = "0";
+    $task = "0";
 }
 
 if ($action == "delete") {
-$id = str_replace("**",",",$id);
-$tmpquery1 = "DELETE FROM ".$tableCollab["files"]." WHERE id IN($id) OR vc_parent IN($id)";
+    $id = str_replace("**", ",", $id);
 
-$tmpquery = "WHERE fil.id IN($id) OR fil.vc_parent IN($id) ORDER BY fil.name";
-$listFiles = new phpCollab\Request();
-$listFiles->openFiles($tmpquery);
-$comptListFiles = count($listFiles->fil_id);
-	for ($i=0;$i<$comptListFiles;$i++) {
-		if ($task != "0") {
-			if (file_exists ("../files/".$project."/".$task."/".$listFiles->fil_name[$i])) {
-				phpCollab\Util::deleteFile("files/".$project."/".$task."/".$listFiles->fil_name[$i]);
-			}
-		} else {
-			if (file_exists ("../files/".$project."/".$listFiles->fil_name[$i])) {
-				phpCollab\Util::deleteFile("files/".$project."/".$listFiles->fil_name[$i]);
-			}
-		}
-	}
-	phpCollab\Util::connectSql("$tmpquery1");
-	if ($sendto == "filedetails"){
-		phpCollab\Util::headerFunction("../linkedcontent/viewfile.php?id=".$listFiles->fil_vc_parent[0]."&msg=deleteFile");
-	} else {
-		if ($task != "0") {
-			phpCollab\Util::headerFunction("../tasks/viewtask.php?id=$task&msg=deleteFile");
-		} else {
-			phpCollab\Util::headerFunction("../projects/viewproject.php?id=$project&msg=deleteFile");
-		}
-	}
+    $listFiles = $files->getFiles($id);
+
+    foreach ($listFiles as $file) {
+        if ($task != "0") {
+            if (file_exists("../files/" . $project . "/" . $task . "/" . $file["fil_name"])) {
+                phpCollab\Util::deleteFile("files/" . $project . "/" . $task . "/" . $file["fil_name"]);
+            }
+        } else {
+            if (file_exists("../files/" . $project . "/" . $file["fil_name"])) {
+                phpCollab\Util::deleteFile("files/" . $project . "/" . $file["fil_name"]);
+            }
+        }
+    }
+
+    $deleteFile = $files->deleteFile($id);
+
+    if ($sendto == "filedetails") {
+        phpCollab\Util::headerFunction("../linkedcontent/viewfile.php?id=" . $listFiles["fil_vc_parent"] . "&msg=deleteFile");
+    } else {
+        if ($task != "0") {
+            phpCollab\Util::headerFunction("../tasks/viewtask.php?id=$task&msg=deleteFile");
+        } else {
+            phpCollab\Util::headerFunction("../projects/viewproject.php?id=$project&msg=deleteFile");
+        }
+    }
 }
 
-$tmpquery = "WHERE pro.id = '$project'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
-
-if ($task != "0") {
-	$tmpquery = "WHERE tas.id = '$task'";
-	$taskDetail = new phpCollab\Request();
-	$taskDetail->openTasks($tmpquery);
-}
+//$tmpquery = "WHERE pro.id = '$project'";
+//$projectDetail = new phpCollab\Request();
+//$projectDetail->openProjects($tmpquery);
+$projects = new \phpCollab\Projects\Projects();
+$projectDetail = $projects->getProjectById($project);
 
 include '../themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?",$strings["projects"],in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=".$projectDetail->pro_id[0],$projectDetail->pro_name[0],in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail["pro_id"], $projectDetail["pro_name"], "in"));
 
 if ($task != "0") {
-	$blockPage->itemBreadcrumbs($blockPage->buildLink("../tasks/listtasks.php?project=".$projectDetail->pro_id[0],$strings["tasks"],in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../tasks/viewtask.php?id=".$taskDetail->tas_id[0],$taskDetail->tas_name[0],in));
+    $tasks = new \phpCollab\Tasks\Tasks();
+    $taskDetail = $tasks->getTaskById($task);
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../tasks/listtasks.php?project=" . $projectDetail["pro_id"], $strings["tasks"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../tasks/viewtask.php?id=" . $taskDetail["tas_id"], $taskDetail["tas_name"], "in"));
 }
 
 $blockPage->itemBreadcrumbs($strings["unlink_files"]);
 $blockPage->closeBreadcrumbs();
 
 if ($msg != "") {
-	include '../includes/messages.php';
-	$blockPage->messageBox($msgLabel);
+    include '../includes/messages.php';
+    $blockPage->messageBox($msgLabel);
 }
 
 $block1 = new phpCollab\Block();
@@ -81,19 +83,22 @@ $block1->heading($strings["unlink_files"]);
 $block1->openContent();
 $block1->contentTitle($strings["delete_following"]);
 
-$id = str_replace("**",",",$id);
-$tmpquery = "WHERE fil.id IN($id) ORDER BY fil.name";
-$listFiles = new phpCollab\Request();
-$listFiles->openFiles($tmpquery);
-$comptListFiles = count($listFiles->fil_id);
+$id = str_replace("**", ",", $id);
+$listFiles = $files->getFiles($id);
 
-for ($i=0;$i<$comptListFiles;$i++) {
-	echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">&nbsp;</td><td>".$listFiles->fil_name[$i]."</td></tr>";
+foreach ($listFiles as $file) {
+    echo '<tr class="odd"><td valign="top" class="leftvalue">&nbsp;</td>';
+    echo '<td>' . $file["fil_name"] . '</td></tr>';
 }
 
-echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">&nbsp;</td><td><input type=\"SUBMIT\" value=\"".$strings["delete"]."\">&#160;<input type=\"BUTTON\" value=\"".$strings["cancel"]."\" onClick=\"history.back();\"></td></tr>";
+echo <<<HTML
+<tr class="odd">
+    <td valign="top" class="leftvalue">&nbsp;</td>
+    <td><input type="SUBMIT" value="{$strings["delete"]}">&#160;<input type="BUTTON" value="{$strings["cancel"]}" onClick="history.back();"></td>
+</tr>
+HTML;
 
 $block1->closeContent();
 $block1->closeForm();
 
-include '../themes/'.THEME.'/footer.php';
+include '../themes/' . THEME . '/footer.php';
