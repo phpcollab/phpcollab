8fc8ee6fb31f15fafa292ddbb0834db391be3e23
Jeff Sittler <mindblender@gmail.com>
2017-02-03 01:46:57 -0700

    Adding Notes and NotesGateway classes

---
+++ classes/Notes/Notes.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace phpCollab\Notes;
+
+use phpCollab\Database;
+
+/**
+ * Class Notes
+ * @package phpCollab\Notes
+ */
+class Notes
+{
+    protected $otes_gateway;
+    protected $db;
+
+    /**
+     * Notes constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->notes_gateway = new NotesGateway($this->db);
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function getNoteById($noteId)
+    {
+        return $this->notes_gateway->getNoteById($noteId);
+    }
+
+    /**
+     * @param $projectId
+     * @param null $sorting
+     * @return mixed
+     * @internal param $noteId
+     */
+    public function getNoteByProject($projectId, $sorting = null)
+    {
+        return $this->notes_gateway->getNoteByProject($projectId, $sorting);
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function deleteNotes($noteId)
+    {
+        $noteId = filter_var((string)$noteId, FILTER_SANITIZE_STRING);
+
+        $response = $this->notes_gateway->deleteNotes($noteId);
+        return $response;
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function publishToSite($noteId)
+    {
+        $noteId = filter_var((string)$noteId, FILTER_SANITIZE_STRING);
+        return $this->notes_gateway->publishNoteToSite($noteId);
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function unPublishFromSite($noteId)
+    {
+        $noteId = filter_var((string)$noteId, FILTER_SANITIZE_STRING);
+        return $this->notes_gateway->unPublishNoteFromSite($noteId);
+    }
+
+}
---
+++ classes/Notes/NotesGateway.php
@@ -0,0 +1,140 @@
+<?php
+
+
+namespace phpCollab\Notes;
+
+use phpCollab\Database;
+
+/**
+ * Class NotesGateway
+ * @package phpCollab\Notes
+ */
+class NotesGateway
+{
+    protected $db;
+    protected $initrequest;
+    protected $tableCollab;
+
+    /**
+     * NotesGateway constructor.
+     * @param Database $db
+     */
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+        $this->initrequest = $GLOBALS['initrequest'];
+        $this->tableCollab = $GLOBALS['tableCollab'];
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function getNoteById($noteId)
+    {
+        $query = $this->initrequest["notes"] . " WHERE fil.id IN(:note_id) OR fil.vc_parent IN(:note_id) ORDER BY fil.name";
+        $this->db->query($query);
+        $this->db->bind(':note_id', $noteId);
+        return $this->db->single();
+    }
+
+    /**
+     * @param $projectId
+     * @param $sorting
+     * @return mixed
+     * @internal param $noteId
+     */
+    public function getNoteByProject($projectId, $sorting)
+    {
+        $query = $this->initrequest["notes"] . " WHERE note.project = :project_id" . $this->orderBy($sorting);
+        $this->db->query($query);
+        $this->db->bind(':project_id', $projectId);
+        return $this->db->resultset();
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function deleteNotes($noteId)
+    {
+        if ( strpos($noteId, ',') ) {
+            $ids = explode(',', $noteId);
+            $placeholders = str_repeat ('?, ', count($ids)-1) . '?';
+            $sql = "DELETE FROM {$this->tableCollab['notes']} WHERE id IN ($placeholders)";
+            $this->db->query($sql);
+
+            $this->db->execute($ids);
+
+            return $this->db->fetchAll();
+        } else {
+            $query = "DELETE FROM {$this->tableCollab['notes']} WHERE id IN (:note_id)";
+            $this->db->query($query);
+            $this->db->bind(':note_id', $noteId);
+            return $this->db->execute();
+        }
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function publishNoteToSite($noteId)
+    {
+        if ( strpos($noteId, ',') ) {
+            $ids = explode(',', $noteId);
+            $placeholders = str_repeat ('?, ', count($ids)-1) . '?';
+            $sql = "UPDATE {$this->tableCollab["notes"]} SET published=0 WHERE id IN($placeholders)";
+            $this->db->query($sql);
+            return $this->db->execute($ids);
+        } else {
+            $query = "UPDATE {$this->tableCollab["notes"]} SET published=0 WHERE id = :note_id";
+            $this->db->query($query);
+            $this->db->bind(':note_id', $noteId);
+            return $this->db->execute();
+        }
+    }
+
+    /**
+     * @param $noteId
+     * @return mixed
+     */
+    public function unPublishNoteFromSite($noteId)
+    {
+        if ( strpos($noteId, ',') ) {
+            $ids = explode(',', $noteId);
+            $placeholders = str_repeat ('?, ', count($ids)-1) . '?';
+            $sql = "UPDATE {$this->tableCollab["notes"]} SET published=1 WHERE id IN($placeholders)";
+            $this->db->query($sql);
+            return $this->db->execute($ids);
+        } else {
+            $query = "UPDATE {$this->tableCollab["notes"]} SET published=1 WHERE id = :note_id";
+            $this->db->query($query);
+            $this->db->bind(':note_id', $noteId);
+            return $this->db->execute();
+        }
+    }
+
+    /**
+     * @param string $sorting
+     * @return string
+     */
+    private function orderBy($sorting)
+    {
+        if (!is_null($sorting)) {
+            $allowedOrderedBy = ["note.id", "note.project", "note.owner", "note.topic", "note.subject", "note.description", "note.date", "note.published", "mem.id", "mem.login", "mem.name", "mem.email_work"];
+        $pieces = explode(' ', $sorting);
+
+            if ($pieces) {
+                $key = array_search($pieces[0], $allowedOrderedBy);
+
+                if ($key !== false) {
+                    $order = $allowedOrderedBy[$key];
+                    return " ORDER BY $order $pieces[1]";
+                }
+            }
+        }
+
+        return '';
+    }
+}
