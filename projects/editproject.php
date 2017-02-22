bfaaf0079ba5663ee8fdb63fa877afcb04dcf125
Jeff Sittler <mindblender@gmail.com>
2017-02-21 18:46:56 -0700

    Refactored insert phases SQL call to use newConnectSql method

--- projects/editproject.php
+++ projects/editproject.php
@@ -632,8 +632,10 @@ STAMP;
 		
 			for($i=0;$i<$comptThisPhase;$i++)
 			{
-				$tmpquery = "INSERT INTO ".$tableCollab["phases"]."(project_id,order_num,status,name) VALUES('$num','$i','0','".$phaseArraySets[$thisPhase][$i]."')";
-				phpCollab\Util::connectSql("$tmpquery");
+				phpCollab\Util::newConnectSql(
+				    "INSERT INTO {$tableCollab["phases"]} (project_id,order_num,status,name) VALUES(:project_id,:order_num,:status,:name)",
+                    ["project_id" => $num,"order_num" => $i,"status" => 0,"name" => $phaseArraySets[$thisPhase][$i]]
+                );
 			}
 		}
 
