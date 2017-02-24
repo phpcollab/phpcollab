bbb2f41cf7df1ed5426a69042bbac212d996fd4a
Jeff Sittler <mindblender@gmail.com>
2017-02-24 00:33:53 -0700

    initial add of the Notifications and NotificationsGateway classes

---
+++ classes/Notifications/Notifications.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace phpCollab\Notifications;
+
+use phpCollab\Database;
+
+/**
+ * Class Notifications
+ * @package phpCollab\Notifications
+ */
+class Notifications
+{
+    protected $notifications_gateway;
+    protected $db;
+
+    /**
+     * Members constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+
+        $this->notifications_gateway = new NotificationsGateway($this->db);
+    }
+
+}
