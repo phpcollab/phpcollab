d445c96406a8721c97ab0decb81a43fdbbd6e341
Jeff Sittler <mindblender@gmail.com>
2017-02-24 00:16:50 -0700

    Added deleteMemberByIdIn method generated missing doc blocks

--- classes/Members/Members.php
+++ classes/Members/Members.php
@@ -4,11 +4,18 @@ namespace phpCollab\Members;
 
 use phpCollab\Database;
 
+/**
+ * Class Members
+ * @package phpCollab\Members
+ */
 class Members
 {
     protected $members_gateway;
     protected $db;
 
+    /**
+     * Members constructor.
+     */
     public function __construct()
     {
         $this->db = new Database();
@@ -16,12 +23,20 @@ class Members
         $this->members_gateway = new MembersGateway($this->db);
     }
 
+    /**
+     * @param $memberLogin
+     * @return mixed
+     */
     public function getMemberByLogin($memberLogin) {
         $data = $this->members_gateway->getMemberByLogin($memberLogin);
 
         return $data;
     }
 
+    /**
+     * @param $memberId
+     * @return mixed
+     */
     public function getMemberById($memberId) {
         $memberId = filter_var($memberId, FILTER_VALIDATE_INT);
 
@@ -30,6 +45,11 @@ class Members
         return $data;
     }
 
+    /**
+     * @param $orgId
+     * @param $sorting
+     * @return mixed
+     */
     public function getMembersByOrg($orgId, $sorting) {
         $orgId = filter_var($orgId, FILTER_VALIDATE_INT);
         $sorting = filter_var($sorting, FILTER_SANITIZE_STRING);
@@ -39,16 +59,31 @@ class Members
         return $data;
     }
 
+    /**
+     * @return mixed
+     */
     public function getAllMembers() {
         $data = $this->members_gateway->getAllMembers();
 
         return $data;
     }
 
+    /**
+     * @param $orgId
+     * @return mixed
+     */
     public function deleteMemberByOrgId($orgId)
     {
         $orgId = filter_var($orgId, FILTER_SANITIZE_STRING);
         return $this->members_gateway->deleteMember($orgId);
     }
 
+    /**
+     * @param $memberIds
+     * @return mixed
+     */
+    public function deleteMemberByIdIn($memberIds)
+    {
+        return $this->members_gateway->deleteMemberByIdIn($memberIds);
+    }
 }
