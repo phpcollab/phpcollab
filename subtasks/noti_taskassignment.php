da5cfdc40ae32f48b42aa0385fbb0deec11a913b
Jeff Sittler <mindblender@gmail.com>
2017-04-04 20:58:42 -0700

    Code refactoring Refactored $subtaskNoti to use the Tasks getSubtasksByIdIn method

--- subtasks/noti_taskassignment.php
+++ subtasks/noti_taskassignment.php
@@ -1,12 +1,12 @@
 <?php
-if ($num == "") {
-    $num = $id;
-}
-$tmpquery = "WHERE subtas.id IN($num)";
-$subtaskNoti = new phpCollab\Request();
-$subtaskNoti->openSubtasks($tmpquery);
 
-$tmpquery = "WHERE tas.id = '" . $subtaskNoti->subtas_task[0] . "'";
+$num = isset($_GET["num"]) ? $_GET["num"] : $_GET["id"];
+
+$tasks = new \phpCollab\Tasks\Tasks();
+
+$subtaskNoti = $tasks->getSubTaskByIdIn($num);
+
+$tmpquery = "WHERE tas.id = '" . $subtaskNoti["subtas_task"] . "'";
 $taskNoti = new phpCollab\Request();
 $taskNoti->openTasks($tmpquery);
 
@@ -31,23 +31,23 @@ if ($listNotifications->not_taskassignment[0] == "0") {
         $projectNoti->pro_org_name[0] = $strings["none"];
     }
 
-    $complValue = ($subtaskNoti->subtas_completion[0] > 0) ? $subtaskNoti->subtas_completion[0] . "0 %" : $subtaskNoti->subtas_completion[0] . " %";
-    $idStatus = $subtaskNoti->subtas_status[0];
-    $idPriority = $subtaskNoti->subtas_priority[0];
+    $complValue = ($subtaskNoti["subtas_completion"] > 0) ? $subtaskNoti["subtas_completion"] . "0 %" : $subtaskNoti["subtas_completion"] . " %";
+    $idStatus = $subtaskNoti["subtas_status"];
+    $idPriority = $subtaskNoti["subtas_priority"];
 
-    $body = $mail->partMessage . "\n\n" . $strings["subtask"] . " : " . $subtaskNoti->subtas_name[0] . "\n" . $strings["start_date"] . " : " . $subtaskNoti->subtas_start_date[0] . "\n" . $strings["due_date"] . " : " . $subtaskNoti->subtas_due_date[0] . "\n" . $strings["completion"] . " : " . $complValue . "\n" . $strings["priority"] . " : $priority[$idPriority]\n" . $strings["status"] . " : $status[$idStatus]\n" . $strings["description"] . " : " . $subtaskNoti->subtas_description[0] . "\n\n" . $strings["project"] . " : " . $projectNoti->pro_name[0] . " (" . $projectNoti->pro_id[0] . ")\n" . $strings["task"] . " : " . $taskNoti->tas_name[0] . " (" . $taskNoti->tas_id[0] . ")\n" . $strings["organization"] . " : " . $projectNoti->pro_org_name[0] . "\n\n" . $strings["noti_moreinfo"] . "\n";
+    $body = $mail->partMessage . "\n\n" . $strings["subtask"] . " : " . $subtaskNoti["subtas_name"] . "\n" . $strings["start_date"] . " : " . $subtaskNoti["subtas_start_date"] . "\n" . $strings["due_date"] . " : " . $subtaskNoti["subtas_due_date"] . "\n" . $strings["completion"] . " : " . $complValue . "\n" . $strings["priority"] . " : $priority[$idPriority]\n" . $strings["status"] . " : $status[$idStatus]\n" . $strings["description"] . " : " . $subtaskNoti["subtas_description"] . "\n\n" . $strings["project"] . " : " . $projectNoti->pro_name[0] . " (" . $projectNoti->pro_id[0] . ")\n" . $strings["task"] . " : " . $taskNoti->tas_name[0] . " (" . $taskNoti->tas_id[0] . ")\n" . $strings["organization"] . " : " . $projectNoti->pro_org_name[0] . "\n\n" . $strings["noti_moreinfo"] . "\n";
 
-    if ($subtaskNoti->subtas_mem_organization[0] == "1") {
+    if ($subtaskNoti["subtas_mem_organization"] == "1") {
         $body .= "$root/general/login.php?url=subtasks/viewsubtask.php%3Fid=$num%26task=" . $taskNoti->tas_id[0];
-    } else if ($subtaskNoti->subtas_mem_organization[0] != "1" && $projectNoti->pro_published[0] == "0" && $subtaskNoti->subtas_published[0] == "0") {
+    } else if ($subtaskNoti["subtas_mem_organization"] != "1" && $projectNoti->pro_published[0] == "0" && $subtaskNoti["subtas_published"] == "0") {
         $body .= "$root/general/login.php?url=projects_site/home.php%3Fproject=" . $projectNoti->pro_id[0];
     }
 
     $body .= "\n\n" . $mail->footer;
-    $subject = $mail->partSubject . " " . $subtaskNoti->subtas_name[0];
+    $subject = $mail->partSubject . " " . $subtaskNoti["subtas_name"];
     $mail->Subject = $subject;
 
-    if ($subtaskNoti->subtas_priority[0] == "4" || $subtaskNoti->subtas_priority[0] == "5") {
+    if ($subtaskNoti["subtas_priority"] == "4" || $subtaskNoti["subtas_priority"] == "5") {
         $mail->Priority = "1";
     } else {
         $mail->Priority = "3";
@@ -58,4 +58,3 @@ if ($listNotifications->not_taskassignment[0] == "0") {
     $mail->Send();
     $mail->ClearAddresses();
 }
-?>
