882a3d0eee97e590d8fa3f731d7ac4288a3a2892
Jeff Sittler <mindblender@gmail.com>
2017-04-25 21:08:46 -0700

    Refactored $taskNoti to use Tasks->getTasksById method Updated references

--- tasks/noti_clientaddtask.php
+++ tasks/noti_clientaddtask.php
@@ -1,12 +1,15 @@
 <?php
+
 if ($num == "") {
     $num = $id;
 }
-$tmpquery = "WHERE tas.id IN($num)";
-$taskNoti = new phpCollab\Request();
-$taskNoti->openTasks($tmpquery);
 
-$tmpquery = "WHERE pro.id = '" . $taskNoti->tas_project[0] . "'";
+$tasks = new \phpCollab\Tasks\Tasks();
+
+$taskNoti = $tasks->getTasksById($num);
+
+
+$tmpquery = "WHERE pro.id = '" . $taskNoti["tas_project"] . "'";
 $projectNoti = new phpCollab\Request();
 $projectNoti->openProjects($tmpquery);
 
@@ -26,20 +29,20 @@ if ($listNotifications->not_taskassignment[0] == "0") {
     if ($projectNoti->pro_org_id[0] == "1") {
         $projectNoti->pro_org_name[0] = $strings["none"];
     }
-    $complValue = ($taskNoti->tas_completion[0] > 0) ? $taskNoti->tas_completion[0] . "0 %" : $taskNoti->tas_completion[0] . " %";
-    $idStatus = $taskNoti->tas_status[0];
-    $idPriority = $taskNoti->tas_priority[0];
+    $complValue = ($taskNoti["tas_completion"] > 0) ? $taskNoti["tas_completion"] . "0 %" : $taskNoti["tas_completion"] . " %";
+    $idStatus = $taskNoti["tas_status"];
+    $idPriority = $taskNoti["tas_priority"];
 
-    $body = $mail->partMessage . "\n\n" . $strings["task"] . " : " . $taskNoti->tas_name[0] . "\n" . $strings["start_date"] . " : " . $taskNoti->tas_start_date[0] . "\n" . $strings["due_date"] . " : " . $taskNoti->tas_due_date[0] . "\n" . $strings["completion"] . " : " . $complValue . "\n" . $strings["priority"] . " : $priority[$idPriority]\n" . $strings["status"] . " : $status[$idStatus]\n" . $strings["description"] . " : " . $taskNoti->tas_description[0] . "\n\n" . $strings["project"] . " : " . $projectNoti->pro_name[0] . " (" . $projectNoti->pro_id[0] . ")\n" . $strings["organization"] . " : " . $projectNoti->pro_org_name[0] . "\n\n" . $strings["noti_moreinfo"] . "\n";
+    $body = $mail->partMessage . "\n\n" . $strings["task"] . " : " . $taskNoti["tas_name"] . "\n" . $strings["start_date"] . " : " . $taskNoti["tas_start_date"] . "\n" . $strings["due_date"] . " : " . $taskNoti["tas_due_date"] . "\n" . $strings["completion"] . " : " . $complValue . "\n" . $strings["priority"] . " : $priority[$idPriority]\n" . $strings["status"] . " : $status[$idStatus]\n" . $strings["description"] . " : " . $taskNoti["tas_description"] . "\n\n" . $strings["project"] . " : " . $projectNoti->pro_name[0] . " (" . $projectNoti->pro_id[0] . ")\n" . $strings["organization"] . " : " . $projectNoti->pro_org_name[0] . "\n\n" . $strings["noti_moreinfo"] . "\n";
 
     $body .= "$root/general/login.php?url=tasks/viewtask.php%3Fid=$num";
 
     $body .= "\n\n" . $mail->footer;
 
-    $subject = $mail->partSubject . " " . $taskNoti->tas_name[0];
+    $subject = $mail->partSubject . " " . $taskNoti["tas_name"];
 
     $mail->Subject = $subject;
-    if ($taskNoti->tas_priority[0] == "4" || $taskNoti->tas_priority[0] == "5") {
+    if ($taskNoti["tas_priority"] == "4" || $taskNoti["tas_priority"] == "5") {
         $mail->Priority = "1";
     } else {
         $mail->Priority = "3";
