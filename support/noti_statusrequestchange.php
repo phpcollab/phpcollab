5e2e7aea754f64270fd9951bd0c691c67ea2336f
Jeff Sittler <mindblender@gmail.com>
2017-05-16 18:21:58 -0700

    Refactored $requestDetail to use Support->getRequestB yId method Updated references Scoped variables

--- support/noti_statusrequestchange.php
+++ support/noti_statusrequestchange.php
@@ -1,25 +1,26 @@
 <?php
 $mail = new phpCollab\Notification();
 $members = new \phpCollab\Members\Members();
+$supportRequests = new \phpCollab\Support\Support();
 
-$mail->getUserinfo($idSession, "from");
+$mail->getUserinfo($_SESSION["idSession"], "from");
 
-$tmpquery = "WHERE sr.id = '$id'";
-$requestDetail = new phpCollab\Request();
-$requestDetail->openSupportRequests($tmpquery);
+$strings = $GLOBALS["strings"];
 
-$userDetail = $members->getMemberById($requestDetail->sr_user[0]);
+$requestDetail = $supportRequests->getSupportRequestById($_GET["id"]);
+
+$userDetail = $members->getMemberById($requestDetail["sr_user"]);
 
 $mail->partSubject = $strings["support"] . " " . $strings["support_id"];
 $mail->partMessage = $strings["noti_support_status2"];
-$subject = $mail->partSubject . ": " . $requestDetail->sr_id[0];
+$subject = $mail->partSubject . ": " . $requestDetail["sr_id"];
 $body = $mail->partMessage . "";
 
-$body .= "\n\n" . $strings["id"] . " : " . $requestDetail->sr_id[0] . "\n" . $strings["subject"] . " : " . $requestDetail->sr_subject[0] . "\n" . $strings["status"] . " : " . $requestStatus[$requestDetail->sr_status[0]] . "\n" . $strings["details"] . " : ";
+$body .= "\n\n" . $strings["id"] . " : " . $requestDetail["sr_id"] . "\n" . $strings["subject"] . " : " . $requestDetail["sr_subject"] . "\n" . $strings["status"] . " : " . $requestStatus[$requestDetail["sr_status"]] . "\n" . $strings["details"] . " : ";
 if ($listTeam->tea_mem_profil[$i] == 3) {
-    $body .= "$root/general/login.php?url=projects_site/home.php%3Fproject=" . $requestDetail->sr_project[0] . "\n\n";
+    $body .= "$root/general/login.php?url=projects_site/home.php%3Fproject=" . $requestDetail["sr_project"] . "\n\n";
 } else {
-    $body .= "$root/general/login.php?url=support/viewrequest.php%3Fid=$num \n\n";
+    $body .= "$root/general/login.php?url=support/viewrequest.php%3Fid={$_GET["num"]}\n\n";
 }
 
 $mail->Subject = $subject;
