4d7ffc3b62f7f5611ad3e2642325bac97b144f91
Jeff Sittler <mindblender@gmail.com>
2017-05-24 16:45:31 -0700

    Added User acceptance test

---
+++ tests/acceptance/loggedIn/UserCest.php
@@ -0,0 +1,62 @@
+<?php
+namespace loggedIn;
+use \AcceptanceTester;
+
+class UserCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/general/login.php');
+        $I->fillField(['name' => 'loginForm'], 'testUser');
+        $I->fillField(['name' => 'passwordForm'], 'testing');
+        $I->click('input[type="submit"]');
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function viewHomePage(AcceptanceTester $I)
+    {
+        $I->wantTo('See the home page');
+        $I->amOnPage('/general/home.php');
+        $I->see('PhpCollab : Home Page');
+    }
+
+    public function viewProfile(AcceptanceTester $I)
+    {
+        $I->wantTo('Logout');
+        $I->amOnPage('/general/home.php');
+        $I->click('#account > a:nth-child(2)');
+        $I->seeElement('form', ['name' => 'user_edit_profileForm']);
+    }
+
+    public function changePassword(AcceptanceTester $I)
+    {
+        $I->wantTo('Change my password');
+        $I->amOnPage('/general/home.php');
+        $I->click('#account > a:nth-child(2)');
+        $I->seeElement('form', ['name' => 'user_edit_profileForm']);
+        $I->click('.breadcrumbs > a:nth-child(1)');
+        $I->seeElement('form', ['name' => 'change_passwordForm']);
+    }
+
+    public function changeNotifications(AcceptanceTester $I)
+    {
+        $I->wantTo('Change my notifications');
+        $I->amOnPage('/general/home.php');
+        $I->click('#account > a:nth-child(2)');
+        $I->seeElement('form', ['name' => 'user_edit_profileForm']);
+        $I->click('.breadcrumbs > a:nth-child(2)');
+        $I->seeElement('form', ['name' => 'user_avertForm']);
+    }
+
+    public function logout(AcceptanceTester $I)
+    {
+        $I->wantTo('Logout');
+        $I->amOnPage('/general/home.php');
+        $I->click('#account > a:nth-child(1)');
+        $I->see('You have successfully logged out. You can log back in by typing your user name and password below.', 'table.message td');
+    }
+}
