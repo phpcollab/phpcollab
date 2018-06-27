52df27cf79faafa1561b1d5743ca5920cb4aa34d
Jeff Sittler <mindblender@gmail.com>
2018-06-26 19:29:27 -0700

    Added setConnectedByLogin method Added phpDoc blocks

--- classes/Logs/Logs.php
+++ classes/Logs/Logs.php
@@ -10,6 +10,9 @@ class Logs
     protected $logs_gateway;
     protected $db;
 
+    /**
+     * Logs constructor.
+     */
     public function __construct()
     {
         $this->db = new Database();
@@ -17,21 +20,43 @@ class Logs
         $this->logs_gateway = new LogsGateway($this->db);
     }
 
+    /**
+     * @param $memberLogin
+     * @return mixed
+     */
     public function getLogByLogin($memberLogin) {
         $data = $this->logs_gateway->getLogByLogin($memberLogin);
 
         return $data;
     }
 
+    /**
+     * @param $entryData
+     * @return mixed
+     */
     public function insertLogEntry($entryData) {
         $data = $this->logs_gateway->insertLogEntry($entryData);
 
         return $data;
     }
 
+    /**
+     * @param $entryData
+     * @return mixed
+     */
     public function updateLogEntry($entryData) {
         $data = $this->logs_gateway->updateLogEntry($entryData);
 
         return $data;
+    }
+
+    /**
+     * @param string $login
+     * @param boolean $connected
+     */
+    public function setConnectedByLogin($login, $connected)
+    {
+//        $tmpquery1 = "UPDATE {$tableCollab["logs"]} SET connected='' WHERE login = :login_id";
+        $data = $this->logs_gateway->setConnectedByLogin($login, $connected);
     }
 }
