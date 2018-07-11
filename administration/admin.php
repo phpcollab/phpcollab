00edd0db3249da5d2cb8435c79964b31fb450d17
Jeff Sittler <mindblender@gmail.com>
2018-07-11 00:15:29 -0700

    Removed trailing ? from links

--- administration/admin.php
+++ administration/admin.php
@@ -53,24 +53,24 @@ $block1->heading($strings["administration"]);
 $block1->openContent();
 $block1->contentTitle($strings["admin_intro"]);
 
-$block1->contentRow("", $blockPage->buildLink("../users/listusers.php?", $strings["user_management"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../users/listusers.php", $strings["user_management"], "in"));
 
 if ($enableInvoicing == "true") {
-    $block1->contentRow("", $blockPage->buildLink("../services/listservices.php?", $strings["service_management"], "in"));
+    $block1->contentRow("", $blockPage->buildLink("../services/listservices.php", $strings["service_management"], "in"));
 }
 
 if ($supportType == "admin") {
-    $block1->contentRow("", $blockPage->buildLink("../administration/support.php?", $strings["support_management"], "in"));
+    $block1->contentRow("", $blockPage->buildLink("../administration/support.php", $strings["support_management"], "in"));
 }
 
 if ($databaseType == "mysql") {
-    $block1->contentRow("", $blockPage->buildLink("../administration/phpmyadmin.php?", $strings["database"], "in"));
+    $block1->contentRow("", $blockPage->buildLink("../administration/phpmyadmin.php", $strings["database"], "in"));
 }
 
-$block1->contentRow("", $blockPage->buildLink("../administration/systeminfo.php?", $strings["system_information"], "in"));
-$block1->contentRow("", $blockPage->buildLink("../administration/mycompany.php?", $strings["company_details"], "in"));
-$block1->contentRow("", $blockPage->buildLink("../administration/listlogs.php?", $strings["logs"], "in"));
-$block1->contentRow($strings["update"] . $blockPage->printHelp("admin_update"), "1. " . $blockPage->buildLink("../administration/updatesettings.php?", $strings["edit_settings"], "in") . " 2. " . $blockPage->buildLink("../administration/updatedatabase.php?", $strings["edit_database"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../administration/systeminfo.php", $strings["system_information"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../administration/mycompany.php", $strings["company_details"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../administration/listlogs.php", $strings["logs"], "in"));
+$block1->contentRow($strings["update"] . $blockPage->printHelp("admin_update"), "1. " . $blockPage->buildLink("../administration/updatesettings.php", $strings["edit_settings"], "in") . " 2. " . $blockPage->buildLink("../administration/updatedatabase.php", $strings["edit_database"], "in"));
 
 if ($updateChecker == "true" && $installationType == "online") {
     $block1->contentRow("", phpCollab\Util::updateChecker($version));
