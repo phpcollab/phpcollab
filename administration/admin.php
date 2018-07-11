c46ce46bc2ab2575abdbf3ba655802ecade50f23
Jeff Sittler <mindblender@gmail.com>
2018-07-10 23:36:41 -0700

    Code cleanup Added APP_ROOT to include calls "quoted" the buildLink type string

--- administration/admin.php
+++ administration/admin.php
@@ -34,16 +34,16 @@ if ($profilSession != "0") {
     phpCollab\Util::headerFunction('../general/permissiondenied.php');
 }
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], "in"));
 $blockPage->itemBreadcrumbs($strings["admin_intro"]);
 $blockPage->closeBreadcrumbs();
 
 if ($msg != "") {
-    include '../includes/messages.php';
+    include APP_ROOT . '/includes/messages.php';
     $blockPage->messageBox($msgLabel);
 }
 
@@ -53,31 +53,24 @@ $block1->heading($strings["administration"]);
 $block1->openContent();
 $block1->contentTitle($strings["admin_intro"]);
 
-$block1->contentRow("", $blockPage->buildLink("../users/listusers.php?", $strings["user_management"], in));
+$block1->contentRow("", $blockPage->buildLink("../users/listusers.php?", $strings["user_management"], "in"));
 
 if ($enableInvoicing == "true") {
-    $block1->contentRow("", $blockPage->buildLink("../services/listservices.php?", $strings["service_management"], in));
+    $block1->contentRow("", $blockPage->buildLink("../services/listservices.php?", $strings["service_management"], "in"));
 }
 
 if ($supportType == "admin") {
-    $block1->contentRow("", $blockPage->buildLink("../administration/support.php?", $strings["support_management"], in));
+    $block1->contentRow("", $blockPage->buildLink("../administration/support.php?", $strings["support_management"], "in"));
 }
 
 if ($databaseType == "mysql") {
-    $block1->contentRow("", $blockPage->buildLink("../administration/phpmyadmin.php?", $strings["database"], in));
+    $block1->contentRow("", $blockPage->buildLink("../administration/phpmyadmin.php?", $strings["database"], "in"));
 }
 
-/* disabled
-if ($databaseType == "postgresql")
-{
-    $block1->contentRow("",$blockPage->buildLink("../administration/phppgadmin.php?",$strings["database"],in));
-}
-*/
-
-$block1->contentRow("", $blockPage->buildLink("../administration/systeminfo.php?", $strings["system_information"], in));
-$block1->contentRow("", $blockPage->buildLink("../administration/mycompany.php?", $strings["company_details"], in));
-$block1->contentRow("", $blockPage->buildLink("../administration/listlogs.php?", $strings["logs"], in));
-$block1->contentRow($strings["update"] . $blockPage->printHelp("admin_update"), "1. " . $blockPage->buildLink("../administration/updatesettings.php?", $strings["edit_settings"], in) . " 2. " . $blockPage->buildLink("../administration/updatedatabase.php?", $strings["edit_database"], in));
+$block1->contentRow("", $blockPage->buildLink("../administration/systeminfo.php?", $strings["system_information"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../administration/mycompany.php?", $strings["company_details"], "in"));
+$block1->contentRow("", $blockPage->buildLink("../administration/listlogs.php?", $strings["logs"], "in"));
+$block1->contentRow($strings["update"] . $blockPage->printHelp("admin_update"), "1. " . $blockPage->buildLink("../administration/updatesettings.php?", $strings["edit_settings"], "in") . " 2. " . $blockPage->buildLink("../administration/updatedatabase.php?", $strings["edit_database"], "in"));
 
 if ($updateChecker == "true" && $installationType == "online") {
     $block1->contentRow("", phpCollab\Util::updateChecker($version));
@@ -94,4 +87,4 @@ if (file_exists("../installation/setup.php")) {
 
 $block1->closeContent();
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
