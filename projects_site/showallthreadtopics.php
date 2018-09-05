7d660847e1eec9ffb5167a4f4b2001f7512372fa
Jeff Sittler <mindblender@gmail.com>
2018-09-05 01:03:16 -0700

    HTML cleanup

--- projects_site/showallthreadtopics.php
+++ projects_site/showallthreadtopics.php
@@ -19,8 +19,15 @@ $block1 = new phpCollab\Block();
 $block1->heading($strings["bulletin_board"]);
 
 if ($comptListTopics != "0") {
-    echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\" cols=\"4\" class=\"listing\">
-<tr><th>" . $strings["topic"] . "</th><th>" . $strings["posts"] . "</th><th>" . $strings["owner"] . "</th><th class=\"active\">" . $strings["last_post"] . "</th></tr>";
+    echo <<<TABLE
+<table cellspacing="0" width="90%" border="0" cellpadding="3" cols="4" class="listing">
+    <tr>
+        <th>{$strings["topic"]}</th>
+        <th>{$strings["posts"]}</th>
+        <th>{$strings["owner"]}</th>
+        <th class="active">{$strings["last_post"]}</th>
+    </tr>
+TABLE;
 
     for ($i = 0; $i < $comptListTopics; $i++) {
         if (!($i % 2)) {
@@ -30,14 +37,25 @@ if ($comptListTopics != "0") {
             $class = "even";
             $highlightOff = $block1->getEvenColor();
         }
-        echo "<tr class=\"$class\" onmouseover=\"this.style.backgroundColor='" . $block1->getHighlightOn() . "'\" onmouseout=\"this.style.backgroundColor='" . $highlightOff . "'\"><td><a href=\"showallthreads.php?id=" . $listTopics->top_id[$i] . "\">" . $listTopics->top_subject[$i] . "</a></td><td>" . $listTopics->top_posts[$i] . "</td><td>" . $listTopics->top_mem_name[$i] . "</td><td>" . phpCollab\Util::createDate($listTopics->top_last_post[$i], $timezoneSession) . "</td></tr>";
+
+        $topicDate = phpCollab\Util::createDate($listTopics->top_last_post[$i], $timezoneSession);
+        echo <<< TR
+<tr class="{$class}" onmouseover="this.style.backgroundColor='{$block1->getHighlightOn()}'" onmouseout="this.style.backgroundColor='{$highlightOff}'">
+    <td><a href="showallthreads.php?id={$listTopics->top_id[$i]}">{$listTopics->top_subject[$i]}</a></td>
+    <td>{$listTopics->top_posts[$i]}</td>
+    <td>{$listTopics->top_mem_name[$i]}</td>
+    <td>{$topicDate}</td></tr>
+TR;
     }
-    echo '</table><hr />\n';
+    echo '</table><hr />';
 } else {
     echo '<table cellspacing="0" border="0" cellpadding="2"><tr><td colspan="4">' . $strings["no_items"] . '</td></tr></table><hr>';
 }
 
-echo "<br/><br/>
-<a href=\"createthread.php?\" class=\"FooterCell\">" . $strings["create_topic"] . "</a>";
+echo <<<CREATE
+<br><br>
+<a href="createthread.php?" class="FooterCell">{$strings["create_topic"]}</a>
+CREATE;
+
 
 include("include_footer.php");
