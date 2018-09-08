b1c736a6c91135a86b3faf6c9fcdec405a205a0a
Jeff Sittler <mindblender@gmail.com>
2018-09-07 20:04:08 -0700

    Changed loginForm to usernameForm Changed the validation to look for "Home Page" in the title

--- tests/acceptance/public/LoginSuccessCept.php
+++ tests/acceptance/public/LoginSuccessCept.php
@@ -1,11 +1,10 @@
 <?php
-$scenario->incomplete('your message, why skipping');
 $I = new AcceptanceTester($scenario);
 
 $I->wantTo('login');
 $I->amOnPage('/general/login.php');
-$I->fillField(['name' => 'loginForm'], 'testUser');
+$I->fillField(['name' => 'usernameForm'], 'testUser');
 $I->fillField(['name' => 'passwordForm'], 'testing');
 $I->click('input[type="submit"]');
 $I->seeInCurrentUrl('/general/home.php');
-$I->see('Home Page', '#header');
+$I->seeInTitle('Home Page');
