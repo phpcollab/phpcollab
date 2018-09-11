6f918d2dd055ca2420c7dbbfead0b213bd3969fb
Jeff Sittler <mindblender@gmail.com>
2018-09-10 19:41:12 -0700

    Added a try/catch block around the main code to catch if the file doesn't exist.

--- includes/download.php
+++ includes/download.php
@@ -16,30 +16,37 @@ if ($task == "0") {
     $path = $filespath . "/" . $project . "/" . $task . "/" . $name;
 }
 
-if (!file_exists($path)) {
-    throw new \Exception("file does not exist:-/");
+try {
+    if (!file_exists($path)) {
+        throw new \Exception("file does not exist");
+    } else {
+        // figure out mimetype, should be done using PHP mime.magic once it's out
+        $mimetype = $mimetypes[$fileDetail["fil_extension"]];
+
+        // Apache behaviour seems to send text/plain for unknown mimetypes so that's what we do, too
+        if ($mimetype == "") {
+            $mimetype = "text/plain";
+        }
+
+        // eval 'mode' parameter for either view or download
+        if ($mode == "download") {
+            header("Content-Length: " . filesize($path));
+            header("Content-Type: $mimetype");
+            header("Content-Disposition: attachment; filename=$name");
+        } elseif ($mode == "view") {
+            header("Content-Length: " . filesize($path));
+            header("Content-Type: $mimetype");
+            header("Content-Disposition: inline; filename=$name");
+            // Apache is sending Last Modified header, so we'll do it, too
+            $modified = filemtime($path);
+            header("Last-Modified: " . date("D, j M Y G:i:s T", $modified));    // something like Thu, 03 Oct 2002 18:01:08 GMT
+        }
+
+        # write file as response
+        readfile($path);
+
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
 
-// figure out mimetype, should be done using PHP mime.magic once it's out
-$mimetype = $mimetypes[$fileDetail["fil_extension"]];
-// Apache behaviour seems to send text/plain for unknown mimetypes so that's what we do, too
-if ($mimetype == "") {
-    $mimetype = "text/plain";
-}
-
-// eval 'mode' parameter for either view or download
-if ($mode == "download") {
-    header("Content-Length: " . filesize($path));
-    header("Content-Type: $mimetype");
-    header("Content-Disposition: attachment; filename=$name");
-} elseif ($mode == "view") {
-    header("Content-Length: " . filesize($path));
-    header("Content-Type: $mimetype");
-    header("Content-Disposition: inline; filename=$name");
-    // Apache is sending Last Modified header, so we'll do it, too
-    $modified = filemtime($path);
-    header("Last-Modified: " . date("D, j M Y G:i:s T", $modified));    // something like Thu, 03 Oct 2002 18:01:08 GMT
-}
-
-# write file as response
-readfile($path);
