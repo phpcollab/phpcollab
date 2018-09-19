c8b32252d14435880167682bb5b4b9b858e44671
Jeff Sittler <mindblender@gmail.com>
2018-09-18 18:31:06 -0700

    Refactored to use the Members and Support classes for retrieving data HTML code cleanup Added a check for blank subject and message fields and displays an error message accordingly

--- projects_site/addsupport.php
+++ projects_site/addsupport.php
@@ -5,53 +5,84 @@
 $checkSession = "true";
 include '../includes/library.php';
 
-$tmpquery = "WHERE mem.id = '$idSession'";
-$userDetail = new phpCollab\Request();
-$userDetail->openMembers($tmpquery);
+$members = new \phpCollab\Members\Members();
+$support = new \phpCollab\Support\Support();
 
-$tmpquery = "WHERE sr.member = '$idSession'";
-$listRequests = new phpCollab\Request();
-$listRequests->openSupportRequests($tmpquery);
-$comptListRequests = count($listRequests->sr_id);
+$userDetail = $members->getMemberById($idSession);
+
+$project = $_GET["project"];
+$priority = $GLOBALS["priority"];
 
 if ($action == "add") {
-    $sub = phpCollab\Util::convertData($sub);
-    $mes = phpCollab\Util::convertData($mes);
+    $request_priority = $_POST["priority"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $userId = $_POST["userId"];
+    $projectId = $_POST["projectId"];
 
-    $num = phpCollab\Util::newConnectSql(
-        "INSERT INTO {$tableCollab["support_requests"]} (member,priority,subject,message,date_open,project,status) VALUES(:member,:priority,:subject,:message,:date_open,:project,:status)",
-        ["member" => $user,"priority" => $pr,"subject" => $sub,"message" => $mes,"date_open" => $dateheure,"project" => $project,"status" => 0]
-    );
+    if (empty($subject) || empty($message)) {
+        $errorMessage = "Please enter a subject and message";
+    } else {
+        $supportRequestId = $support->addSupportRequest($userId, $request_priority, $subject, $message, $projectId);
 
-    if ($notifications == "true") {
-        include '../support/noti_newrequest.php';
+        if ($notifications == "true") {
+            include '../support/noti_newrequest.php';
+        }
+
+        phpCollab\Util::headerFunction("suprequestdetail.php?id=$supportRequestId");
     }
-
-    phpCollab\Util::headerFunction("suprequestdetail.php?id=$num");
 }
 
 $bouton[6] = "over";
 $titlePage = $strings["support"];
 include 'include_header.php';
 
-echo "<form accept-charset='UNKNOWN' method='POST' action='../projects_site/addsupport.php?action=add&project=$projectSession#filedetailsAnchor' name='addsupport' enctype='multipart/form-data' >";
+if (isset($errorMessage) && !empty($errorMessage)) {
+    echo <<<ERROR
+<div style="margin: 2rem; color: firebrick; font-size: 1rem;">{$errorMessage}</div>
+ERROR;
+}
 
-echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\">
-<tr><th colspan=\"2\">" . $strings["add_support_request"] . "</th></tr>
-<tr><th>" . $strings["priority"] . " :</th><td><select name=\"pr\">";
+echo <<<STARTFORM
+<form method="POST" action="../projects_site/addsupport.php?action=add&project={$projectSession}#filedetailsAnchor" name="addsupport">
+STARTFORM;
 
-$comptPri = count($priority);
-for ($i = 0; $i < $comptPri; $i++) {
-    if ($i != 0) {
-        echo "<option value=\"$i\">$priority[$i]</option>";
+echo <<<TABLE
+<table cellspacing="0" width="90%" border="0" cellpadding="3">
+    <tr><th colspan="2">{$strings["add_support_request"]}</th></tr>
+    <tr>
+        <th>{$strings["priority"]} :</th>
+        <td><select name="priority">
+TABLE;
+
+foreach ($priority as $key => $param) {
+    if ($key != 0) {
+        echo '<option value="' . $key . '">' . $param . '</option>';
     }
 }
-echo "</select></td></tr>
-<tr><th>" . $strings["subject"] . "</th><td><input size=\"32\" value=\"$sub\" style=\"width: 250px\" name=\"sub\" maxlength=\"32\" type=\"TEXT\"></td></tr>
-<tr><th>" . $strings["message"] . "</th><td><textarea rows=\"3\" style=\"width: 400px; height: 200px;\" name=\"mes\" cols=\"43\">$mes</textarea></td></tr>
-<input type=\"hidden\" name=\"user\" value=\"$idSession\">
-<tr><th>&nbsp;</th><td><input type=\"SUBMIT\" value=\"" . $strings["submit"] . "\"></td></tr>
+
+echo <<<CLOSETABLE
+        </select></td>
+    </tr>
+    <tr>
+        <th>{$strings["subject"]}</th>
+        <td><input size="32" value="{$subject}" style="width: 250px" name="subject" maxlength="64" type="text"></td>
+    </tr>
+    <tr>
+        <th>{$strings["message"]}</th>
+        <td><textarea rows="3" style="width: 400px; height: 200px;" name="message" cols="43">{$message}</textarea></td>
+    </tr>
+    <tr>
+        <th>&nbsp;</th>
+        <td>
+            <input type="submit" value="{$strings["submit"]}">
+            <input type="hidden" name="userId" value="{$idSession}">
+            <input type="hidden" name="projectId" value="{$project}">
+        </td>
+    </tr>
 </table>
-</form>";
+</form>
+CLOSETABLE;
+
 
 include("include_footer.php");
