489605757c15878c48190815be8539598efd752a
Jeff Sittler <mindblender@gmail.com>
2019-01-02 19:24:49 -0700

    Renamed $id to $userId and updated references. Swapped the parameters around for the getOrganizationByIdAndOwner call Changed $valueTasks to get values from Tasks class Added APP_ROOT to the theme includes

--- users/viewclientuser.php
+++ users/viewclientuser.php
@@ -33,16 +33,16 @@ include_once '../includes/library.php';
 
 $members = new \phpCollab\Members\Members();
 $organizations = new \phpCollab\Organizations\Organizations();
+$tasks = new \phpCollab\Tasks\Tasks();
 
 $orgId = $_GET['organization'];
-$id = $_GET['id'];
+$userId = $_GET['id'];
 
-if (empty($id) || empty($orgId)) {
+if (empty($userId) || empty($orgId)) {
     phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
 }
 
-$userDetail = $members->getMemberById($id);
-
+$userDetail = $members->getMemberById($userId);
 if (empty($userDetail)) {
     phpCollab\Util::headerFunction("../clients/viewclient.php?msg=blankUser&id=$orgId");
 }
@@ -56,12 +56,11 @@ if ($clientsFilter == "true" && $profilSession == "2") {
     $teamMember = "false";
 
     $memberTest = $teams->getTeamByTeamMemberAndOrgId($idSession, $memberOrganization);
-
     if (empty($memberTest)) {
         phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
     }
 } elseif ($clientsFilter == "true" && $profilSession == "1") {
-    $detailClient = $organizations->getOrganizationByIdAndOwner($idSession, $orgId);
+    $detailClient = $organizations->getOrganizationByIdAndOwner($orgId, $idSession);
 } else {
     $detailClient = $organizations->getOrganizationById($orgId);
 }
@@ -72,12 +71,12 @@ if (empty($detailClient)) {
     phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
 }
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/listclients.php?", $strings["clients"], in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=$orgId", $detailClient['org_name'], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/listclients.php?", $strings["clients"], "in"));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=$orgId", $detailClient['org_name'], "in"));
 $blockPage->itemBreadcrumbs($userDetail['mem_login']);
 $blockPage->closeBreadcrumbs();
 
@@ -86,7 +85,7 @@ $block1 = new phpCollab\Block();
 $block1->form = "cuserD";
 $block1->openForm("../users/viewclientuser.php#" . $block1->form . "Anchor");
 
-if ($error != "") {
+if (isset($error) && $error != "") {
     $block1->headingError($strings["errors"]);
     $block1->contentError($error);
 }
@@ -118,9 +117,7 @@ $block1->contentRow($strings["last_page"], $userDetail['mem_last_page']);
 
 $block1->contentTitle($strings["information"]);
 
-// Refactor
-$tmpquery = "SELECT tas.id FROM {$tableCollab["tasks"]} tas LEFT OUTER JOIN {$tableCollab["projects"]} pro ON pro.id = tas.project WHERE tas.assigned_to = '" . $userDetail['mem_id'] . "' AND tas.status IN(0,2,3) AND pro.status IN(0,2,3)";
-$valueTasks = phpCollab\Util::computeTotal($tmpquery);
+$valueTasks = $tasks->getClientUserTasks($userDetail['mem_id']);
 
 $block1->contentRow($strings["tasks"], $valueTasks);
 
@@ -137,10 +134,10 @@ $block1->closeForm();
 
 $block1->openPaletteScript();
 if ($profilSession == "0" || $profilSession == "1") {
-    $block1->paletteScript(0, "remove", "../users/deleteclientusers.php?id=$id&organization=$orgId", "true,true,true", $strings["delete"]);
-    $block1->paletteScript(1, "edit", "../users/updateclientuser.php?id=$id&organization=$orgId", "true,true,true", $strings["edit"]);
+    $block1->paletteScript(0, "remove", "../users/deleteclientusers.php?id=$userId&organization=$orgId", "true,true,true", $strings["delete"]);
+    $block1->paletteScript(1, "edit", "../users/updateclientuser.php?id=$userId&organization=$orgId", "true,true,true", $strings["edit"]);
 }
-$block1->paletteScript(2, "export", "../users/exportuser.php?id=$id", "true,true,true", $strings["export"]);
-$block1->closePaletteScript("", "");
+$block1->paletteScript(2, "export", "../users/exportuser.php?id=$userId", "true,true,true", $strings["export"]);
+$block1->closePaletteScript("", []);
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
