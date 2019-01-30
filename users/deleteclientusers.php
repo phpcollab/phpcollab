0f556854c2813eb438140ec1d5cb1658bbe1bf33
Jeff Sittler <mindblender@gmail.com>
2019-01-29 18:18:32 -0700

    Refactored to use class methods and to clean up HTML

--- users/deleteclientusers.php
+++ users/deleteclientusers.php
@@ -1,14 +1,18 @@
 <?php
-#Application name: PhpCollab
-#Status page: 0
-#Path by root: ../users/deleteclientusers.php
 
 $checkSession = "true";
 include_once '../includes/library.php';
-$tmpquery = "WHERE org.id = '$organization'";
-$detailOrganization = new phpCollab\Request();
-$detailOrganization->openOrganizations($tmpquery);
-$comptDetailOrganization = count($detailOrganization->org_id);
+
+$org_id = $_GET["orgid"];
+$user_id = $_GET["id"];
+
+if (empty($user_id) || empty($org_id)) {
+    phpCollab\Util::headerFunction("../clients/listclients.php?msg=blankClient");
+}
+
+$organizations = new \phpCollab\Organizations\Organizations();
+
+$detailOrganization = $organizations->getOrganizationById($org_id);
 
 $members = new \phpCollab\Members\Members();
 $teams = new \phpCollab\Teams\Teams();
@@ -16,29 +20,36 @@ $tasks = new \phpCollab\Tasks\Tasks();
 $assignments = new \phpCollab\Assignments\Assignments();
 $notifications = new \phpCollab\Notifications\Notifications();
 
-if ($action == "delete") {
-    $id = str_replace("**", ",", $id);
-    $members->deleteMemberByIdIn($id);
-    $tasks->setTasksAssignedToWhereAssignedToIn($at, $id);
-    $assignments->reassignAssignmentByAssignedTo($at, $dateheure, $id);
-    $notifications->deleteNotificationsByMemberIdIn($id);
-    $teams->deleteTeamWhereMemberIn($id);
+if ($_GET["action"] == "delete") {
+    if ($_POST["id"]) {
+        $id = str_replace("**", ",", $_POST["id"]);
 
-    //if mantis bug tracker enabled
-    if ($enableMantis == "true") {
-        // Call mantis function to remove user
-        include("../mantis/user_delete.php");
+        if (isset( $_POST["assign_to"]) ) {
+            $tasks->setTasksAssignedToWhereAssignedToIn($_POST["assign_to"], $id);
+            $assignments->reassignAssignmentByAssignedTo($_POST["assign_to"], $dateheure, $id);
+        }
+
+        $notifications->deleteNotificationsByMemberIdIn($id);
+        $teams->deleteTeamWhereMemberIn($id);
+
+        $members->deleteMemberByIdIn($id);
+
+        //if mantis bug tracker enabled
+        if ($enableMantis == "true") {
+            // Call mantis function to remove user
+            include("../mantis/user_delete.php");
+        }
+
+        phpCollab\Util::headerFunction("../clients/viewclient.php?id=$org_id&msg=delete");
     }
-
-    phpCollab\Util::headerFunction("../clients/viewclient.php?id=$organization&msg=delete");
 }
 
 include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/listclients.php?", $strings["clients"], in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=" . $detailOrganization->org_id[0], $detailOrganization->org_name[0], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/listclients.php?", $strings["clients"], 'in'));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../clients/viewclient.php?id=" . $detailOrganization["org_id"], $detailOrganization["org_name"], 'in'));
 $blockPage->itemBreadcrumbs($strings["delete_users"]);
 $blockPage->closeBreadcrumbs();
 
@@ -50,7 +61,7 @@ if ($msg != "") {
 $block1 = new phpCollab\Block();
 
 $block1->form = "client_user_delete";
-$block1->openForm("../users/deleteclientusers.php?organization=$organization&action=delete");
+$block1->openForm("../users/deleteclientusers.php?orgid=$org_id&action=delete");
 
 $block1->heading($strings["delete_users"]);
 
@@ -58,40 +69,58 @@ $block1->openContent();
 $block1->contentTitle($strings["delete_following"]);
 
 $id = str_replace("**", ",", $id);
-$tmpquery = "WHERE mem.id IN($id) ORDER BY mem.name";
 
-$listMembers = new phpCollab\Request();
-$listMembers->openMembers($tmpquery);
-$comptListMembers = count($listMembers->mem_id);
+$listMembers = $members->getMembersByIdIn($id, "mem.name");
 
-for ($i = 0; $i < $comptListMembers; $i++) {
-    echo "<tr class='odd'><td valign='top' class='leftvalue'>&nbsp;</td><td>" . $listMembers->mem_login[$i] . "&nbsp;(" . $listMembers->mem_name[$i] . ")</td></tr>";
+foreach ($listMembers as $listMember) {
+    echo <<< HTML
+<tr class="odd">
+    <td valign="top" class="leftvalue">&nbsp;</td>
+    <td>{$listMember["mem_login"]} ({$listMember["mem_name"]})</td>
+</tr>
+HTML;
 }
 
-$tmpquery = "SELECT tas.id FROM {$tableCollab["tasks"]} tas WHERE tas.assigned_to IN($id)";
-phpCollab\Util::computeTotal($tmpquery);
-$totalTasks = $countEnregTotal;
+$totalTasks = $tasks->getClientUserTasksCount($id);
 
-$block1->contentTitle($strings["reassignment_clientuser"]);
+/**
+ * If there are tasks, then display the select member to re-assign to
+ * If no tasks, then skip
+ */
+if ($totalTasks) {
+    $block1->contentTitle($strings["reassignment_clientuser"]);
+    echo <<<HTML
+    <tr class="odd">
+        <td valign="top" class="leftvalue">&nbsp;</td>
+        <td>{$strings["there"]} $totalTasks {$strings["tasks"]} {$strings["owned_by"]}</td>
+    </tr>
+    <tr class="odd">
+        <td valign="top" class="leftvalue">&nbsp;</td>
+        <td><b>{$strings["reassign_to"]} : </b>
+HTML;
 
-echo "<tr class='odd'><td valign='top' class='leftvalue'>&nbsp;</td><td>" . $strings["there"] . " $totalTasks " . $strings["tasks"] . " " . $strings["owned_by"] . "</td></tr>
-<tr class='odd'><td valign='top' class='leftvalue'>&nbsp;</td><td><b>" . $strings["reassign_to"] . " : </b> ";
+    $reassign = $members->getNonClientMembersExcept($user_id);
+    echo <<<HTML
+    <select name="assign_to">
+        <option value="0" selected>{$strings["unassigned"]}</option>
+HTML;
 
-$tmpquery = "WHERE mem.profil != '3' ORDER BY mem.name";
-$reassign = new phpCollab\Request();
-$reassign->openMembers($tmpquery);
-$comptReassign = count($reassign->mem_id);
-
-echo "<select name='at'>
-<option value='0' selected>" . $strings["unassigned"] . "</option>";
-
-for ($i = 0; $i < $comptReassign; $i++) {
-    echo "<option value='" . $reassign->mem_id[$i] . "'>" . $reassign->mem_login[$i] . " / " . $reassign->mem_name[$i] . "</option>";
+foreach ($reassign as $item) {
+    echo '<option value="' . $item["mem_id"] . '">' . $item["mem_login"] . ' / ' . $item["mem_name"] . '</option>';
 }
 
-echo "</select></td></tr>
+echo <<<HTML
+    </select></td>
+</tr>
+HTML;
+}
+echo <<<HTML
+<tr class="odd">
+    <td valign="top" class="leftvalue">&nbsp;</td>
+    <td><input type="submit" name="delete" value="{$strings["delete"]}"> <input type="button" name="cancel" value="{$strings["cancel"]}" onClick="history.back();"><input type="hidden" value="$id" name="id"></td>
+</tr>
+HTML;
 
-<tr class='odd'><td valign='top' class='leftvalue'>&nbsp;</td><td><input type='submit' name='delete' value='" . $strings["delete"] . "'> <input type='button' name='cancel' value='" . $strings["cancel"] . "' onClick='history.back();'><input type='hidden' value='$id' name='id'></td></tr>";
 
 $block1->closeContent();
 $block1->closeForm();
