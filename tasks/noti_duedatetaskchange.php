87f3ec5de7537455d8df0c87cd96e28d1fd95687
Jeff Sittler <mindblender@gmail.com>
2019-03-07 02:43:25 -0700

    Complete refactor to use updated Notification class

--- tasks/noti_duedatetaskchange.php
+++ tasks/noti_duedatetaskchange.php
@@ -1,52 +1,69 @@
 <?php
-$tmpquery = "WHERE tas.id IN($id)";
-$taskNoti = new phpCollab\Request();
-$taskNoti->openTasks($tmpquery);
+$notificationsClass = new \phpCollab\Notifications\Notifications();
 
-$tmpquery = "WHERE pro.id = '$project'";
-$projectNoti = new phpCollab\Request();
-$projectNoti->openProjects($tmpquery);
+$taskNoti = $tasks->getTaskById($listTask["tas_id"]);
 
-$tmpquery = "WHERE noti.member IN($at)";
-$listNotifications = new phpCollab\Request();
-$listNotifications->openNotifications($tmpquery);
-$comptListNotifications = count($listNotifications->not_id);
+$projectNoti = $projects->getProjectById($listTask["tas_project"]);
 
-if ($listNotifications->not_statustaskchange[0] == "0") {
-    $mail = new phpCollab\Notification();
+$task_owner = $listTask["tas_owner"];
 
-    $mail->getUserinfo($idSession, "from");
+$listNotifications = $notificationsClass->getMemberNotifications($task_owner);
 
-    $mail->partSubject = $strings["noti_duedatetaskchange1"];
-    $mail->partMessage = $strings["noti_duedatetaskchange2"];
+if ($listNotifications["duedateTaskChange"] == "0") {
+    $mail = new phpCollab\Notification(true);
 
-    if ($projectNoti->pro_org_id[0] == "1") {
-        $projectNoti->pro_org_name[0] = $strings["none"];
+    try {
+        $mail->getUserinfo($idSession, "from");
+
+        $mail->partSubject = $strings["noti_duedatetaskchange1"];
+        $mail->partMessage = $strings["noti_duedatetaskchange2"];
+
+        if ($projectNoti["pro_org_id"] == "1") {
+            $projectNoti["pro_org_name"] = $strings["none"];
+        }
+
+        $complValue = ($taskNoti["tas_completion"] > 0) ? $taskNoti["tas_completion"] . "0 %" : $taskNoti["tas_completion"] . " %";
+        $idStatus = $taskNoti["tas_status"];
+        $idPriority = $taskNoti["tas_priority"];
+
+        $body = $mail->partMessage . "\n\n";
+        $body .= $strings["task"] . " : " . $taskNoti["tas_name"] . "\n";
+        $body .= $strings["start_date"] . " : " . $taskNoti["tas_start_date"] . "\n";
+        $body .= $strings["due_date"] . " : " . $taskNoti["tas_due_date"] . "\n";
+        $body .= $strings["completion"] . " : " . $complValue . "\n";
+        $body .= $strings["priority"] . " : " . $GLOBALS["priority"][$idPriority] . "\n";
+        $body .= $strings["status"] . " : " . $GLOBALS["status"][$idStatus] . "\n";
+        $body .= $strings["description"] . " : " . $taskNoti["tas_description"] . "\n\n";
+        $body .= $strings["project"] . " : " . $projectNoti["pro_name"] . " (" . $projectNoti["pro_id"] . ")\n";
+        $body .= $strings["organization"] . " : " . $projectNoti["pro_org_name"] . "\n\n";
+        $body .= $strings["noti_moreinfo"] . "\n";
+
+
+        if ($taskNoti["tas_mem_organization"] == "1") {
+            $body .= "$root/general/login.php?url=tasks/viewtask.php%3Fid=$id";
+        } elseif ($taskNoti["tas_mem_organization"] != "1" && $projectNoti["pro_published"] == "0" && $taskNoti["tas_published"] == "0") {
+            $body .= "$root/general/login.php?url=projects_site/home.php%3Fproject=" . $projectNoti["pro_id"];
+        }
+
+        $body .= "\n\n" . $mail->footer;
+
+        $subject = $mail->partSubject . " " . $taskNoti["tas_name"];
+
+        $mail->Subject = $subject;
+
+        if ($taskNoti["tas_priority"] == "4" || $taskNoti["tas_priority"] == "5") {
+            $mail->Priority = "1";
+        } else {
+            $mail->Priority = "3";
+        }
+
+        $mail->addAddress($listNotifications["email_work"], $listNotifications["name"]);
+
+        $mail->Body = $body;
+        $mail->send();
+        $mail->clearAddresses();
+
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
-    $complValue = ($taskNoti->tas_completion[0] > 0) ? $taskNoti->tas_completion[0] . "0 %" : $taskNoti->tas_completion[0] . " %";
-    $idStatus = $taskNoti->tas_status[0];
-    $idPriority = $taskNoti->tas_priority[0];
-
-    $body = $mail->partMessage . "\n\n" . $strings["task"] . " : " . $taskNoti->tas_name[0] . "\n" . $strings["start_date"] . " : " . $taskNoti->tas_start_date[0] . "\n" . $strings["due_date"] . " : " . $taskNoti->tas_due_date[0] . "\n" . $strings["completion"] . " : " . $complValue . "\n" . $strings["priority"] . " : $priority[$idPriority]\n" . $strings["status"] . " : $status[$idStatus]\n" . $strings["description"] . " : " . $taskNoti->tas_description[0] . "\n\n" . $strings["project"] . " : " . $projectNoti->pro_name[0] . " (" . $projectNoti->pro_id[0] . ")\n" . $strings["organization"] . " : " . $projectNoti->pro_org_name[0] . "\n\n" . $strings["noti_moreinfo"] . "\n";
-
-    if ($taskNoti->tas_mem_organization[0] == "1") {
-        $body .= "$root/general/login.php?url=tasks/viewtask.php%3Fid=$id";
-    } elseif ($taskNoti->tas_mem_organization[0] != "1" && $projectNoti->pro_published[0] == "0" && $taskNoti->tas_published[0] == "0") {
-        $body .= "$root/general/login.php?url=projects_site/home.php%3Fproject=" . $projectNoti->pro_id[0];
-    }
-
-    $body .= "\n\n" . $mail->footer;
-
-    $subject = $mail->partSubject . " " . $taskNoti->tas_name[0];
-
-    $mail->Subject = $subject;
-    if ($taskNoti->tas_priority[0] == "4" || $taskNoti->tas_priority[0] == "5") {
-        $mail->Priority = "1";
-    } else {
-        $mail->Priority = "3";
-    }
-    $mail->Body = $body;
-    $mail->AddAddress($listNotifications->not_mem_email_work[0], $listNotifications->not_mem_name[0]);
-    $mail->Send();
-    $mail->ClearAddresses();
 }
