4f1f65ec716078914c24cf7141a24caf390304e8
Jeff Sittler <mindblender@gmail.com>
2019-03-07 02:19:34 -0700

    Updated to use new phpMailer class and cleaned up some of the logic

--- classes/Notification.php
+++ classes/Notification.php
@@ -1,87 +1,64 @@
 <?php
+
 namespace phpCollab;
 
-use \phpmailer;
-
-/*
-** Application name: phpCollab
-** Last Edit page: 26/01/2004 
-** Path by root: ../includes/notification.class.php
-** Authors: Ceam / Fullo 
-**
-** =============================================================================
-**
-**               phpCollab - Project Managment 
-**
-** -----------------------------------------------------------------------------
-** Please refer to license, copyright, and credits in README.TXT
-**
-** -----------------------------------------------------------------------------
-** FILE: notification.class.php
-**
-** DESC: Screen: notification class
-**
-** HISTORY:
-** 	26/01/2004	-	added file comment
-**	03/08/2005	-	added language file for phpMailer
-** -----------------------------------------------------------------------------
-** TO-DO:
-** 
-**
-** =============================================================================
-*/
-
-define('APP_ROOT', dirname(dirname(__FILE__)));
-
-require(APP_ROOT . "/includes/phpmailer/class.phpmailer.php");
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use phpCollab\Members\Members;
 
 class Notification extends phpmailer
 {
+    private $lang;
+    private $notificationMethod;
+    private $strings;
+    private $root;
+    public $partMessage;
+    public $footer;
+    public $partSubject;
 
-    function notification()
+    public function __construct($exceptions = null)
     {
-        global $strings, $root, $notificationMethod, $lang;
+        parent::__construct($exceptions);
+        $this->lang = $GLOBALS["lang"];
+        $this->notificationMethod = $GLOBALS["notificationMethod"];
+        $this->strings = $GLOBALS["strings"];
+        $this->root = $GLOBALS["root"];
 
-        if (file_exists(
-            APP_ROOT . "/includes/phpmailer/language/phpmailer.lang-$lang.php"
-        )) {
-            $phpmailer_lang = $lang;
-        } else {
-            $phpmailer_lang = 'en';
-        }
-
-
-        $this->Mailer = $notificationMethod;
-        $this->PluginDir = APP_ROOT . "/includes/phpmailer/";
-        $this->SetLanguage(
-            $phpmailer_lang, APP_ROOT . "/includes/phpmailer/language/"
-        );
+        $this->Mailer = $this->notificationMethod;
+        $this->SetLanguage($this->lang);
 
         if ($this->Mailer == "smtp") {
+            $this->isSMTP();
             $this->Host = SMTPSERVER;
-            $this->Username = SMTPLOGIN;
-            $this->Password = SMTPPASSWORD;
+            $this->Port = 25;                                   // TCP port to connect to
+            $this->SMTPAuth = false;
+            if (!empty(SMTPLOGIN && !empty(SMTPPASSWORD))) {
+                $this->SMTPAuth = true;
+                $this->Username = SMTPLOGIN;                    // SMTP username
+                $this->Password = SMTPPASSWORD;                 // SMTP password
+//                $this->SMTPSecure = 'tls';                      // Enable TLS encryption, `ssl` also accepted
+            }
         }
 
-        $this->footer = "--\n" . $strings["noti_foot1"] . "\n\n"
-            . $strings["noti_foot2"] . "\n$root/";
+        $this->footer = "--\n" . $this->strings["noti_foot1"] . "\n\n" . $this->strings["noti_foot2"] . "\n$this->root/";
+
     }
 
     function getUserinfo($idUser, $type)
     {
-        $tmpquery = "WHERE mem.id = '$idUser'";
-        $detailUser = new \phpCollab\Request();
-        $detailUser->openMembers($tmpquery);
+        $detailUser = (new Members)->getMemberById($idUser);
+        try {
+            if ($type == "from") {
+                $this->setFrom($detailUser["mem_email_work"], $detailUser["mem_name"]);
+            }
 
-        if ($type == "from") {
-            $this->From = $detailUser->mem_email_work[0];
-            $this->FromName = $detailUser->mem_name[0];
-        }
-
-        if ($type == "to") {
-            $this->AddAddress(
-                $detailUser->mem_email_work[0], $detailUser->mem_name[0]
-            );
+            if ($type == "to") {
+                $this->AddAddress(
+                    $detailUser["mem_email_work"], $detailUser["mem_name"]
+                );
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
         }
     }
 
