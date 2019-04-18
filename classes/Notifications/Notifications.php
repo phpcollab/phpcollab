d516645c3395477055d2451857a895f6fb5ffb7a
Jeff Sittler <mindblender@gmail.com>
2019-04-18 02:41:35 -0700

    Added setAlerts method

--- classes/Notifications/Notifications.php
+++ classes/Notifications/Notifications.php
@@ -64,4 +64,50 @@ class Notifications
     {
         $this->notifications_gateway->deleteNotificationsByMemberIdIn($memberId);
     }
+
+    /**
+     * Sets the various flags for notifications and alerts
+     * @param $memberId
+     * @param $taskAssignment
+     * @param $removeProjectTeam
+     * @param $addProjectTeam
+     * @param $newTopic
+     * @param $newPost
+     * @param $statusTaskChange
+     * @param $priorityTaskChange
+     * @param $dueDateTaskChange
+     * @param $clientAddTask
+     * @param $uploadFile
+     * @param $dailyAlert
+     * @param $weeklyAlert
+     * @param $pastDueAlert
+     * @return mixed
+     */
+    public function setAlerts($memberId, $taskAssignment, $removeProjectTeam, $addProjectTeam, $newTopic, $newPost,
+                              $statusTaskChange, $priorityTaskChange, $dueDateTaskChange, $clientAddTask,
+                              $uploadFile, $dailyAlert, $weeklyAlert, $pastDueAlert)
+    {
+        // Gather all the attributes into an array
+        $arr = get_defined_vars();
+
+        // Remove $memberId from the array
+        unset($arr['memberId']);
+
+        // This loops through the array of arguments and sets their default value if they are null.
+        // Loop through each of the arguments and set it to 1 or 0 .
+        foreach ($arr as $key => $value) {
+            if (array_key_exists($key, $arr)) {
+                if (is_null($value) || $value === '1') {
+                    ${$key} = 1;
+                } else if ($value === 'true') {
+                    ${$key} = 0;
+                }
+            }
+        }
+
+        return $this->notifications_gateway->updateAlerts($memberId, $taskAssignment, $removeProjectTeam,
+                $addProjectTeam, $newTopic, $newPost, $statusTaskChange, $priorityTaskChange, $dueDateTaskChange,
+                $clientAddTask, $uploadFile, $dailyAlert, $weeklyAlert, $pastDueAlert);
+    }
+
 }
