6e7a8e80cfa4f1a2d0abbd2884cef50b32aa19ca
Jeff Sittler <mindblender@gmail.com>
2019-04-18 02:55:13 -0700

    Changed to use Members class methods for retrieving the data.  Still need to refactor the vcard functionality since it is broken.

--- users/exportuser.php
+++ users/exportuser.php
@@ -3,6 +3,8 @@
 #Status page: 0
 #Path by root: ../users/exportuser.php
 
+use phpCollab\Members\Members;
+
 $export = "true";
 
 $checkSession = "false";
@@ -10,29 +12,29 @@ include_once '../includes/library.php';
 
 include '../includes/vcard.class.php';
 
-$tmpquery = "WHERE mem.id = '$id'";
-$userDetail = new phpCollab\Request();
-$userDetail->openMembers($tmpquery);
+$members = new Members();
+
+$userDetail = $members->getMemberById($id);
 
 $v = new vCard();
 
-$v->setPhoneNumber($userDetail->mem_phone_work[0]);
+$v->setPhoneNumber($userDetail["mem_phone_work"]);
 
-$v->setName($userDetail->mem_name[0]);
+$v->setName($userDetail["mem_name"]);
 
-$v->setTitle($userDetail->mem_title[0]);
+$v->setTitle($userDetail["mem_title"]);
 
-$v->setOrganization($userDetail->mem_org_name[0]);
+$v->setOrganization($userDetail["mem_org_name"]);
 
-$v->setEmail($userDetail->mem_email_work[0]);
+$v->setEmail($userDetail["mem_email_work"]);
 
-$v->setPhoneNumber($userDetail->mem_phone_work[0], "WORK;VOICE");
+$v->setPhoneNumber($userDetail["mem_phone_work"], "WORK;VOICE");
 
-$v->setPhoneNumber($userDetail->mem_phone_home[0], "HOME;VOICE");
+$v->setPhoneNumber($userDetail["mem_phone_home"], "HOME;VOICE");
 
-$v->setPhoneNumber($userDetail->mem_mobile[0], "CELL;VOICE");
+$v->setPhoneNumber($userDetail["mem_mobile"], "CELL;VOICE");
 
-$v->setPhoneNumber($userDetail->mem_fax[0], "WORK;FAX");
+$v->setPhoneNumber($userDetail["mem_fax"], "WORK;FAX");
 
 $output = $v->getVCard();
 $filename = $v->getFileName();
