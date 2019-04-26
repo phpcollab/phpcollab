32a917aeb8c7a1ae549d41a90b362a66fc5b8013
Jeff Sittler <mindblender@gmail.com>
2019-04-25 19:06:11 -0700

    Code clean up Refactored to use class methods

--- projects_site/showallsupport.php
+++ projects_site/showallsupport.php
@@ -8,66 +8,86 @@ use phpCollab\Support\Support;
 $checkSession = "true";
 include '../includes/library.php';
 
+$members = new Members();
+$support = new Support();
+
 $bouton[6] = "over";
 $titlePage = $strings["support"];
 include 'include_header.php';
 
-$tmpquery = "WHERE mem.id = '$idSession'";
-$userDetail = new phpCollab\Request();
-$userDetail->openMembers($tmpquery);
+$userDetail = $members->getMemberById($idSession);
 
-$tmpquery = "WHERE sr.member = '$idSession' AND sr.project = '$project'";
-$listRequests = new phpCollab\Request();
-$listRequests->openSupportRequests($tmpquery);
-$comptListRequests = count($listRequests->sr_id);
+$listRequests = $support->getSupportRequestByMemberIdAndProjectId($idSession, $project);
 
 $block1 = new phpCollab\Block();
 
 $block1->heading($strings["my_support_request"]);
 
-if ($comptListRequests != "0") {
-    echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\" cols=\"4\" class=\"listing\">
-<tr><th class=\"active\">" . $strings["id"] . "</th><th>" . $strings["subject"] . "</th><th>" . $strings["priority"] . "</th><th>" . $strings["status"] . "</th><th>" . $strings["project"] . "</th><th>" . $strings["date_open"] . "</th><th>" . $strings["date_close"] . "</th></tr>";
+if ($listRequests) {
+    echo <<<TABLE
+        <table style="width: 90%;" class="listing">
+            <tr>
+                <th class="active">{$strings["id"]}</th>
+                <th>{$strings["subject"]}</th>
+                <th>{$strings["priority"]}</th>
+                <th>{$strings["status"]}</th>
+                <th>{$strings["project"]}</th>
+                <th>{$strings["date_open"]}</th>
+                <th>{$strings["date_close"]}</th>
+            </tr>
+TABLE;
 
-    for ($i = 0; $i < $comptListRequests; $i++) {
+    foreach ($listRequests as $listRequest) {
         if (!($i % 2)) {
             $class = "odd";
             $highlightOff = $block1->getOddColor();
+            $highlightOn = $block1->getEvenColor();
         } else {
             $class = "even";
             $highlightOff = $block1->getEvenColor();
+            $highlightOn = $block1->getOddColor();
         }
 
         $comptSta = count($requestStatus);
         for ($sr = 0; $sr < $comptSta; $sr++) {
-            if ($listRequests->sr_status[$i] == $sr) {
+            if ($listRequest["sr_status"] == $sr) {
                 $currentStatus = $requestStatus[$sr];
             }
         }
 
         $comptPri = count($priority);
         for ($rp = 0; $rp < $comptPri; $rp++) {
-            if ($listRequests->sr_priority[$i] == $rp) {
+            if ($listRequest["sr_priority"] == $rp) {
                 $requestPriority = $priority[$rp];
             }
         }
 
-        echo "<tr class=\"$class\" onmouseover=\"this.style.backgroundColor='" . $block1->getHighlightOn() . "'\" onmouseout=\"this.style.backgroundColor='" . $highlightOff . "'\">
-<td>" . $listRequests->sr_id[$i] . "</td>
-<td><a href=\"suprequestdetail.php?id=" . $listRequests->sr_id[$i] . "\">" . $listRequests->sr_subject[$i] . "</a></td>
-<td>$requestPriority</td>
-<td>$currentStatus</td>
-<td>" . $listRequests->sr_project[$i] . "</td>
-<td>" . $listRequests->sr_date_open[$i] . "</td>
-<td>" . $listRequests->sr_date_close[$i] . "</td>
-</tr>";
+        echo <<<TABLE_ROW
+            <tr class="$class" 
+                onmouseover="this.style.backgroundColor='{$highlightOn}'" 
+                onmouseout="this.style.backgroundColor='{$highlightOff}'">
+                <td>{$listRequest["sr_id"]}</td>
+                <td><a href="suprequestdetail.php?id={$listRequest["sr_id"]}">{$listRequest["sr_subject"]}</a></td>
+                <td>{$requestPriority}</td>
+                <td>{$currentStatus}</td>
+                <td>{$listRequest["sr_project"]}</td>
+                <td>{$listRequest["sr_date_open"]}</td>
+                <td>{$listRequest["sr_date_close"]}</td>
+            </tr>
+TABLE_ROW;
+
+
     }
-    echo "</table>
-<hr />\n";
+    echo "</table><hr />\n";
 } else {
-    echo "<table cellspacing=\"0\" border=\"0\" cellpadding=\"2\"><tr><td colspan=\"4\">" . $strings["no_items"] . "</td></tr></table><hr>";
+    echo <<<NO_RESULTS
+    <table><tr><td colspan="4">{$strings["no_items"]}</td></tr></table><hr>
+NO_RESULTS;
+
 }
-echo "<br/><br/>
-<a href=\"addsupport.php?project=".$project."\" class=\"FooterCell\">" . $strings["add_support_request"] . "</a>";
+echo <<<FOOTER
+    <br/><br/>
+<a href="addsupport.php?project={$project}" class="FooterCell">{$strings["add_support_request"]}</a>
+FOOTER;
 
 include("include_footer.php");
