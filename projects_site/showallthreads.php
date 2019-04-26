b6ae886757f9a17bff46eb09b8969691bbf29f53
Jeff Sittler <mindblender@gmail.com>
2019-04-26 00:54:53 -0700

    Code clean up Refactored to use class methods

--- projects_site/showallthreads.php
+++ projects_site/showallthreads.php
@@ -2,10 +2,12 @@
 #Application name: PhpCollab
 #Status page: 0
 
+use phpCollab\Topics\Topics;
+
 $checkSession = "true";
 include '../includes/library.php';
 
-$topics = new \phpCollab\Topics\Topics();
+$topics = new Topics();
 
 $id = $_GET["id"];
 $strings = $GLOBALS["strings"];
@@ -41,42 +43,92 @@ $listPosts = $topics->getPostsByTopicId($detailTopic["top_id"]);
 
 $idStatus = $detailTopic["top_status"];
 
-echo "<table cellspacing='0' width='90%' cellpadding='3'>
-<tr><th colspan='4'>" . $strings["information"] . ":</th></tr>
-<tr><th>" . $strings["subject"] . ":</th><td>" . $detailTopic["top_subject"] . "</td><th>" . $strings["posts"] . ":</th><td>" . $detailTopic["top_posts"] . "</td></tr>
-<tr><th>" . $strings["project"] . ":</th><td>" . $projectDetail["pro_name"] . "</td><th>" . $strings["last_post"] . ":</th><td>" . phpCollab\Util::createDate($detailTopic["top_last_post"], $timezoneSession) . "</td></tr>
-<tr><th>&nbsp;</th><td>&nbsp;</td><th>" . $strings["retired"] . ":</th><td>$statusTopicBis[$idStatus]</td></tr>
-<tr><th>" . $strings["owner"] . ":</th><td colspan='3'><a href='mailto:" . $detailTopic["top_mem_email_work"] . "'>" . $detailTopic["top_mem_login"] . "</a></td></tr>
-<tr><td colspan='4'>&nbsp;</td></tr>
-<tr><th colspan='4'>" . $strings["discussion"] . ":</th></tr>";
+$topicDate = phpCollab\Util::createDate($detailTopic["top_last_post"], $timezoneSession);
+
+echo <<<TABLE
+<table style="width: 90%;" class="nonStriped">
+    <tr class="lightHighlight">
+        <th colspan="4">{$strings["information"]}:</th>
+    </tr>
+    <tr class="lightHighlight">
+        <th>{$strings["subject"]}:</th>
+        <td>{$detailTopic["top_subject"]}</td>
+        <th>{$strings["posts"]}:</th>
+        <td>{$detailTopic["top_posts"]}</td>
+    </tr>
+    <tr class="lightHighlight">
+        <th>{$strings["project"]}:</th>
+        <td>{$projectDetail["pro_name"]}</td>
+        <th>{$strings["last_post"]}:</th>
+        <td>{$topicDate}</td>
+    </tr>
+    <tr class="lightHighlight">
+        <th>&nbsp;</th>
+        <td>&nbsp;</td>
+        <th>{$strings["retired"]}:</th>
+        <td>{$statusTopicBis[$idStatus]}</td>
+    </tr>
+    <tr class="lightHighlight">
+        <th>{$strings["owner"]}:</th>
+        <td colspan="3"><a href="mailto:{$detailTopic["top_mem_email_work"]}">{$detailTopic["top_mem_login"]}</a></td>
+    </tr>
+    <tr class="lightHighlight">
+        <td colspan="4">&nbsp;</td>
+    </tr>
+    <tr class="even">
+        <th colspan="4">{$strings["discussion"]}:</th>
+    </tr>
+TABLE;
+
 
 if ($detailTopic["top_status"] == "1") {
-    echo "<tr><td colspan='4' align='right'><a href='threadpost.php?id=$id'>" . $strings["post_reply"] . "</a></td></tr>";
+    echo <<<TR
+        <tr class="even">
+            <td colspan="4" style="text-align: right;"><a href="threadpost.php?id={$id}">{$strings["post_reply"]}</a></td>
+        </tr>
+TR;
 }
 
 if ($listPosts) {
     foreach ($listPosts as $post) {
-        if (!($i % 2)) {
-            $class = "odd";
-        } else {
-            $class = "even";
-        }
-        echo "<tr><td colspan='4' class='$class'>&nbsp;</td></tr>
-<tr class='$class'><th>" . $strings["posted_by"] . " :</th><td>" . $post["pos_mem_name"] . "</td><td colspan='2' align='right'>";
+        echo <<<TR
+        <tr class="odd"><td colspan="4"><hr /></td></tr>
+        <tr class="even">
+            <th>{$strings["posted_by"]} :</th>
+            <td>{$post["pos_mem_name"]}</td>
+            <td colspan="2" style="text-align: right;">
+TR;
 
         if ($detailProject["pro_owner"] == $idSession || $profilSession == "0" || $post["pos_member"] == $idSession) {
-            echo "<a href='../projects_site/showallthreads.php?id=$id&action=delete&post=" . $post["pos_id"] . "'>" . $strings["delete_message"] . "</a>";
+            echo <<<LINK
+                <a href="../projects_site/showallthreads.php?id={$id}&action=delete&post={$post["pos_id"]}">{$strings["delete_message"]}</a>
+LINK;
         } else {
             echo "&nbsp";
         }
 
-        echo "</td></tr>
-<tr class='$class'><th>" . $strings["email"] . " :</th><td colspan='3'><a href='mailto:" . $post["pos_mem_email_work"] . "'>" . $post["pos_mem_email_work"] . "</a></td></tr>
-<tr class='$class'><th nowrap>" . $strings["when"] . " :</th><td colspan='3'>" . phpCollab\Util::createDate($post["pos_created"], $timezoneSession) . "</td></tr>
-<tr class='$class'><th>" . $strings["message"] . " :</th><td colspan='3'>" . nl2br($post["pos_message"]) . "</td></tr>";
+        $createdDate = phpCollab\Util::createDate($post["pos_created"], $timezoneSession);
+        $postMessage = nl2br($post["pos_message"]);
+        echo <<<TR
+            </td>
+        </tr>
+        <tr class="even">
+            <th>{$strings["email"]} :</th>
+            <td colspan="3"><a href="mailto:{$post["pos_mem_email_work"]}">{$post["pos_mem_email_work"]}</a></td>
+        </tr>
+        <tr class="even">
+            <th nowrap>{$strings["when"]} :</th>
+            <td colspan="3">{$createdDate}</td>
+        </tr>
+        <tr class="even">
+            <th>{$strings["message"]} :</th>
+            <td colspan="3">{$postMessage}</td>
+        </tr>
+        <tr class="odd"><td colspan="4"><hr /></td></tr>
+TR;
     }
 } else {
-    echo "<tr><td colspan='4' class='ListOddRow'>" . $strings["no_items"] . "</td></tr>";
+    echo '<tr><td colspan="4" class="ListOddRow">' . $strings["no_items"] . '</td></tr>';
 }
 echo "</table>";
 
