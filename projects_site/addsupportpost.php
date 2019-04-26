0e8a8b1fa1b7520ed0530e63730510f6fc53a90c
Jeff Sittler <mindblender@gmail.com>
2019-04-25 18:15:54 -0700

    Code clean up Refactored to use class methods Removed include for support/noti_newpost.php

--- projects_site/addsupportpost.php
+++ projects_site/addsupportpost.php
@@ -2,6 +2,9 @@
 #Application name: PhpCollab
 #Status page: 0
 
+use phpCollab\Members\Members;
+use phpCollab\Support\Support;
+
 $checkSession = "true";
 include '../includes/library.php';
 
@@ -12,22 +15,30 @@ $requestDetail->openSupportRequests($tmpquery);
 if ($requestDetail->sr_project[0] != $projectSession || $requestDetail->sr_user[0] != $idSession) {
     phpCollab\Util::headerFunction("index.php");
 }
+if ($_GET["action"] == "add") {
+    $message = phpCollab\Util::convertData($_POST["response_message"]);
 
-if ($action == "add") {
-    $mes = phpCollab\Util::convertData($mes);
+    if (!empty($message)) {
+        $newPostId = $support->addSupportPost($id, $message, $dateheure, $idSession, $requestDetail["sr_project"]);
 
-    $num = phpCollab\Util::newConnectSql(
-        "INSERT INTO {$tableCollab["support_posts"]} (request_id,message,date,owner,project) VALUES(:request_id,:message,:date,:owner,:project)",
-        ["request_id" => $id,"message" => $mes,"date" => $dateheure,"owner" => $idSession,"project" => $requestDetail->sr_project[0]]
-    );
+        if ($notifications == "true") {
+            // Gather additional information for the notification
+            $postDetail = $support->getSupportPostById($newPostId);
+            $requestDetail = $support->getSupportRequestById($postDetail["sp_request_id"]);
+            $userDetail = $members->getMemberById($requestDetail["sr_member"]);
 
-    if ($notifications == "true") {
-        if ($mes != "") {
-            include '../support/noti_newpost.php';
+            try {
+                $support->sendNewPostNotification($requestDetail, $postDetail, $userDetail);
+            }
+            catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
+        phpCollab\Util::headerFunction("suprequestdetail.php?id=$id");
+    } else {
+        $error = "The message can not be blank.  Please enter a message.";
     }
-
-    phpCollab\Util::headerFunction("suprequestdetail.php?id=$id");
+//die('added?');
 }
 
 
@@ -35,15 +46,37 @@ $bouton[6] = "over";
 $titlePage = $strings["support"];
 include 'include_header.php';
 
-echo "<form accept-charset=\"UNKNOWN\" method=\"POST\" action=\"../projects_site/addsupportpost.php?id=$id&action=add&project=$projectSession#filedetailsAnchor\" name=\"addsupport\" enctype=\"multipart/form-data\">";
+if (!empty($error)) {
+    echo <<<ERROR
+<div style="color: darkred; padding: 1em;">{$error}</div>
+ERROR;
 
-echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\">
-<tr><th colspan=\"2\">" . $strings["add_support_response"] . "</th></tr>
-<tr><th>" . $strings["message"] . "</th><td><textarea rows=\"3\" style=\"width: 400px; height: 200px;\" name=\"mes\" cols=\"43\">$mes</textarea></td></tr>
-<input type=\"hidden\" name=\"user\" value=\"$idSession\">";
+}
 
-echo "<tr><th>&nbsp;</th><td><input type=\"SUBMIT\" value=\"" . $strings["submit"] . "\"></td></tr>
+echo <<<FORM
+<form accept-charset="UNKNOWN" 
+    method="POST" 
+    action="../projects_site/addsupportpost.php?id={$id}&action=add&project={$projectSession}#filedetailsAnchor" 
+    name="addsupport" 
+    enctype="multipart/form-data">
+
+<table style="width: 90%; margin-bottom: 3em;">
+    <tr>
+        <th colspan="2" style="text-align: left; padding-bottom: 2em; font-size: 1rem;">{$strings["add_support_response"]}</th>
+    </tr>
+    <tr>
+        <th style="vertical-align: top"><label for="response_message" >{$strings["message"]}</label></th>
+        <td><textarea required rows="3" style="width: 400px; height: 200px;" name="response_message" id="response_message" cols="43">{$_POST["response_message"]}</textarea></td>
+    </tr>
+    <tr>
+        <th>&nbsp;</th>
+        <td><input type="SUBMIT" value="{$strings["submit"]}"></td>
+    </tr>
 </table>
-</form>";
+    <input type="hidden" name="user" value="{$idSession}">
+</form>
+FORM;
+
+
 
 include("include_footer.php");
