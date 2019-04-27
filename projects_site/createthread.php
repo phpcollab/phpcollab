d353a52d1d2d71c08e159c075ea1895f1706e047
Jeff Sittler <mindblender@gmail.com>
2019-04-26 17:03:19 -0700

    Code clean up Refactored to use class methods

--- projects_site/createthread.php
+++ projects_site/createthread.php
@@ -2,31 +2,35 @@
 #Application name: PhpCollab
 #Status page: 0
 
+use phpCollab\Projects\Projects;
+use phpCollab\Topics\Topics;
+
 $checkSession = "true";
 include '../includes/library.php';
 
-if ($action == "add") {
-    $topicField = phpCollab\Util::convertData($topicField);
-    $messageField = phpCollab\Util::convertData($messageField);
-    $num = phpCollab\Util::newConnectSql(
-        "INSERT INTO {$tableCollab["topics"]} (project,owner,subject,status,last_post,posts,published) VALUES (:project,:owner,:subject,:status,:last_post,:posts,:published)",
-        ["project" => $projectSession,"owner" => $idSession,"subject" => $topicField,"status" => 1,"last_post" => $dateheure,"posts" => 1,"published" => 0]
-    );
-    phpCollab\Util::autoLinks($messageField);
+$projects = new Projects();
+$topics = new Topics();
 
-    phpCollab\Util::newConnectSql(
-        "INSERT INTO {$tableCollab["posts"]} (topic,member,created,message) VALUES (:topic,:member,:created,:message)",
-        ["topic" => $num, "member" => $idSession, "created" => $dateheure, "message" => $newText]
-    );
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($action == "add") {
+        $topicField = phpCollab\Util::convertData($_POST["topicField"]);
+        $messageField = phpCollab\Util::convertData($_POST["messageField"]);
 
-    if ($notifications == "true") {
-        $tmpquery = "WHERE pro.id = '$projectSession'";
-        $projectDetail = new phpCollab\Request();
-        $projectDetail->openProjects($tmpquery);
+        $newTopic = $topics->addTopic($projectSession, $idSession, $topicField, 1, 1, 0);
 
-        include '../topics/noti_newtopic.php';
+        phpCollab\Util::autoLinks($messageField);
+
+        $newPostId = $topics->addPost($newTopic["top_id"], $idSession, $messageField);
+
+        if ($notifications == "true") {
+            try {
+                $topics->sendNewTopicNotification($newTopic);
+            } catch (Exception $e) {
+                echo 'Error sending mail, ' . $e->getMessage();
+            }
+        }
+        phpCollab\Util::headerFunction("showallthreadtopics.php");
     }
-    phpCollab\Util::headerFunction("showallthreadtopics.php");
 }
 
 $bodyCommand = "onload=\"document.createThreadTopic.topicField.focus();\"";
@@ -35,15 +39,29 @@ $bouton[5] = "over";
 $titlePage = $strings["create_topic"];
 include 'include_header.php';
 
-echo "<form accept-charset=\"UNKNOWN\" method=\"post\" action=\"../projects_site/createthread.php?project=$projectSession&action=add&id=$id\" name=\"createThreadTopic\" enctype=\"application/x-www-form-urlencoded\">";
-
-echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\">
-<tr><th colspan=\"2\">" . $strings["create_topic"] . "</th></tr>
-<tr><th>*&nbsp;" . $strings["topic"] . " :</th><td><input size=\"35\" value=\"$topicField\" name=\"topicField\" type=\"text\"></td></tr>
-<tr><th colspan=\"2\">" . $strings["enter_message"] . "</th></tr>
-<tr><th>*&nbsp;" . $strings["message"] . " :</th><td colspan=\"2\"><textarea rows=\"3\" name=\"messageField\" cols=\"43\"></textarea></td></tr>
-<tr><th>&nbsp;</th><td colspan=\"2\"><input name=\"submit\" type=\"submit\" value=\"" . $strings["save"] . "\"></td></tr>
-</table>
-</form>";
+echo <<<FORM
+<form method="post" action="../projects_site/createthread.php?project={$projectSession}&action=add&id={$id}" name="createThreadTopic">
+    <table style="width: 90%;" class="nonStriped">
+        <tr>
+            <th colspan="2">{$strings["create_topic"]}</th>
+        </tr>
+        <tr>
+            <th>* {$strings["topic"]} :</th>
+            <td><input size="35" value="$topicField" name="topicField" type="text" required></td>
+        </tr>
+        <tr>
+            <th colspan="2">{$strings["enter_message"]}</th>
+        </tr>
+        <tr>
+            <th>* {$strings["message"]} :</th>
+            <td><textarea rows="3" name="messageField" cols="43" required></textarea></td>
+        </tr>
+        <tr>
+            <th>&nbsp;</th>
+            <td><input name="submit" type="submit" value="{$strings["save"]}"></td>
+        </tr>
+    </table>
+</form>
+FORM;
 
 include("include_footer.php");
