ab3029923edb21a4bc322e0ff1c1d25a8fe23245
Jeff Sittler <mindblender@gmail.com>
2019-04-30 11:17:41 -0700

    Code clean up Refactored to use class methods

--- projects_site/teamtaskdetail.php
+++ projects_site/teamtaskdetail.php
@@ -2,14 +2,18 @@
 #Application name: PhpCollab
 #Status page: 0
 
+use phpCollab\Tasks\Tasks;
+use phpCollab\Updates\Updates;
+
 $checkSession = "true";
 include '../includes/library.php';
 
-$tmpquery = "WHERE tas.id = '$id'";
-$taskDetail = new phpCollab\Request();
-$taskDetail->openTasks($tmpquery);
+$tasks = new Tasks();
+$updates = new Updates();
 
-if ($taskDetail->tas_published[0] == "1" || $taskDetail->tas_project[0] != $projectSession) {
+$taskDetail = $tasks->getTaskById($id);
+
+if ($taskDetail["tas_published"] == "1" || $taskDetail["tas_project"] != $projectSession) {
     phpCollab\Util::headerFunction("index.php");
 }
 
@@ -21,37 +25,41 @@ $block1 = new phpCollab\Block();
 
 $block1->heading($strings["team_task_details"]);
 
-echo "<table cellspacing=\"0\" cellpadding=\"3\">";
-if ($taskDetail->tas_name[0] != "") {
-    echo "<tr><td>" . $strings["name"] . " :</td><td>" . $taskDetail->tas_name[0] . "</td></tr>";
+echo "<table>";
+if ($taskDetail["tas_name"] != "") {
+    echo "<tr><td>" . $strings["name"] . " :</td><td>" . $taskDetail["tas_name"] . "</td></tr>";
 }
-if ($taskDetail->tas_description[0] != "") {
-    echo "<tr><td valign=\"top\">" . $strings["description"] . " :</td><td>" . nl2br($taskDetail->tas_description[0]) . "</td></tr>";
+if ($taskDetail["tas_description"] != "") {
+    $taskDescription = nl2br($taskDetail["tas_description"]);
+    echo <<<TR
+        <tr>
+            <td style='vertical-align: top'>{$strings["description"]} :</td>
+            <td>{$taskDescription}</td>
+        </tr>
+TR;
 }
-$complValue = ($taskDetail->tas_completion[0] > 0) ? $taskDetail->tas_completion[0] . "0 %" : $taskDetail->tas_completion[0] . " %";
+$complValue = ($taskDetail["tas_completion"] > 0) ? $taskDetail["tas_completion"] . "0 %" : $taskDetail["tas_completion"] . " %";
 echo "<tr><td>" . $strings["completion"] . " :</td><td>" . $complValue . "</td></tr>";
-if ($taskDetail->tas_assigned_to[0] != "0") {
-    echo "<tr><td>" . $strings["assigned_to"] . " :</td><td>" . $taskDetail->tas_mem_name[0] . "</td></tr>";
+if ($taskDetail["tas_assigned_to"] != "0") {
+    echo "<tr><td>" . $strings["assigned_to"] . " :</td><td>" . $taskDetail["tas_mem_name"] . "</td></tr>";
 }
-if ($taskDetail->tas_comments[0] != "") {
-    echo "<tr><td>" . $strings["comments"] . " :</td><td>" . nl2br($taskDetail->tas_comments[0]) . "</td></tr>";
+if ($taskDetail["tas_comments"] != "") {
+    echo "<tr><td>" . $strings["comments"] . " :</td><td>" . nl2br($taskDetail["tas_comments"]) . "</td></tr>";
 }
-if ($taskDetail->tas_start_date[0] != "") {
-    echo "<tr><td>" . $strings["start_date"] . " :</td><td>" . $taskDetail->tas_start_date[0] . "</td></tr>";
+if ($taskDetail["tas_start_date"] != "") {
+    echo "<tr><td>" . $strings["start_date"] . " :</td><td>" . $taskDetail["tas_start_date"] . "</td></tr>";
 }
-if ($taskDetail->tas_due_date[0] != "") {
-    echo "<tr><td>" . $strings["due_date"] . " :</td><td>" . $taskDetail->tas_due_date[0] . "</td></tr>";
+if ($taskDetail["tas_due_date"] != "") {
+    echo "<tr><td>" . $strings["due_date"] . " :</td><td>" . $taskDetail["tas_due_date"] . "</td></tr>";
 }
 echo "<tr><td>" . $strings["updates_task"] . " :</td><td>";
-$tmpquery = "WHERE upd.type='1' AND upd.item = '$id' ORDER BY upd.created DESC";
-$listUpdates = new phpCollab\Request();
-$listUpdates->openUpdates($tmpquery);
-$comptListUpdates = count($listUpdates->upd_id);
 
-if ($comptListUpdates != "0") {
+$listUpdates = $updates->getUpdates(1, $id, 'upd.created DESC');
+
+if ($listUpdates) {
     $j = 1;
-    for ($i = 0; $i < $comptListUpdates; $i++) {
-        echo "<b>" . $j . ".</b> <i>" . phpCollab\Util::createDate($listUpdates->upd_created[$i], $timezoneSession) . "</i><br/>" . nl2br($listUpdates->upd_comments[$i]);
+    foreach ($listUpdates as $update) {
+        echo "<b>" . $j . ".</b> <i>" . phpCollab\Util::createDate($update["upd_created"], $timezoneSession) . "</i><br/>" . nl2br($update["upd_comments"]);
         echo "<br/>";
         $j++;
     }
@@ -61,10 +69,7 @@ if ($comptListUpdates != "0") {
 
 echo "</td></tr> </table> <hr>";
 
-$tmpquery = "WHERE subtas.task = '$id' AND subtas.published = '0' ORDER BY subtas.name";
-$listSubtasks = new phpCollab\Request();
-$listSubtasks->openSubtasks($tmpquery);
-$comptListSubtasks = count($listSubtasks->subtas_id);
+$listSubtasks = $tasks->getPublishedSubtasksByParentTaskId($id, 'subtas.name');
 
 echo "<br/>";
 
@@ -72,32 +77,43 @@ $block2 = new phpCollab\Block();
 
 $block2->heading($strings["subtasks"]);
 
-if ($comptListSubtasks != "0") {
+if ($listSubtasks) {
     if ($activeJpgraph == "true") {
-        echo "<img src=\"graphsubtasks.php?task=" . $taskDetail->tas_id[0] . "\" alt=\"\">
-<span class=\"listEvenBold\">[<a href=\"http://www.aditus.nu/jpgraph/\" target=\"_blank\">JpGraph</a>]</span><br/><br/>";
+        echo <<<JPGRAPH
+        <img src="graphsubtasks.php?task={$taskDetail["tas_id"]}" alt="">
+        <span class="listEvenBold">[<a href="http://www.aditus.nu/jpgraph/" target="_blank">JpGraph</a>]</span><br/><br/>
+JPGRAPH;
     }
 
-    echo "<table cellspacing=\"0\" width=\"90%\" border=\"0\" cellpadding=\"3\" cols=\"4\" class=\"listing\">
-<tr><th class=\"active\">" . $strings["name"] . "</th><th>" . $strings["description"] . "</th><th>" . $strings["status"] . "</th><th>" . $strings["due"] . "</th></tr>";
-
-    for ($i = 0; $i < $comptListSubtasks; $i++) {
-        if (!($i % 2)) {
-            $class = "odd";
-            $highlightOff = $block2->getOddColor();
-        } else {
-            $class = "even";
-            $highlightOff = $block2->getEvenColor();
-        }
-        $idStatus = $listSubtasks->subtas_status[$i];
-        echo "<tr class=\"$class\" onmouseover=\"this.style.backgroundColor='" . $block2->getHighlightOn() . "'\" onmouseout=\"this.style.backgroundColor='" . $highlightOff . "'\"><td><a href=\"teamsubtaskdetail.php?task=$id&id=" . $listSubtasks->subtas_id[$i] . "\">" . $listSubtasks->subtas_name[$i] . "</a></td><td>" . nl2br($listSubtasks->subtas_description[$i]) . "</td><td>$status[$idStatus]</td><td>" . $listSubtasks->subtas_due_date[$i] . "</td></tr>";
+    echo <<<SUBTASK_TABLE
+<table style="width: 90%;" class="listing striped">
+    <tr>
+        <th class="active">{$strings["name"]}</th>
+        <th>{$strings["description"]}</th>
+        <th>{$strings["status"]}</th>
+        <th>{$strings["due"]}</th>
+    </tr>
+SUBTASK_TABLE;
+    foreach ($listSubtasks as $subtask) {
+        $idStatus = $subtask["subtas_status"];
+        $subtaskDescription = nl2br($subtask["subtas_description"]);
+        echo <<<TR
+            <tr>
+                <td><a href=\"teamsubtaskdetail.php?task={$id}&id="{$subtask["subtas_id"]}">{$subtask["subtas_name"]}</a></td>
+                <td>{$subtaskDescription}</td>
+                <td>{$status[$idStatus]}</td>
+                <td>{$subtask["subtas_due_date"]}</td>
+            </tr>
+TR;
     }
-    echo "</table> <hr>\n";
+    echo "</table>";
 } else {
-    echo "<table cellspacing=\"0\" border=\"0\" cellpadding=\"2\"><tr><td colspan=\"4\" class=\"listOddBold\">" . $strings["no_items"] . "</td></tr></table><hr>";
+    echo '<div class="no-records">' . $strings["no_items"] . '</div>';
 }
 
-echo "<br/><br/>
-<a href=\"showallteamtasks.php?\">" . $strings["show_all"] . "</a>";
+echo <<<SHOW_ALL_LINK
+<br/><br/>
+<a href="showallteamtasks.php">{$strings["show_all"]}</a>
+SHOW_ALL_LINK;
 
 include("include_footer.php");
