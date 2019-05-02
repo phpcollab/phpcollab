ad0b9dea379d6c3da01d799e6daab2b7babae423
Jeff Sittler <mindblender@gmail.com>
2019-05-01 17:16:43 -0700

    Code clean up Refactored to use class methods

--- projects_site/addteamtask.php
+++ projects_site/addteamtask.php
@@ -30,8 +30,14 @@
 ** =============================================================================
 */
 
+use phpCollab\Assignments\Assignments;
+use phpCollab\Tasks\Tasks;
+
 $checkSession = "true";
 include '../includes/library.php';
+
+$tasks = new Tasks();
+$assignments = new Assignments();
 
 //case add task
 $id = (isset($_GET["id"])) ? $_GET["id"] : null;
@@ -40,57 +46,43 @@ $tableCollab = $GLOBALS["tableCollab"];
 $strings = $GLOBALS["strings"];
 $priority = $GLOBALS["priority"];
 
-if ($id == "") {
+if (empty($_GET["id"])) {
 
     //case add task
-    if ($_GET["action"] == "add") {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_POST["action"] == "add") {
 
-        //concat values from date selector and replace quotes by html code in name
-        $tn = phpCollab\Util::convertData($_POST["tn"]);
-        $d = phpCollab\Util::convertData($_POST["d"]);
-        $c = phpCollab\Util::convertData($_POST["c"]);
+            //concat values from date selector and replace quotes by html code in name
+            $taskName = phpCollab\Util::convertData($_POST["task_name"]);
+            $description = phpCollab\Util::convertData($_POST["description"]);
+            $comments = phpCollab\Util::convertData($_POST["comments"]);
+            $priority = $_POST["priority"];
+            $startDate = $_POST["start_date"];
+            $dueDate = $_POST["due_date"];
+            $publshed = $_POST["published"];
+            $assignedTo = $_POST["assigned_to"];
+            $projectId = $_POST["project_id"];
 
-        $tmpquery1 = "INSERT INTO {$tableCollab["tasks"]} (project,name,description,owner,assigned_to,status,priority,start_date,due_date,estimated_time,actual_time,comments,created,published,completion) VALUES(:project,:name,:description,:owner,:assigned_to,:status,:priority,:start_date,:due_date,:estimated_time,:actual_time,:comments,:created,:published,:completion)";
-        $dbParams = [];
-        $dbParams['project'] = $projectSession;
-        $dbParams['name'] = $tn;
-        $dbParams['description'] = $d;
-        $dbParams['owner'] = $_SESSION["idSession"];
-        $dbParams['assigned_to'] = 0;
-        $dbParams['status'] = 2;
-        $dbParams['priority'] = $_POST["pr"];
-        $dbParams['start_date'] = $_POST["sd"];
-        $dbParams['due_date'] = $_POST["dd"];
-        $dbParams['estimated_time'] = $_POST["etm"];
-        $dbParams['actual_time'] = $_POST["atm"];
-        $dbParams['comments'] = $c;
-        $dbParams['created'] = $dateheure;
-        $dbParams['published'] = $_POST["pub"];
-        $dbParams['completion'] = 0;
-        
-        $num = phpCollab\Util::newConnectSql($tmpquery1, $dbParams);
-        unset($dbParams);
+            try {
+                $newTask = $tasks->addTask($projectId, $taskName, $description, $idSession, 0, 2, $priority, $startDate, $dueDate, 0, 0, $comments, $publshed, 0);
 
-        $tmpquery2 = "INSERT INTO {$tableCollab["assignments"]} (task,owner,assigned_to,assigned) VALUES (:task,:owner,:assigned_to,:assigned)";
-        $dbParams = [];
-        $dbParams['task'] = $num;
-        $dbParams['owner'] = $_SESSION["idSession"];
-        $dbParams['assigned_to'] = $_POST["at"];
-        $dbParams['assigned'] = $dateheure;
-        phpCollab\Util::newConnectSql($tmpquery2, $dbParams);
-        unset($dbParams);
+                $assignments->addAssignment($newTask["tas_id"], $idSession, $assignedTo, $dateheure);
 
-        //send task assignment mail if notifications = true
-        if ($notifications == "true") {
-            include '../tasks/noti_clientaddtask.php';
+                //send task assignment mail if notifications = true
+                if ($notifications == "true") {
+                    $tasks->sendClientAddTaskNotification($newTask);
+                }
+
+                //create task sub-folder if filemanagement = true
+                if ($fileManagement == "true") {
+                    phpCollab\Util::createDirectory("../files/$projectSession/" . $newTask["tas_id"]);
+                }
+                phpCollab\Util::headerFunction("showallteamtasks.php");
+            }
+            catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
-
-        //create task sub-folder if filemanagement = true
-        if ($fileManagement == "true") {
-            phpCollab\Util::createDirectory("../files/$projectSession/$num");
-        }
-
-        phpCollab\Util::headerFunction("showallteamtasks.php");
     }
 }
 
@@ -101,27 +93,35 @@ $titlePage = $strings["add_task"];
 $includeCalendar = true; //Include Javascript files for the pop-up calendar
 include 'include_header.php';
 
-echo '<form accept-charset="UNKNOWN" method="POST" action="../projects_site/addteamtask.php?project='.$projectSession.'&action=add#etDAnchor" name="etDForm" enctype="application/x-www-form-urlencoded">';
+echo <<<FORM
+<form method="POST" action="../projects_site/addteamtask.php" name="etDForm">
+FORM;
+
+$publishTask = ($autoPublishTasks === false ? 0 : 1);
 
 echo <<< HTML
-<table cellpadding="3" cellspacing="0" border="0">
-	<tr><th colspan="2">{$strings["add_task"]}</th></tr>
+<input type="hidden" name="owner" value="{$projectDetail->pro_owner[0]}" />
+<input type="hidden" name="action" value="add" />
+<input type="hidden" name="assigned_to" value="0" />
+<input type="hidden" name="status" value="2" />
+<input type="hidden" name="completion" value="0" />
+<input type="hidden" name="project_id" value="$projectSession" />
+<input type="hidden" value="{$publishTask}" name="publish" />
+<table class="nonStriped">
+	<tr>
+	    <th colspan="2">{$strings["add_task"]}</th>
+    </tr>
 	<tr>
 		<th>*&nbsp;{$strings["name"]} :</th>
-		<td><input size="44" value="{$_POST["tn"]}" style="width: 400px" name="tn" maxlength="100" type="TEXT" /></td>
+		<td><input size="44" value="{$_POST["task_name"]}" style="width: 400px" name="task_name" maxlength="100" type="TEXT" /></td>
 	</tr>
 	<tr>
-		<th>{$strings["description"]} :</th>
-		<td><textarea rows="10" style="width: 400px; height: 160px;" name="d" cols="47">{$_POST["d"]}</textarea></td>
+		<th style="vertical-align: top">{$strings["description"]} :</th>
+		<td><textarea rows="10" style="width: 400px; height: 160px;" name="description" cols="47">{$_POST["description"]}</textarea></td>
 	</tr>
 
-	<input type="hidden" name="owner" value="{$projectDetail->pro_owner[0]}" />
-	<input type="hidden" name="at" value="0" />
-	<input type="hidden" name="st" value="2" />
-	<input type="hidden" name="completion" value="0" />
-	<input type="hidden" value="{($autoPublishTasks === false ? 0 : 1)}" name="pub" />
 	<tr><th>{$strings["priority"]} :</th>
-	<td><select name='pr'>
+	<td><select name='priority'>
 HTML;
 
 
@@ -137,24 +137,24 @@ for ($i = 0; $i < $comptPri; $i++) {
 
 echo "</select></td></tr>";
 
-if ($_POST["sd"] == "") {
-    $sd = $GLOBALS["date"];
+if (empty($_POST["start_date"])) {
+    $_POST["start_date"] = $GLOBALS["date"];
 }
-if ($_POST["dd"] == "") {
-    $dd = "--";
+if (empty($_POST["due_date"])) {
+    $_POST["due_date"] = "--";
 }
 
 echo <<< STARTDATE
  <tr>
 			<th>{$strings["start_date"]} :</th>
-			<td><input type="text" name="sd" id="start_date" size="20" value="{$_POST["sd"]}" />
+			<td><input type="text" name="start_date" id="start_date" size="20" value="{$_POST["start_date"]}" />
 				<input type="button" value=" ... " id="trigStartDate" />
 				<script type="text/javascript">
 				    Calendar.setup({
 				        inputField     :    "start_date",
 				        button         :    "trigStartDate",
 				        {$calendar_common_settings}
-				    });
+				    })
 				</script>
 			</td>
 	</tr>
@@ -164,14 +164,14 @@ echo <<< DUEDATE
 	<tr>
 		<th>{$strings["due_date"]} :</th>
 		<td>
-			<input type="text" name="dd" id="due_date" size="20" value="{$_POST["dd"]}" />
+			<input type="text" name="due_date" id="due_date" size="20" value="{$_POST["due_date"]}" />
 			<input type="button" value=" ... " id="trigDueDate" />
 			<script type="text/javascript">
 			    Calendar.setup({
 			        inputField     :    "due_date",
 			        button         :    "trigDueDate",
 			        {$calendar_common_settings}
-			    });
+			    })
 			</script>
 		</td>
 	</tr>
@@ -179,9 +179,9 @@ DUEDATE;
 
 echo <<< COMMENT
 	<tr>
-		<th>{strings["comments"]} :</th>
+		<th style="vertical-align: top">{$strings["comments"]} :</th>
 		<td>
-			<textarea rows='10' style='width: 400px; height: 160px;' name='c' cols='47'>{$_POST["c"]}</textarea>
+			<textarea rows='10' style='width: 400px; height: 160px;' name='comments' cols='47'>{$_POST["comments"]}</textarea>
 		</td>
 	</tr>
 	<tr><th>&nbsp;</th>
@@ -192,7 +192,7 @@ COMMENT;
 echo <<< CLOSE
 </table>
 </form>
-<p class="note">{$strings["client_add_task_note"]}</p>
+<h4 class="note">{$strings["client_add_task_note"]}</h4>
 CLOSE;
 
 include("include_footer.php");
