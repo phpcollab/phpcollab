d958cb32c274eda47ff3169a86d4be90ed23aa6a
Jeff Sittler <mindblender@gmail.com>
2019-05-06 20:36:52 -0700

    refactor: Code clean up and modified to use class methods

--- support/addpost.php
+++ support/addpost.php
@@ -2,12 +2,16 @@
 #Application name: PhpCollab
 #Status page: 0
 
+use phpCollab\Support\Support;
+use phpCollab\Util;
+
 $checkSession = "true";
 include_once '../includes/library.php';
 
 $id = $_GET["id"];
 $action = $_GET["action"];
 $idSession = $_SESSION["idSession"];
+
 
 if ($enableHelpSupport != "true") {
     phpCollab\Util::headerFunction('../general/permissiondenied.php');
@@ -19,54 +23,57 @@ if ($supportType == "admin") {
     }
 }
 
-$tmpquery = "WHERE sr.id = '$id'";
-$requestDetail = new phpCollab\Request();
-$requestDetail->openSupportRequests($tmpquery);
+$support = new Support();
 
-if ($action == "edit") {
-    $dbParams = [];
-    $dbParams["status"] = $_POST["sta"];
-    $dbParams["date_close"] = ($_POST["sta"] == 2) ? $dateheure : '--' ;
-    $dbParams["support_request_id"] = $id;
-    phpCollab\Util::newConnectSql("UPDATE {$tableCollab["support_requests"]} SET status=:status,date_close=:date_close WHERE id = :support_request_id", $dbParams);
-    unset($dbParams);
+$requestDetail = $support->getSupportRequestById($id);
 
-    if ($notifications == "true") {
-        if ($requestDetail->sr_status[0] != $_POST["sta"]) {
-            $num = $id;
-            include '../support/noti_statusrequestchange.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST["action"] == "edit") {
+
+        try {
+            $status = $_POST["status"];
+            $dateClose = ($_POST["status"] == 2) ? $dateheure : null;
+
+            $support->updateSupportPostStatus($id, $status, $dateClose);
+
+            $postDetails = $support->getSupportPostById($id);
+            if ($notifications == "true") {
+                if ($requestDetail["sr_status"] != $_POST["status"]) {
+                    $num = $id;
+                    $support->sendPostChangedNotification($postDetails);
+                }
+            }
+
+            phpCollab\Util::headerFunction("../support/viewrequest.php?id=$id");
+        } catch (Exception $e) {
+            echo '<div class="alert error">' . $e->getMessage() . '</div>';
         }
     }
 
-    phpCollab\Util::headerFunction("../support/viewrequest.php?id=$id");
-}
+    if ($_POST["action"] == "add") {
+        try {
+            $newPost = $support->addSupportPost($id, Util::convertData($_POST["message"]), $dateheure, $idSession, $requestDetail["sr_project"]);
 
-if ($action == "add") {
-    $num = phpCollab\Util::newConnectSql(
-        "INSERT INTO {$tableCollab["support_posts"]} (request_id,message,date,owner,project) VALUES(:request_id, :message, :date, :owner, :project)",
-        ["request_id" => $id, "message" => phpCollab\Util::convertData($mes), "date" => $dateheure, "owner" => $idSession, "project" => $requestDetail->sr_project[0]]
-    );
-
-    if ($notifications == "true") {
-        if ($mes != "") {
-            include '../support/noti_newpost.php';
+            if (!empty($newPost) && $notifications == "true") {
+                $support->sendPostChangedNotification($newPost);
+            }
+        } catch (Exception $e) {
+            echo '<div class="alert error">' . $e->getMessage() . '</div>';
         }
+
+        phpCollab\Util::headerFunction("../support/viewrequest.php?id=$id");
     }
-
-    phpCollab\Util::headerFunction("../support/viewrequest.php?id=$id");
 }
-
-
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
 
 if ($supportType == "team") {
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $requestDetail->sr_project[0], $requestDetail->sr_pro_name[0], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $requestDetail["sr_project"], $requestDetail["sr_pro_name"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail["sr_project"], $strings["support_requests"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail["sr_id"], $requestDetail["sr_subject"], "in"));
     if ($action == "status") {
         $blockPage->itemBreadcrumbs($strings["edit_status"]);
     } else {
@@ -75,8 +82,8 @@ if ($supportType == "team") {
 } elseif ($supportType == "admin") {
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], "in"));
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/support.php?", $strings["support_management"], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail->sr_project[0], $strings["support_requests"], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail->sr_id[0], $requestDetail->sr_subject[0], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/listrequests.php?id=" . $requestDetail["sr_project"], $strings["support_requests"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../support/viewrequest.php?id=" . $requestDetail["sr_id"], $requestDetail["sr_subject"], "in"));
     if ($action == "status") {
         $blockPage->itemBreadcrumbs($strings["edit_status"]);
     } else {
@@ -95,11 +102,19 @@ $block2 = new phpCollab\Block();
 
 $block2->form = "sr";
 if ($action == "status") {
-    $block2->openForm("../support/addpost.php?action=edit&id=$id&#" . $block2->form . "Anchor");
+    $block2->openForm("../support/addpost.php?id=$id&#" . $block2->form . "Anchor");
+    echo <<<FORM
+    <input type="hidden" name="action" value="edit">
+FORM;
 } else {
-    $block2->openForm("../support/addpost.php?action=add&id=$id&#" . $block2->form . "Anchor");
+    $block2->openForm("../support/addpost.php?id=$id&#" . $block2->form . "Anchor");
+    echo <<<FORM
+    <input type="hidden" name="action" value="add">
+FORM;
 }
-if ($error != "") {
+
+
+if (!empty($error)) {
     $block2->headingError($strings["errors"]);
     $block2->contentError($error);
 }
@@ -109,11 +124,15 @@ $block2->heading($strings["add_support_respose"]);
 $block2->openContent();
 $block2->contentTitle($strings["details"]);
 if ($action == "status") {
-    echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">" . $strings["status"] . " :</td><td><select name=\"sta\">";
+    echo <<<TR
+    <tr class="odd">
+        <td class="leftvalue">{$strings["status"]} :</td>
+        <td><select name="status">
+TR;
 
     $comptSta = count($requestStatus);
     for ($i = 0; $i < $comptSta; $i++) {
-        if ($requestDetail->sr_status[0] == $i) {
+        if ($requestDetail["sr_status"] == $i) {
             echo "<option value=\"$i\" selected>$requestStatus[$i]</option>";
         } else {
             echo "<option value=\"$i\">$requestStatus[$i]</option>";
@@ -121,11 +140,26 @@ if ($action == "status") {
     }
     echo "</select></td></tr>";
 } else {
-    echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">" . $strings["message"] . "</td><td><textarea rows=\"3\" style=\"width: 400px; height: 200px;\" name=\"mes\" cols=\"43\">$mes</textarea></td></tr>
-<input type=\"hidden\" name=\"user\" value=\"$idSession\">";
+    echo <<<HTML
+        <tr class="odd">
+            <td class="leftvalue">{$strings["message"]}</td>
+            <td><textarea rows="3" style="width: 400px; height: 200px;" name="message" cols="43">{$_POST["message"]}</textarea></td>
+        </tr>
+HTML;
 }
-echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">&nbsp;</td><td><input type=\"SUBMIT\" value=\"" . $strings["submit"] . "\"></td></tr>";
+echo <<<TR
+        <tr class="odd">
+            <td class="leftvalue">&nbsp;</td>
+            <td><input type="submit" value="{$strings["submit"]}"></td>
+        </tr>
+TR;
 
 $block2->closeContent();
 
-include '../themes/' . THEME . '/footer.php';
+echo <<<FORM
+    <input type="hidden" name="user" value="$idSession">
+FORM;
+
+$block2->closeForm();
+
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
