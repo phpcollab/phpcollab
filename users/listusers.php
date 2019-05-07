d84b5412b8027788380fe142e57e714b9a2d5629
Jeff Sittler <mindblender@gmail.com>
2019-05-06 22:00:40 -0700

    refactor: code clean up and changed to use member class methods

--- users/listusers.php
+++ users/listusers.php
@@ -3,6 +3,8 @@
 #Status page: 1
 #Path by root: ../users/listusers.php
 
+use phpCollab\Members\Members;
+
 $checkSession = "true";
 include_once '../includes/library.php';
 
@@ -10,17 +12,19 @@ if ($profilSession != "0") {
     phpCollab\Util::headerFunction('../general/permissiondenied.php');
 }
 
+$members = new Members();
+
 $setTitle .= " : List Users";
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../administration/admin.php?", $strings["administration"], "in"));
 $blockPage->itemBreadcrumbs($strings["user_management"]);
 $blockPage->closeBreadcrumbs();
 
-if ($msg != "") {
+if (!empty($msg)) {
     include '../includes/messages.php';
     $blockPage->messageBox($msgLabel);
 }
@@ -41,34 +45,50 @@ $block1->paletteIcon(4, "export", $strings["export"]);
 $block1->paletteIcon(5, "email", $strings["email"]);
 $block1->closePaletteIcon();
 
-$block1->sorting("users", $sortingUser["users"], "mem.name ASC", $sortingFields = array(0 => "mem.name", 1 => "mem.login", 2 => "mem.email_work", 3 => "mem.profil", 4 => "log.connected"));
+$block1->sorting("users", $sortingUser["users"], "mem.name ASC", $sortingFields = array(
+    0 => "mem.name",
+    1 => "mem.login",
+    2 => "mem.email_work",
+    3 => "mem.profil",
+    4 => "log.connected")
+);
 
 if ($demoMode == "true") {
-    $tmpquery = "WHERE mem.id != '1' AND mem.profil != '3' ORDER BY $block1->sortingValue";
+    $listMembers = $members->getNonClientMembers($block1->sortingValue);
 } else {
-    $tmpquery = "WHERE mem.id != '2' ORDER BY $block1->sortingValue";
-}
-$listMembers = new phpCollab\Request();
-$listMembers->openMembers($tmpquery);
-$comptListMembers = count($listMembers->mem_id);
+    $listMembers = $members->getAllMembers($block1->sortingValue);
 
-if ($comptListMembers != "0") {
+    // Find the "demo" user in the user list
+    $key = array_search(2, array_column($listMembers, 'mem_id'));
+
+    // Remove the "demo" user from results
+    unset($listMembers[$key]);
+}
+
+if ($listMembers) {
     $block1->openResults();
 
-    $block1->labels($labels = array(0 => $strings["full_name"], 1 => $strings["user_name"], 2 => $strings["email"], 3 => $strings["profile"], 4 => $strings["connected"]), "false");
+    $block1->labels($labels = array(
+        0 => $strings["full_name"], 
+        1 => $strings["user_name"], 
+        2 => $strings["email"], 
+        3 => $strings["profile"], 
+        4 => $strings["connected"]), 
+        "false"
+    );
 
-    for ($i = 0; $i < $comptListMembers; $i++) {
-        $idProfil = $listMembers->mem_profil[$i];
+    foreach ($listMembers as $member) {
+        $idProfil = $member["mem_profil"];
         $block1->openRow();
-        $block1->checkboxRow($listMembers->mem_id[$i]);
-        $block1->cellRow($blockPage->buildLink("../users/viewuser.php?id=" . $listMembers->mem_id[$i], $listMembers->mem_name[$i], "in"));
-        $block1->cellRow($listMembers->mem_login[$i]);
-        $block1->cellRow($blockPage->buildLink($listMembers->mem_email_work[$i], $listMembers->mem_email_work[$i], "mail"));
+        $block1->checkboxRow($member["mem_id"]);
+        $block1->cellRow($blockPage->buildLink("../users/viewuser.php?id=" . $member["mem_id"], $member["mem_name"], "in"));
+        $block1->cellRow($member["mem_login"]);
+        $block1->cellRow($blockPage->buildLink($member["mem_email_work"], $member["mem_email_work"], "mail"));
         $block1->cellRow($profil[$idProfil]);
-        if ($listMembers->mem_log_connected[$i] > $dateunix - 5 * 60) {
-            $block1->cellRow($strings["yes"] . " " . $strings["clients_connected"] . " (" . $listMembers->mem_last_page[$i] . ")");
+        if ($member["mem_log_connected"] > $dateunix - 5 * 60) {
+            $block1->cellRow($strings["yes"] . " " . $strings["clients_connected"] . " (" . $member["mem_last_page"] . ")");
         } else {
-            $block1->cellRow($strings["no"] . " (" . $listMembers->mem_last_page[$i] . ")");
+            $block1->cellRow($strings["no"] . " (" . $member["mem_last_page"] . ")");
         }
         $block1->closeRow();
     }
@@ -85,6 +105,6 @@ $block1->paletteScript(2, "info", "../users/viewuser.php?", "false,true,false", 
 $block1->paletteScript(3, "edit", "../users/edituser.php?", "false,true,false", $strings["edit"]);
 $block1->paletteScript(4, "export", "../users/exportusers.php?", "true,false,true", $strings["export"]);
 $block1->paletteScript(5, "email", "../users/emailusers.php?", "false,true,true", $strings["email"]);
-$block1->closePaletteScript($comptListMembers, $listMembers->mem_id);
+$block1->closePaletteScript(count($listMembers), $listMembers[0]["mem_id"]);
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
