ffbd9cc6506966278f819732ce5983d3c2530944
Jeff Sittler <mindblender@gmail.com>
2019-05-09 18:14:38 -0700

    refactor: Rewrote pretty much the whole file to generate the CSV file.  Also changed checkSession flag to true.

--- projects/exportproject.php
+++ projects/exportproject.php
@@ -3,91 +3,103 @@
 #Status page: 0
 #Path by root: ../projects/exportproject.php
 
+use phpCollab\Projects\Projects;
+use phpCollab\Tasks\Tasks;
+use phpCollab\Util;
+
 $export = "true";
 
-$checkSession = "false";
+$checkSession = "true";
 include_once '../includes/library.php';
 
-require '../includes/phpmyadmin/defines.lib.php';
-function which_crlf()
-{
-    $the_crlf = "\n";
+$projects = new Projects();
+$tasks = new Tasks();
 
-    // The 'USR_OS' constant is defined in "./libraries/defines.lib.php"
-    // Win case
-    if (USR_OS == 'Win') {
-        $the_crlf = "\r\n";
-    } // Mac case
-    elseif (USR_OS == 'Mac') {
-        $the_crlf = "\r";
-    } // Others
-    else {
-        $the_crlf = "\n";
-    }
+$projectDetail = $projects->getProjectById($id);
 
-    return $the_crlf;
+if ($projectDetail["pro_org_id"] == "1") {
+    $projectDetail["pro_org_name"] = $strings["none"];
 }
+$idStatus = $projectDetail["pro_status"];
+$idPriority = $projectDetail["pro_priority"];
 
+$export_csv = [];
+array_push($export_csv, array($strings["this_report_generated_by"]));
+array_push($export_csv, array($strings["project"]));
+array_push($export_csv, array($strings["name"], $strings["description"], $strings["owner"], $strings["priority"], $strings["status"], $strings["created"], $strings["organization"]));
+array_push($export_csv, array($projectDetail["pro_name"], $projectDetail["pro_description"], $projectDetail["pro_mem_login"], $priority[$idPriority], $status[$idStatus], Util::createDate($projectDetail["pro_created"], $timezoneSession), $projectDetail["pro_org_name"]));
 
-@set_time_limit(600);
-$crlf = which_crlf();
+$listTasks = $tasks->getTasksByProjectId($id);
 
-/**
- * Send headers depending on whether the user choosen to download a dump file
- * or not
- */
+if ($listTasks) {
+    array_push($export_csv, array($strings["tasks"]));
+    array_push($export_csv,
+        array(
+            $strings["name"],
+            $strings["description"],
+            $strings["owner"],
+            $strings["priority"],
+            $strings["status"],
+            $strings["created"],
+            $strings["start_date"],
+            $strings["due_date"],
+            $strings["complete_date"],
+            $strings["completion"],
+            $strings["scope_creep"],
+            $strings["estimated_time"],
+            $strings["actual_time"],
+            $strings["published"],
+            $strings["comments"],
+            $strings["assigned"],
+            $strings["assigned_to"]
+        )
+    );
 
-$tmpquery = "WHERE pro.id = '$id'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
-
-if ($projectDetail->pro_org_id[0] == "1") {
-    $projectDetail->pro_org_name[0] = $strings["none"];
-}
-$idStatus = $projectDetail->pro_status[0];
-$idPriority = $projectDetail->pro_priority[0];
-
-$dump_buffer .= $strings["project"] . $crlf;
-$dump_buffer .= "\"" . $strings["name"] . "\";\"" . $strings["description"] . "\";\"" . $strings["owner"] . "\";\"" . $strings["priority"] . "\";\"" . $strings["status"] . "\";\"" . $strings["created"] . "\";\"" . $strings["organization"] . "\"" . $crlf;
-$dump_buffer .= "\"" . $projectDetail->pro_name[0] . "\";\"" . $projectDetail->pro_description[0] . "\";\"" . $projectDetail->pro_mem_login[0] . "\";\"" . $priority[$idPriority] . "\";\"" . $status[$idStatus] . "\";\"" . phpCollab\Util::createDate($projectDetail->pro_created[0], $timezoneSession) . "\";\"" . $projectDetail->pro_org_name[0] . "\"" . $crlf . $crlf;
-
-$tmpquery = "WHERE tas.project = '$id'";
-$listTasks = new phpCollab\Request();
-$listTasks->openTasks($tmpquery);
-$comptListTasks = count($listTasks->tas_id);
-
-if ($comptListTasks != "0") {
-    $dump_buffer .= $strings["tasks"] . $crlf;
-    $dump_buffer .= "\"" . $strings["name"] . "\";\"" . $strings["description"] . "\";\"" . $strings["owner"] . "\";\"" . $strings["priority"] . "\";\"" . $strings["status"] . "\";\"" . $strings["created"] . "\";\"" . $strings["start_date"] . "\";\"" . $strings["due_date"] . "\";\"" . $strings["complete_date"] . "\";\"" . $strings["completion"] . "\";\"" . $strings["scope_creep"] . "\";\"" . $strings["estimated_time"] . "\";\"" . $strings["actual_time"] . "\";\"" . $strings["published"] . "\";\"" . $strings["comments"] . "\";\"" . $strings["assigned"] . "\";\"" . $strings["assigned_to"] . "\"" . $crlf;
-
-    for ($i = 0; $i < $comptListTasks; $i++) {
-        if ($listTasks->tas_assigned_to[$i] == "0") {
-            $listTasks->tas_mem_login[$i] = $strings["unassigned"];
+    foreach ($listTasks as $task) {
+        if ($task["tas_assigned_to"] == "0") {
+            $task["tas_mem_login"] = $strings["unassigned"];
         }
-        $idStatus = $listTasks->tas_status[$i];
-        $idPriority = $listTasks->tas_priority[$i];
-        $idPublish = $listTasks->tas_published[$i];
-        $complValue = ($listTasks->tas_completion[$i] > 0) ? $listTasks->tas_completion[$i] . "0 %" : $listTasks->tas_completion[$i] . " %";
+        $idStatus = $task["tas_status"];
+        $idPriority = $task["tas_priority"];
+        $idPublish = $task["tas_published"];
+        $complValue = ($task["tas_completion"] > 0) ? $task["tas_completion"] . "0 %" : $task["tas_completion"] . " %";
 
-        if ($listTasks->tas_complete_date[$i] != "" && $listTasks->tas_complete_date[$i] != "--" && $listTasks->tas_due_date[$i] != "--") {
-            $diff = phpCollab\Util::diffDate($listTasks->tas_complete_date[$i], $listTasks->tas_due_date[$i]);
+        if ($task["tas_complete_date"] != "" && $task["tas_complete_date"] != "--" && $task["tas_due_date"] != "--") {
+            $diff = phpCollab\Util::diffDate($task["tas_complete_date"], $task["tas_due_date"]);
         }
-        $dump_buffer .= "\"" . $listTasks->tas_name[$i] . "\";\"" . $listTasks->tas_description[$i] . "\";\"" . $listTasks->tas_mem2_login[$i] . "\";\"" . $priority[$idPriority] . "\";\"" . $status[$idStatus] . "\";\"" . phpCollab\Util::createDate($listTasks->tas_created[$i], $timezoneSession) . "\";\"" . $listTasks->tas_start_date[$i] . "\";\"" . $listTasks->tas_due_date[$i] . "\";\"" . $listTasks->tas_complete_date[$i] . "\";\"" . $complValue . "\";\"$diff\";\"" . $listTasks->tas_estimated_time[$i] . "\";\"" . $listTasks->tas_actual_time[$i] . "\";\"" . $statusPublish[$idPublish] . "\";\"" . $listTasks->tas_comments[$i] . "\";\"" . $listTasks->tas_assigned[$i] . "\";\"" . $listTasks->tas_mem_login[$i] . "\"" . $crlf;
+        array_push($export_csv,
+            array(
+                $task["tas_name"],
+                $task["tas_description"],
+                $task["tas_mem2_login"],
+                $priority[$idPriority],
+                $status[$idStatus],
+                phpCollab\Util::createDate($task["tas_created"], $timezoneSession),
+                $task["tas_start_date"],
+                $task["tas_due_date"],
+                $task["tas_complete_date"],
+                $complValue,
+                $diff,
+                $task["tas_estimated_time"],
+                $task["tas_actual_time"],
+                $statusPublish[$idPublish],
+                $task["tas_comments"],
+                $task["tas_assigned"],
+                $task["tas_mem_login"]
+            )
+        );
     }
 }
+$filename = $strings["project"] . $projectDetail["pro_id"];
+$separator = ',';
 
-$filename = $strings["project"] . $projectDetail->pro_id[0];
-
-$ext = 'csv';
-$mime_type = 'text/x-csv';
-
-// Send headers
-header('Content-Type: ' . $mime_type);
-// lem9: we need "inline" instead of "attachment" for IE 5.5
-$content_disp = (USR_BROWSER_AGENT == 'IE') ? 'inline' : 'attachment';
-header('Content-Disposition:  ' . $content_disp . '; filename="' . $filename . '.' . $ext . '"');
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="' . $filename . '.csv"');
 header('Pragma: no-cache');
 header('Expires: 0');
 
-
-echo $dump_buffer;
+$fp = fopen('php://output', 'wb');
+foreach ($export_csv as $line) {
+    fputcsv($fp, $line, $separator);
+}
+fclose($fp);
