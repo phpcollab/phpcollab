f0ec0c54ca68a46a04c0a74762e2271feb642ac2
Jeff Sittler <mindblender@gmail.com>
2019-05-09 20:18:34 -0700

    refactor: Code clean up and refactor to use class methods

--- projects/viewprojectsite.php
+++ projects/viewprojectsite.php
@@ -1,9 +1,6 @@
 <?php
 /*
 ** Application name: phpCollab
-** Last Edit page: 23/05/2005
-** Path by root: ../projects/viewprojectsite.php
-** Authors: Ceam / Fullo
 ** =============================================================================
 **
 **               phpCollab - Project Managment
@@ -16,22 +13,20 @@
 **
 ** DESC: screen: view client info and projects
 **
-** HISTORY:
-** 	23/05/2005	-	added new document info
-**  23/05/2005	-	fix for http://www.php-collab.org/community/viewtopic.php?p=7124
-** -----------------------------------------------------------------------------
-** TO-DO:
-**
 ** =============================================================================
 */
+
+use phpCollab\Projects\Projects;
+use phpCollab\Teams\Teams;
 
 $checkSession = "true";
 include_once '../includes/library.php';
 
-$teams = new \phpCollab\Teams\Teams();
+$projects = new Projects();
+$teams = new Teams();
 
-if ($action == "publish") {
-    if ($addToSiteTeam == "true") {
+if ($_GET["action"] == "publish") {
+    if ($_GET["addToSiteTeam"] == "true") {
         $multi = strstr($id, "**");
         if ($multi != "") {
             $id = str_replace("**", ",", $id);
@@ -41,7 +36,7 @@ if ($action == "publish") {
         $id = $project;
     }
 
-    if ($removeToSiteTeam == "true") {
+    if ($_GET["removeToSiteTeam"] == "true") {
         $multi = strstr($id, "**");
         if ($multi != "") {
             $id = str_replace("**", ",", $id);
@@ -56,37 +51,24 @@ if ($msg == "demo") {
     $id = $project;
 }
 
-$tmpquery = "WHERE pro.id = '$id'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
-$comptProjectDetail = count($projectDetail->pro_id);
+$projectDetail = $projects->getProjectById($id);
 
-$teamMember = "false";
-$tmpquery = "WHERE tea.project = '$id' AND tea.member = '$idSession'";
-$memberTest = new phpCollab\Request();
-$memberTest->openTeams($tmpquery);
-$comptMemberTest = count($memberTest->tea_id);
-
-if ($comptMemberTest == "0") {
-    $teamMember = "false";
-} else {
-    $teamMember = "true";
-}
+$memberTest = $teams->isTeamMember($id, $idSession);
 
 if ($teamMember == "false" && $projectsFilter == "true") {
     header("Location:../general/permissiondenied.php");
 }
 
-if ($comptPro == "0") {
+if (empty($projectDetail)) {
     phpCollab\Util::headerFunction("../projects/listprojects.php?msg=blankProject");
 }
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=$id", $projectDetail->pro_name[0], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=$id", $projectDetail["pro_name"], "in"));
 $blockPage->itemBreadcrumbs($strings["project_site"]);
 $blockPage->closeBreadcrumbs();
 
@@ -98,10 +80,10 @@ if ($msg != "") {
 $block1 = new phpCollab\Block();
 $block1->form = "uploadlogo";
 $block1->form = "pdD";
-$block1->openForm("../projects/viewprojectsite.php?action=update&id=$id&#" . $block1->form . "Anchor");
-$block1->heading($strings["project_site"] . " : " . $projectDetail->pro_name[0]);
+$block1->openForm("../projects/viewprojectsite.php?id=$id&#" . $block1->form . "Anchor");
+$block1->heading($strings["project_site"] . " : " . $projectDetail["pro_name"]);
 
-if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
+if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
     $block1->openPaletteIcon();
     $block1->paletteIcon(0, "remove", $strings["delete"]);
     $block1->closePaletteIcon();
@@ -109,30 +91,31 @@ if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
 
 $block1->openContent();
 $block1->contentTitle($strings["details"]);
-$block1->contentRow($strings["project"], $blockPage->buildLink("../projects/viewproject.php?id=$id", $projectDetail->pro_name[0] . " (#" . $projectDetail->pro_id[0] . ")", in));
+$block1->contentRow($strings["project"], $blockPage->buildLink("../projects/viewproject.php?id=$id", $projectDetail["pro_name"] . " (#" . $projectDetail["pro_id"] . ")", "in"));
 
-if ($projectDetail->pro_org_id[0] == "1") {
+if ($projectDetail["pro_org_id"] == "1") {
     $block1->contentRow($strings["organization"], $strings["none"]);
 } else {
-    $block1->contentRow($strings["organization"], $blockPage->buildLink("../clients/viewclient.php?id=" . $projectDetail->pro_org_id[0], $projectDetail->pro_org_name[0], in));
+    $block1->contentRow($strings["organization"], $blockPage->buildLink("../clients/viewclient.php?id=" . $projectDetail["pro_org_id"], $projectDetail["pro_org_name"], "in"));
 }
 
 $block1->closeContent();
 $block1->closeForm();
 
-if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
+if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
     $block1->openPaletteScript();
     $block1->paletteScript(0, "remove", "../projects/deleteprojectsite.php?project=$id", "true,true,true", $strings["delete"]);
-    $block1->closePaletteScript("", "");
+    $block1->closePaletteScript(1, $projectDetail["pro_id"]);
 }
 
-if ($projectDetail->pro_organization[0] != "" && $projectDetail->pro_organization[0] != "1") {
+if ($projectDetail["pro_organization"] != "" && $projectDetail["pro_organization"] != "1") {
     $block2 = new phpCollab\Block();
     $block2->form = "csU";
-    $block2->openForm("../projects/viewprojectsite.php?&id=" . $projectDetail->pro_id[0] . "#" . $block2->form . "Anchor");
+    $block2->openForm("../projects/viewprojectsite.php?action=update&id=" . $projectDetail["pro_id"] . "#" . $block2->form . "Anchor");
+
     $block2->heading($strings["permitted_client"]);
 
-    if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
+    if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
         $block2->openPaletteIcon();
         $block2->paletteIcon(0, "add", $strings["add"]);
         $block2->paletteIcon(1, "remove", $strings["delete"]);
@@ -146,35 +129,32 @@ if ($projectDetail->pro_organization[0] != "" && $projectDetail->pro_organizatio
 
     $block2->sorting("team", $sortingUser["team"], "mem.name ASC", $sortingFields = array(0 => "mem.name", 1 => "mem.title", 2 => "mem.login", 3 => "mem.phone_work", 4 => "log.connected", 5 => "tea.published"));
 
-    $tmpquery = "WHERE tea.project = '$id' AND mem.profil = '3' ORDER BY $block2->sortingValue";
-    $listPermitted = new phpCollab\Request();
-    $listPermitted->openTeams($tmpquery);
-    $comptListPermitted = count($listPermitted->tea_id);
+    $listPermitted = $teams->getClientTeamMembersByProject($id, $block2->sortingValue);
 
-    if ($comptListPermitted != "0") {
+    if ($listPermitted) {
         $block2->openResults();
         $block2->labels($labels = array(0 => $strings["full_name"], 1 => $strings["title"], 2 => $strings["user_name"], 3 => $strings["work_phone"], 4 => $strings["connected"], 5 => $strings["published"]), "true");
 
-        for ($i = 0; $i < $comptListPermitted; $i++) {
-            if ($listPermitted->tea_mem_phone_work[$i] == "") {
-                $listPermitted->tea_mem_phone_work[$i] = $strings["none"];
+        foreach ($listPermitted as $permitted) {
+            if ($permitted["tea_mem_phone_work"] == "") {
+                $permitted["tea_mem_phone_work"] = $strings["none"];
             }
 
-            $idPublish = $listPermitted->tea_published[$i];
+            $idPublish = $permitted["tea_published"];
             $block2->openRow();
-            $block2->checkboxRow($listPermitted->tea_mem_id[$i]);
-            $block2->cellRow($blockPage->buildLink("../users/viewclientuser.php?id=" . $listPermitted->tea_mem_id[$i] . "&organization=" . $projectDetail->pro_organization[0], $listPermitted->tea_mem_name[$i], in));
-            $block2->cellRow($listPermitted->tea_mem_title[$i]);
-            $block2->cellRow($blockPage->buildLink($listPermitted->tea_mem_email_work[$i], $listPermitted->tea_mem_login[$i], mail));
-            $block2->cellRow($listPermitted->tea_mem_phone_work[$i]);
+            $block2->checkboxRow($permitted["tea_mem_id"]);
+            $block2->cellRow($blockPage->buildLink("../users/viewclientuser.php?id=" . $permitted["tea_mem_id"] . "&organization=" . $projectDetail["pro_organization"], $permitted["tea_mem_name"], "in"));
+            $block2->cellRow($permitted["tea_mem_title"]);
+            $block2->cellRow($blockPage->buildLink($permitted["tea_mem_email_work"], $permitted["tea_mem_login"], "mail"));
+            $block2->cellRow($permitted["tea_mem_phone_work"]);
 
-            if ($listPermitted->tea_mem_profil[$i] == "3") {
+            if ($permitted["tea_mem_profil"] == "3") {
                 $z = "(Client on project site)";
             } else {
                 $z = "";
             }
 
-            if ($listPermitted->tea_log_connected[$i] > $dateunix - 5 * 60) {
+            if ($permitted["tea_log_connected"] > $dateunix - 5 * 60) {
                 $block2->cellRow($strings["yes"] . " " . $z);
             } else {
                 $block2->cellRow($strings["no"]);
@@ -193,17 +173,17 @@ if ($projectDetail->pro_organization[0] != "" && $projectDetail->pro_organizatio
 
     $block2->closeFormResults();
 
-    if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
+    if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
         $block2->openPaletteScript();
         $block2->paletteScript(0, "add", "../teams/addclientuser.php?project=$id", "true,false,false", $strings["add"]);
         $block2->paletteScript(1, "remove", "../teams/deleteclientusers.php?project=$id", "false,true,true", $strings["delete"]);
 
         if ($sitePublish == "true") {
-            $block2->paletteScript(2, "add_projectsite", "../projects/viewprojectsite.php?addToSiteTeam=true&project=" . $projectDetail->pro_id[0] . "&action=publish", "false,true,true", $strings["add_project_site"]);
-            $block2->paletteScript(3, "remove_projectsite", "../projects/viewprojectsite.php?removeToSiteTeam=true&project=" . $projectDetail->pro_id[0] . "&action=publish", "false,true,true", $strings["remove_project_site"]);
+            $block2->paletteScript(2, "add_projectsite", "../projects/viewprojectsite.php?addToSiteTeam=true&project=" . $projectDetail["pro_id"] . "&action=publish", "false,true,true", $strings["add_project_site"]);
+            $block2->paletteScript(3, "remove_projectsite", "../projects/viewprojectsite.php?removeToSiteTeam=true&project=" . $projectDetail["pro_id"] . "&action=publish", "false,true,true", $strings["remove_project_site"]);
         }
-        $block2->closePaletteScript($comptListPermitted, $listPermitted->tea_mem_id);
+        $block2->closePaletteScript(count($listPermitted), $listPermitted[0]["tea_mem_id"]);
     }
 }
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
