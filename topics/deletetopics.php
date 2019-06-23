232dead08f16cdbba7757d1c014d7ed8cf4fd69d
Jeff Sittler <mindblender@gmail.com>
2019-06-23 14:18:08 -0700

    refactor: Code Refactor and clean up
    Refactor the code to use class methods

--- topics/deletetopics.php
+++ topics/deletetopics.php
@@ -3,18 +3,31 @@
 #Status page: 0
 #Path by root: ../topics/deletetopics.php
 
+use phpCollab\Projects\Projects;
+use phpCollab\Topics\Topics;
+
 $checkSession = "true";
 include_once '../includes/library.php';
+
+$projects = new Projects();
+$topics = new Topics();
 
 $action = $_GET["action"];
 $id = $_GET["id"];
 
-if ($action == "delete") {
+if ($_GET["action"] == "delete") {
     $id = str_replace("**", ",", $id);
     $pieces = explode(",", $id);
     $num = count($pieces);
-    phpCollab\Util::newConnectSql("DELETE FROM {$tableCollab["topics"]} WHERE id = :topic_id", ["topic_id" => $id]);
-    phpCollab\Util::newConnectSql("DELETE FROM {$tableCollab["posts"]} WHERE topic = :topic_id", ["topic_id" => $id]);
+
+    try {
+        $topics->deleteTopics($pieces);
+        $topics->deletePostsFromTopics($pieces);
+
+    } catch (Exception $e) {
+        // handle exception
+    }
+
     if ($project != "") {
         phpCollab\Util::headerFunction("../projects/viewproject.php?num=$num&msg=deleteTopic&id=$project");
     } else {
@@ -26,18 +39,18 @@ if ($_GET['project']) {
 } else {
     unset($project);
 }
-$tmpquery = "WHERE pro.id = '$project'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
+$projectDetail = $projects->getProjectById($project);
 
-include '../themes/' . THEME . '/header.php';
+$setTitle .= " : " . $strings["delete_discussions"];
+
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
 if ($project != "") {
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail->pro_id[0], $projectDetail->pro_name[0], "in"));
-    $blockPage->itemBreadcrumbs($blockPage->buildLink("../topics/listtopics.php?project=" . $projectDetail->pro_id[0], $strings["discussions"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail["pro_id"], $projectDetail["pro_name"], "in"));
+    $blockPage->itemBreadcrumbs($blockPage->buildLink("../topics/listtopics.php?project=" . $projectDetail["pro_id"], $strings["discussions"], "in"));
 } else {
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../general/home.php?", $strings["home"], "in"));
     $blockPage->itemBreadcrumbs($blockPage->buildLink("../topics/listtopics.php?", $strings["my_discussions"], "in"));
@@ -62,18 +75,25 @@ $block1->openContent();
 $block1->contentTitle($strings["delete_following"]);
 
 $id = str_replace("**", ",", $id);
-$tmpquery = "WHERE topic.id IN($id) ORDER BY topic.subject";
-$listTopics = new phpCollab\Request();
-$listTopics->openTopics($tmpquery);
-$comptListTopics = count($listTopics->top_id);
+$listTopics = $topics->getTopicsIn($id);
 
-for ($i = 0; $i < $comptListTopics; $i++) {
-    echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">&nbsp;</td><td>" . $listTopics->top_subject[$i] . "</td></tr>";
+foreach ($listTopics as $listTopic) {
+    echo <<<TR
+        <tr class="odd">
+            <td class="leftvalue">&nbsp;</td>
+            <td>{$listTopic["top_subject"]}</td>
+        </tr>
+TR;
 }
 
-echo "<tr class=\"odd\"><td valign=\"top\" class=\"leftvalue\">&nbsp;</td><td><input type=\"submit\" name=\"delete\" value=\"" . $strings["delete"] . "\"> <input type=\"button\" name=\"cancel\" value=\"" . $strings["cancel"] . "\" onClick=\"history.back();\"></td></tr>";
+echo <<<TR
+    <tr class="odd">
+        <td class="leftvalue">&nbsp;</td>
+        <td><input type="submit" name="delete" value="{$strings["delete"]}"> <input type="button" name="cancel" value="{$strings["cancel"]}" onClick="history.back();"></td>
+    </tr>
+TR;
 
 $block1->closeContent();
 $block1->closeForm();
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
