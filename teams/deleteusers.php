ca3d9b6b38866f882c9a4c6c0e96e898e299943d
Jeff Sittler <mindblender@gmail.com>
2019-06-24 00:37:47 -0700

    refactor: Code clean up and refactor to use class methods

--- teams/deleteusers.php
+++ teams/deleteusers.php
@@ -3,100 +3,113 @@
 #Status page: 1
 #Path by root: ../teams/deleteusers.php
 
+use phpCollab\Members\Members;
+use phpCollab\Projects\Projects;
+use phpCollab\Teams\Teams;
+
 $checkSession = "true";
 include_once '../includes/library.php';
+
+$projects = new Projects();
+$members = new Members();
+$teams = new Teams();
 
 $id = $_GET["id"];
 $project = $_GET["project"];
 $action = $_GET["action"];
 
-$tmpquery = "WHERE pro.id = '$project'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
-$comptProjectDetail = count($projectDetail->pro_id);
+$projectDetail = $projects->getProjectById($project);
 
 //test exists selected project, redirect to list if not
-if ($comptProjectDetail == "0") {
+if (empty($projectDetail)) {
     phpCollab\Util::headerFunction("../projects/listprojects.php?msg=blank");
 }
 
+$id = str_replace("**", ",", $id);
+
+$listMembers = $members->getMembersByIdIn($id);
+
 if ($action == "delete") {
-    $id = str_replace("**", ",", $id);
+    if (!empty($listMembers)) {
+        if ($htaccessAuth == "true") {
+            $Htpasswd = new Htpasswd;
+            $Htpasswd->initialize("../files/" . $projectDetail["pro_id"] . "/.htpasswd");
 
-    if ($htaccessAuth == "true") {
-        $Htpasswd = new Htpasswd;
-        $Htpasswd->initialize("../files/" . $projectDetail->pro_id[0] . "/.htpasswd");
-
-        $tmpquery = "WHERE mem.id IN($id)";
-        $listMembers = new phpCollab\Request();
-        $listMembers->openMembers($tmpquery);
-        $comptListMembers = count($listMembers->mem_id);
-
-        for ($i = 0; $i < $comptListMembers; $i++) {
-            $Htpasswd->deleteUser($listMembers->mem_login[$i]);
+            foreach ($listMembers as $listMember) {
+                try {
+                    $Htpasswd->deleteUser($listMembers["mem_login"]);
+                } catch (Exception $e) {
+                    // Log exception
+                }
+            }
         }
-    }
-    //if mantis bug tracker enabled
-    if ($enableMantis == "true") {
-        //  include mantis library
-        include '../mantis/core_API.php';
-    }
+        //if mantis bug tracker enabled
+        if ($enableMantis == "true") {
+            //  include mantis library
+            include '../mantis/core_API.php';
+        }
 
-    $multi = strstr($id, ",");
-    if ($multi != "") {
-        $pieces = explode(",", $id);
-        $compt = count($pieces);
-        for ($i = 0; $i < $compt; $i++) {
-            if ($projectDetail->pro_owner[0] != $pieces[$i]) {
-                phpCollab\Util::newConnectSql("DELETE FROM {$tableCollab["teams"]} WHERE member = :member AND project = :project", ["member" => $pieces[$i], "project" => $project]);
+        $multi = strstr($id, ",");
+        if ($multi != "") {
+            $pieces = explode(",", $id);
+            foreach ($pieces as $piece) {
+
+                if ($projectDetail["pro_owner"] != $piece) {
+                    $teams->deleteFromTeamsByProjectIdAndMemberId($project, $piece);
+
+                    //if mantis bug tracker enabled
+                    if ($enableMantis == "true") {
+                        // Unassign multiple user from this project in mantis
+                        $f_project_id = $project;
+                        $f_user_id = $piece;
+                        include '../mantis/user_proj_delete.php';
+                    }
+                }
+                if ($projectDetail["pro_owner"] == $piece) {
+                    $foundOwner = "true";
+                }
+            }
+            if ($foundOwner == "true") {
+                $msg = "deleteTeamOwnerMix";
+            } else {
+                $msg = "delete";
+            }
+        } else {
+            if ($projectDetail["pro_owner"] == $id) {
+                $msg = "deleteTeamOwner";
+            } else {
+                $teams->deleteFromTeamsByProjectIdAndMemberId($project, $id);
+                $msg = "delete";
 
                 //if mantis bug tracker enabled
                 if ($enableMantis == "true") {
-                    // Unassign multiple user from this project in mantis
+                    // Unassign single user from this project in mantis
                     $f_project_id = $project;
-                    $f_user_id = $pieces[$i];
+                    $f_user_id = $id;
                     include '../mantis/user_proj_delete.php';
                 }
             }
-            if ($projectDetail->pro_owner[0] == $pieces[$i]) {
-                $foundOwner = "true";
+        }
+
+        if ($notifications == "true") {
+            try {
+                $teams->sendRemoveProjectTeamNotification($projectDetail, $id);
+            } catch (Exception $e) {
+                // log exception
             }
         }
-        if ($foundOwner == "true") {
-            $msg = "deleteTeamOwnerMix";
-        } else {
-            $msg = "delete";
-        }
-    } else {
-        if ($projectDetail->pro_owner[0] == $id) {
-            $msg = "deleteTeamOwner";
-        } else {
-            phpCollab\Util::newConnectSql("DELETE FROM {$tableCollab["teams"]} WHERE member = :member AND project = :project", ["member" => $id, "project" => $project]);
-            $msg = "delete";
 
-            //if mantis bug tracker enabled
-            if ($enableMantis == "true") {
-                // Unassign single user from this project in mantis
-                $f_project_id = $project;
-                $f_user_id = $id;
-                include '../mantis/user_proj_delete.php';
-            }
-        }
+        phpCollab\Util::headerFunction("../projects/viewproject.php?id=$project&msg=$msg");
     }
 
-    if ($notifications == "true") {
-        $organization = "1";
-        include '../teams/noti_removeprojectteam.php';
-    }
-    phpCollab\Util::headerFunction("../projects/viewproject.php?id=$project&msg=$msg");
 }
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
 $blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail->pro_id[0], $projectDetail->pro_name[0], "in"));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail["pro_id"], $projectDetail["pro_name"], "in"));
 $blockPage->itemBreadcrumbs($strings["remove_team"]);
 $blockPage->closeBreadcrumbs();
 
@@ -115,19 +128,13 @@ $block1->heading($strings["remove_team"]);
 $block1->openContent();
 $block1->contentTitle($strings["remove_team_info"]);
 
-$id = str_replace("**", ",", $id);
-$tmpquery = "WHERE mem.id IN($id) ORDER BY mem.name";
-$listMembers = new phpCollab\Request();
-$listMembers->openMembers($tmpquery);
-$comptListMembers = count($listMembers->mem_id);
-
-for ($i = 0; $i < $comptListMembers; $i++) {
-    $block1->contentRow("#" . $listMembers->mem_id[$i], $listMembers->mem_login[$i] . " (" . $listMembers->mem_name[$i] . ")");
+foreach ($listMembers as $listMember) {
+    $block1->contentRow("#" . $listMember["mem_id"], $listMember["mem_login"] . " (" . $listMember["mem_name"] . ")");
 }
 
-$block1->contentRow("", "<input type=\"SUBMIT\" value=\"" . $strings["remove"] . "\">&#160;<input type=\"BUTTON\" value=\"" . $strings["cancel"] . "\" onClick=\"history.back();\">");
+$block1->contentRow("", '<input type="SUBMIT" value="' . $strings["remove"] . '">&#160;<input type="BUTTON" value="' . $strings["cancel"] . '" onClick="history.back();">');
 
 $block1->closeContent();
 $block1->closeForm();
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
--- teams/listusers.php
+++ teams/listusers.php
@@ -28,24 +28,27 @@
 */
 
 
+use phpCollab\Projects\Projects;
+use phpCollab\Teams\Teams;
+
 $checkSession = "true";
 include_once '../includes/library.php';
 
-$tmpquery = "WHERE pro.id = '$id'";
-$projectDetail = new phpCollab\Request();
-$projectDetail->openProjects($tmpquery);
-$comptProjectDetail = count($projectDetail->pro_id);
+$projects = new Projects();
+$teams = new Teams();
 
-if ($comptProjectDetail == "0") {
+$projectDetail = $projects->getProjectById($id);
+
+if (empty($projectDetail)) {
     phpCollab\Util::headerFunction("../projects/listprojects.php?msg=blank");
 }
 
-include '../themes/' . THEME . '/header.php';
+include APP_ROOT . '/themes/' . THEME . '/header.php';
 
 $blockPage = new phpCollab\Block();
 $blockPage->openBreadcrumbs();
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], in));
-$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail->pro_id[0], $projectDetail->pro_name[0], in));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/listprojects.php?", $strings["projects"], "in"));
+$blockPage->itemBreadcrumbs($blockPage->buildLink("../projects/viewproject.php?id=" . $projectDetail["pro_id"], $projectDetail["pro_name"], "in"));
 $blockPage->itemBreadcrumbs($strings["team_members"]);
 $blockPage->closeBreadcrumbs();
 
@@ -58,7 +61,7 @@ $block1->heading($strings["team_members"]);
 
 $block1->openPaletteIcon();
 
-if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
+if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
     $block1->paletteIcon(0, "add", $strings["add"]);
     $block1->paletteIcon(1, "remove", $strings["delete"]);
 
@@ -74,30 +77,28 @@ $block1->closePaletteIcon();
 
 $block1->sorting("team", $sortingUser["team"], "mem.name ASC", $sortingFields = array(0 => "mem.name", 1 => "mem.title", 2 => "mem.login", 3 => "mem.phone_work", 4 => "log.connected", 5 => "tea.published"));
 
-$tmpquery = "WHERE tea.project = '$id' AND mem.profil != '3' ORDER BY $block1->sortingValue";
-$listTeam = new phpCollab\Request();
-$listTeam->openTeams($tmpquery);
-$comptListTeam = count($listTeam->tea_id);
+$listTeam = $teams->getTeamByProjectId($id, null, null, $block1->sortingValue);
 
 $block1->openResults();
 
 $block1->labels($labels = array(0 => $strings["full_name"], 1 => $strings["title"], 2 => $strings["user_name"], 3 => $strings["work_phone"], 4 => $strings["connected"], 5 => $strings["published"]), "true");
 
-for ($i = 0; $i < $comptListTeam; $i++) {
-    if ($listTeam->tea_mem_phone_work[$i] == "") {
-        $listTeam->tea_mem_phone_work[$i] = $strings["none"];
+foreach ($listTeam as $teamMember) {
+
+    if ($teamMember["tea_mem_phone_work"] == "") {
+        $teamMember["tea_mem_phone_work"] = $strings["none"];
     }
 
-    $idPublish = $listTeam->tea_published[$i];
+    $idPublish = $teamMember["tea_published"];
 
     $block1->openRow();
-    $block1->checkboxRow($listTeam->tea_mem_id[$i]);
-    $block1->cellRow($blockPage->buildLink("../users/viewuser.php?id=" . $listTeam->tea_mem_id[$i], $listTeam->tea_mem_name[$i], in));
-    $block1->cellRow($listTeam->tea_mem_title[$i]);
-    $block1->cellRow($blockPage->buildLink($listTeam->tea_mem_email_work[$i], $listTeam->tea_mem_login[$i], mail));
-    $block1->cellRow($listTeam->tea_mem_phone_work[$i]);
+    $block1->checkboxRow($teamMember["tea_mem_id"]);
+    $block1->cellRow($blockPage->buildLink("../users/viewuser.php?id=" . $teamMember["tea_mem_id"], $teamMember["tea_mem_name"], "in"));
+    $block1->cellRow($teamMember["tea_mem_title"]);
+    $block1->cellRow($blockPage->buildLink($teamMember["tea_mem_email_work"], $teamMember["tea_mem_login"], "mail"));
+    $block1->cellRow($teamMember["tea_mem_phone_work"]);
 
-    if ($listTeam->tea_log_connected[$i] > $dateunix - 5 * 60) {
+    if ($teamMember["tea_log_connected"] > $dateunix - 5 * 60) {
         $block1->cellRow($strings["yes"] . " " . $z);
     } else {
         $block1->cellRow($strings["no"]);
@@ -115,17 +116,17 @@ $block1->closeResults();
 $block1->closeFormResults();
 
 $block1->openPaletteScript();
-if ($idSession == $projectDetail->pro_owner[0] || $profilSession == "5") {
-    $block1->paletteScript(0, "add", "../teams/adduser.php?project=" . $projectDetail->pro_id[0] . "", "true,true,true", $strings["add"]);
-    $block1->paletteScript(1, "remove", "../teams/deleteusers.php?project=" . $projectDetail->pro_id[0] . "", "false,true,true", $strings["delete"]);
+if ($idSession == $projectDetail["pro_owner"] || $profilSession == "5") {
+    $block1->paletteScript(0, "add", "../teams/adduser.php?project=" . $projectDetail["pro_id"] . "", "true,true,true", $strings["add"]);
+    $block1->paletteScript(1, "remove", "../teams/deleteusers.php?project=" . $projectDetail["pro_id"] . "", "false,true,true", $strings["delete"]);
 
     if ($sitePublish == "true") {
-        $block1->paletteScript(2, "add_projectsite", "../projects/viewproject.php?addToSiteTeam=true&project=" . $projectDetail->pro_id[0] . "&action=publish", "false,true,true", $strings["add_project_site"]);
-        $block1->paletteScript(3, "remove_projectsite", "../projects/viewproject.php?removeToSiteTeam=true&project=" . $projectDetail->pro_id[0] . "&action=publish", "false,true,true", $strings["remove_project_site"]);
+        $block1->paletteScript(2, "add_projectsite", "../projects/viewproject.php?addToSiteTeam=true&project=" . $projectDetail["pro_id"] . "&action=publish", "false,true,true", $strings["add_project_site"]);
+        $block1->paletteScript(3, "remove_projectsite", "../projects/viewproject.php?removeToSiteTeam=true&project=" . $projectDetail["pro_id"] . "&action=publish", "false,true,true", $strings["remove_project_site"]);
     }
 }
 $block1->paletteScript(4, "info", "../users/viewuser.php?", "false,true,false", $strings["view"]);
 $block1->paletteScript(5, "email", "../users/emailusers.php?", "false,true,true", $strings["email"]);
-$block1->closePaletteScript($comptListTeam, $listTeam->tea_mem_id);
+$block1->closePaletteScript(count($listTeam), $listTeam[0]["tea_mem_id"]);
 
-include '../themes/' . THEME . '/footer.php';
+include APP_ROOT . '/themes/' . THEME . '/footer.php';
