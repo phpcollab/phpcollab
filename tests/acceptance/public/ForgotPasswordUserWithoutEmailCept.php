beaf0c5476b042255a609d6a038cae068536679f
Jeff Sittler <mindblender@gmail.com>
2019-07-19 17:09:24 -0700

    fix: changed the login username to a correct user

--- tests/acceptance/public/ForgotPasswordUserWithoutEmailCept.php
+++ tests/acceptance/public/ForgotPasswordUserWithoutEmailCept.php
@@ -2,7 +2,7 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('forgot password');
 $I->amOnPage('/general/sendpassword.php');
-$I->fillField(['name' => 'loginForm'], 'deleteuser5');
+$I->fillField(['name' => 'loginForm'], 'noemailuser');
 $I->click('input[type="submit"]');
 $I->seeInCurrentUrl('/general/sendpassword.php');
 $I->seeElement('.headingError');
