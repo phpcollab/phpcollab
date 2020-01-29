5ff35a4bd97b5dc0d580a7fa9c3f089f2360deb2
Jeff Sittler <mindblender@gmail.com>
2020-01-29 13:52:28 -0700

    feat(Subtasks): Adding new classes
    Adding Subtasks and SubtasksGateway classes

---
+++ classes/Subtasks/Subtasks.php
@@ -0,0 +1,207 @@
+<?php
+
+
+namespace phpCollab\Subtasks;
+
+use Exception;
+use phpCollab\Database;
+use phpCollab\Notifications\Notifications;
+use phpCollab\Notifications\SubtaskNotifications;
+
+class Subtasks
+{
+    protected $subtasks_gateway;
+    protected $db;
+    protected $strings;
+    protected $notifications;
+    protected $notificationsList;
+    protected $subtaskNotifications;
+    private $send;
+
+    /**
+     * Subtasks constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->subtasks_gateway = new SubtasksGateway($this->db);
+        $this->notifications = new Notifications();
+        $this->subtaskNotifications = new SubtaskNotifications();
+        $this->strings = $GLOBALS["strings"];
+    }
+
+    /**
+     * @param $subtaskId
+     * @return mixed
+     */
+    public function getById($subtaskId)
+    {
+        $taskId = filter_var($subtaskId, FILTER_VALIDATE_INT);
+        return $this->subtasks_gateway->getById($taskId);
+
+    }
+
+    /**
+     * @param $parentTaskId
+     * @param $name
+     * @param $description
+     * @param $owner
+     * @param $assignedTo
+     * @param $status
+     * @param $priority
+     * @param $startDate
+     * @param $dueDate
+     * @param $estimatedTime
+     * @param $actualTime
+     * @param $comments
+     * @param $completion
+     * @param $published
+     * @return string
+     */
+    public function add($parentTaskId, $name, $description, $owner, $assignedTo, $status, $priority, $startDate,
+                        $dueDate, $estimatedTime, $actualTime, $comments, $completion, $published)
+    {
+        $created = date('Y-m-d h:i');
+        return $this->subtasks_gateway->addSubtask($parentTaskId, $name, $description, $owner, $assignedTo, $status, $priority,
+            $startDate, $dueDate, $estimatedTime, $actualTime, $comments, $completion, $published, $created);
+    }
+
+    /**
+     * @param $subtaskId
+     * @param $name
+     * @param $description
+     * @param $assignedTo
+     * @param $status
+     * @param $priority
+     * @param $startDate
+     * @param $dueDate
+     * @param $estimatedTime
+     * @param $actualTime
+     * @param $comments
+     * @param $modified
+     * @param $completion
+     * @param $published
+     * @return mixed
+     */
+    public function update($subtaskId, $name, $description, $assignedTo, $status, $priority, $startDate, $dueDate,
+                           $estimatedTime, $actualTime, $comments, $modified, $completion, $published)
+    {
+
+        $this->subtasks_gateway->updateSubtask($subtaskId, $name, $description, $assignedTo, $status, $priority, $startDate, $dueDate,
+            $estimatedTime, $actualTime, $comments, $modified, $completion, $published);
+        return $this->getById($subtaskId);
+    }
+
+    /**
+     * @param $subtaskId
+     * @return mixed
+     */
+    public function publish($subtaskId)
+    {
+        return $this->subtasks_gateway->publishSubtask($subtaskId);
+    }
+
+    /**
+     * @param $subtaskId
+     * @return mixed
+     */
+    public function unpublish($subtaskId)
+    {
+        return $this->subtasks_gateway->unpublishSubtask($subtaskId);
+    }
+
+    /**
+     * @param $subtaskId
+     * @param $date
+     * @return mixed
+     */
+    public function setCompletionDate($subtaskId, $date)
+    {
+        return $this->subtasks_gateway->setCompletionDate($subtaskId, $date);
+
+    }
+
+    /**
+     * @param $subtaskId
+     * @param $date
+     * @return mixed
+     */
+    public function setAssignedDate($subtaskId, $date)
+    {
+        return $this->subtasks_gateway->setAssignedDate($subtaskId, $date);
+    }
+
+
+    /**
+     * string @param $notification
+     * array @param $subtaskDetails
+     * array @param $projectDetails
+     * @throws Exception
+     */
+    public function sendNotification($notification, $subtaskDetails, $projectDetails)
+    {
+        $this->send = false;
+
+        // Get a list of notifications to be used
+        if (empty($this->notificationsList)) {
+            $this->notificationsList = $this->notifications->getMemberNotifications($subtaskDetails["subtas_assigned_to"]);
+        }
+
+        if (!empty($this->notificationsList["email_work"])) {
+            $this->subtaskNotifications->setWorkEmail($this->notificationsList["email_work"]);
+            $this->subtaskNotifications->setUserName($this->notificationsList["name"]);
+
+        }
+
+        if (empty($this->subtaskNotifications->getTaskDetails())) {
+            $this->subtaskNotifications->setTaskDetails($subtaskDetails);
+        }
+
+        if (empty($this->subtaskNotifications->getProjectDetails())) {
+            $this->subtaskNotifications->setProjectDetails($projectDetails);
+        }
+
+        switch ($notification) {
+            case "priority":
+                if ($this->notificationsList["priorityTaskChange"] == "0") {
+                    $this->subtaskNotifications->setSubject($this->strings["noti_prioritytaskchange1"]);
+                    $this->subtaskNotifications->setBody($this->strings["noti_prioritytaskchange2"]);
+                    $this->send = true;
+                }
+                break;
+            case "status":
+                if ($this->notificationsList["statusTaskChange"] == "0") {
+                    $this->subtaskNotifications->setSubject($this->strings["noti_statustaskchange1"]);
+                    $this->subtaskNotifications->setBody($this->strings["noti_statustaskchange2"]);
+                    $this->send = true;
+                }
+                break;
+            case "dueDate":
+                if ($this->notificationsList["duedateTaskChange"] == "0") {
+                    $this->subtaskNotifications->setSubject($this->strings["noti_duedatetaskchange1"]);
+                    $this->subtaskNotifications->setBody($this->strings["noti_duedatetaskchange2"]);
+                    $this->send = true;
+                }
+                break;
+            case "assignment":
+                if ($this->notificationsList["assignmentTaskChange"] == "0") {
+                    $this->subtaskNotifications->setSubject($this->strings["noti_taskassignment1"]);
+                    $this->subtaskNotifications->setBody($this->strings["noti_taskassignment2"]);
+                    $this->send = true;
+                }
+                break;
+            default:
+                throw new Exception('Error sending notification');
+        }
+
+        if ($this->send) {
+            try {
+                $this->subtaskNotifications->sendEmail();
+            } catch (Exception $e) {
+                // Log this instead of echoing it?
+                throw new Exception($e->getMessage());
+            }
+        }
+        $this->send = false;
+    }
+}
