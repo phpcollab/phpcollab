185cb500a4ec8164fc17b00d35e82b6ea3f208f9
Jeff Sittler <mindblender@gmail.com>
2020-02-11 20:44:38 -0700

    refactor(Linked Content): Refactored to use the new GetFile class

--- linkedcontent/accessfile.php
+++ linkedcontent/accessfile.php
@@ -1,17 +1,43 @@
 <?php
 #Application name: PhpCollab
 #Status page: 0
-// no caching to keep phpCollab 2.0 behaviour
 use phpCollab\Files\Files;
+use phpCollab\Files\GetFile;
 
-session_cache_limiter('none');		// suppress error messages for PHP version < 4.0.2
+session_cache_limiter('none');        // suppress error messages for PHP version < 4.0.2
 error_reporting(0);
 
 $checkSession = "true";
-include '../includes/library.php';		// starts session and writes session cache headers
+include '../includes/library.php';    // starts session and writes session cache headers
 
 $files = new Files();
+
 $fileDetail = $files->getFileById($_GET["id"]);
 
-// serve the requested file
-include '../includes/download.php';
+if ($fileDetail) {
+    $fileAction = new GetFile();
+
+    try {
+        if (!empty($fileDetail["fil_vc_parent"])) {
+            $pos = strrpos($fileDetail["fil_name"], ".");
+            $filename = substr_replace($fileDetail["fil_name"], "_v{$fileDetail["fil_vc_version"]}.", $pos, 1);
+        }
+
+        if (empty($fileDetail["fil_task"])) {
+            $fileAction->setFilesPath(APP_ROOT . "/files/" . $fileDetail["fil_project"] . "/" . $filename);
+        } else {
+            $fileAction->setFilesPath(APP_ROOT . "/files/" . $fileDetail["fil_project"] . "/" . $fileDetail["fil_task"] . "/" . $filename);
+        }
+
+        if ($_GET["mode"] == "download") {
+            $fileAction->downloadFile($filename);
+        } elseif ($_GET["mode"] == "view") {
+            $fileAction->viewFile($filename);
+        }
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
+    }
+
+}
+
+
