8af942c2b5a3856b9d5d7beba78e5583423e2b9d
Jeff Sittler <mindblender@gmail.com>
2020-02-12 14:42:57 -0700

    refactor(Linked Content): Refactored to use Files classes

--- projects_site/clientaccessfile.php
+++ projects_site/clientaccessfile.php
@@ -1,23 +1,44 @@
 <?php
 #Application name: PhpCollab
 #Status page: 0
-// no caching to keep phpCollab 2.0 behaviour
 use phpCollab\Files\Files;
+use phpCollab\Files\GetFile;
 
-session_cache_limiter('none');   // suppress error messages for PHP version < 4.0.2
+session_cache_limiter('none');     // suppress error messages for PHP version < 4.0.2
 error_reporting(0);
 $checkSession = "true";
-include '../includes/library.php';        // starts session and writes session cache headers
-
-$id = $_GET["id"];
+include '../includes/library.php'; // starts session and writes session cache headers
 
 $files = new Files();
-$fileDetail = $files->getFileById($id);
 
-// test if file is published and part of the current project
-if ($fileDetail["fil_published"] == "1" || $fileDetail["fil_project"] != $projectSession) {
-    phpCollab\Util::headerFunction("index.php");
+$fileDetail = $files->getFileById($_GET["id"]);
+
+if ($fileDetail) {
+    // test if file is published and part of the current project
+    if ($fileDetail["fil_published"] == "1" || $fileDetail["fil_project"] != $projectSession) {
+        phpCollab\Util::headerFunction("index.php");
+    } else {
+        $fileAction = new GetFile();
+
+        try {
+            if (!empty($fileDetail["fil_vc_parent"])) {
+                $pos = strrpos($fileDetail["fil_name"], ".");
+                $filename = substr_replace($fileDetail["fil_name"], "_v{$fileDetail["fil_vc_version"]}.", $pos, 1);
+            }
+
+            if (empty($fileDetail["fil_task"])) {
+                $fileAction->setFilesPath(APP_ROOT . "/files/" . $fileDetail["fil_project"] . "/" . $filename);
+            } else {
+                $fileAction->setFilesPath(APP_ROOT . "/files/" . $fileDetail["fil_project"] . "/" . $fileDetail["fil_task"] . "/" . $filename);
+            }
+
+            if ($_GET["mode"] == "download") {
+                $fileAction->downloadFile($filename);
+            } elseif ($_GET["mode"] == "view") {
+                $fileAction->viewFile($filename);
+            }
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+        }
+    }
 }
-
-// serve the requested file
-include '../includes/download.php';
