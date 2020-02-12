f8a241f9f8bac3f2c987cbec7b803f4f5e6a03fb
Jeff Sittler <mindblender@gmail.com>
2020-02-11 19:41:59 -0700

    feat(Linked Content): Slight refactor on the download.php functionality
    Code clean-up
    
    download.php - using the symfony MimeType guesser method
    instead of relying on hard-coded values.

--- includes/download.php
+++ includes/download.php
@@ -1,42 +1,44 @@
 <?php
+use Symfony\Component\HttpFoundation\File\MimeType\FileinfoMimeTypeGuesser;
 
-// MIMETypes should be handled using PHP mime.magic once it's out
-include '../includes/mimetypes.php';
-$filespath = "../files";
+$filesPath = APP_ROOT . "/files";
 
 // construct file path and test whether file exists/is accessible
-$name = $fileDetail["fil_name"];
-$project = $fileDetail["fil_project"];
-$task = $fileDetail["fil_task"];
+$filename = $fileDetail["fil_name"];
+
+
+if (!empty($fileDetail["fil_vc_parent"])) {
+    $pos = strrpos($fileDetail["fil_name"], ".");
+    $filename = substr_replace($fileDetail["fil_name"], "_v{$fileDetail["fil_vc_version"]}.", $pos, 1);
+}
 
 // take care of subdirectories for files associated with tasks
-if ($task == "0") {
-    $path = $filespath . "/" . $project . "/" . $name;
+if (empty($fileDetail["fil_task"])) {
+    $path = $filesPath . "/" . $fileDetail["fil_project"] . "/" . $filename;
 } else {
-    $path = $filespath . "/" . $project . "/" . $task . "/" . $name;
+    $path = $filesPath . "/" . $fileDetail["fil_project"] . "/" . $fileDetail["fil_task"] . "/" . $filename;
 }
 
 try {
     if (!file_exists($path)) {
-        throw new \Exception("file does not exist");
+        throw new Exception("file does not exist");
     } else {
-        // figure out mimetype, should be done using PHP mime.magic once it's out
-        $mimetype = $mimetypes[$fileDetail["fil_extension"]];
-
-        // Apache behaviour seems to send text/plain for unknown mimetypes so that's what we do, too
-        if ($mimetype == "") {
-            $mimetype = "text/plain";
+        $mimeTypeGuesser = new FileinfoMimeTypeGuesser();
+        if ($mimeTypeGuesser->isSupported()) {
+            $mimeType = $mimeTypeGuesser->guess($path);
+        } else {
+            $mimeType = "text/plain";
         }
 
         // eval 'mode' parameter for either view or download
-        if ($mode == "download") {
+        if ($_GET["mode"] == "download") {
             header("Content-Length: " . filesize($path));
-            header("Content-Type: $mimetype");
-            header("Content-Disposition: attachment; filename=$name");
-        } elseif ($mode == "view") {
+            header("Content-Type: $mimeType");
+            header("Content-Disposition: attachment; filename=$filename");
+        } elseif ($_GET["mode"] == "view") {
             header("Content-Length: " . filesize($path));
-            header("Content-Type: $mimetype");
-            header("Content-Disposition: inline; filename=$name");
+            header("Content-Type: $mimeType");
+            header("Content-Disposition: inline; filename=$filename");
             // Apache is sending Last Modified header, so we'll do it, too
             $modified = filemtime($path);
             header("Last-Modified: " . date("D, j M Y G:i:s T", $modified));    // something like Thu, 03 Oct 2002 18:01:08 GMT
@@ -49,4 +51,3 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-
--- linkedcontent/accessfile.php
+++ linkedcontent/accessfile.php
@@ -2,16 +2,16 @@
 #Application name: PhpCollab
 #Status page: 0
 // no caching to keep phpCollab 2.0 behaviour
+use phpCollab\Files\Files;
+
 session_cache_limiter('none');		// suppress error messages for PHP version < 4.0.2
 error_reporting(0);
-
-$id = $_GET["id"];
 
 $checkSession = "true";
 include '../includes/library.php';		// starts session and writes session cache headers
 
-$files = new \phpCollab\Files\Files();
-$fileDetail = $files->getFileById($id);
+$files = new Files();
+$fileDetail = $files->getFileById($_GET["id"]);
 
 // serve the requested file
 include '../includes/download.php';
