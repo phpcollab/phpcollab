325904b5e6fe3ca733702589a74ebcb238d67810
Jeff Sittler <mindblender@gmail.com>
2020-04-15 12:31:45 -0700

    test(Tests): Updated Calendar and Newsdesk tests
    Expanded the calendar tests to cover month view, day view, add event,
    edit event and delete event.
    Expanded Newsdesk to include add comment, and edit comment

--- tests/acceptance/loggedIn/CalendarCest.php
+++ tests/acceptance/loggedIn/CalendarCest.php
@@ -1,9 +1,27 @@
 <?php
+
 namespace loggedIn;
+
 use \AcceptanceTester;
+use Codeception\Util\Locator;
+use DateTime;
+use Exception;
 
 class CalendarCest
 {
+    protected $eventName;
+    protected $eventId;
+    protected $today;
+
+    public function __construct()
+    {
+        $this->eventName = "Codeception Event Test";
+        $this->today = new DateTime();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/general/login.php');
@@ -12,21 +30,107 @@ class CalendarCest
         $I->click('input[type="submit"]');
     }
 
-
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _after(AcceptanceTester $I)
     {
     }
 
-    // tests
-    public function listCalendar(AcceptanceTester $I)
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function viewMonthCalendar(AcceptanceTester $I)
     {
-        $I->wantTo('See a Calendar');
+        $I->wantTo('See a Calendar for ' . $this->today->format('F'));
         $I->amOnPage('/calendar/viewcalendar.php');
         $I->seeInTitle('View Calendar');
+        $I->see($this->today->format('F Y'), ['css' => 'h1.heading']);
         try {
             $I->seeElement('.listing');
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $I->seeElement('.noItemsFound');
         }
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function viewDayCalendar(AcceptanceTester $I)
+    {
+        $I->wantTo('See a Calendar for ' . $this->today->format('l d F Y'));
+        $I->amOnPage('/calendar/viewcalendar.php?dateCalend=' . $this->today->format('Y-m-d') . '&type=dayList');
+        $I->seeInTitle('View Calendar');
+        $I->see($this->today->format('l d F Y'), ['css' => 'h1.heading']);
+        try {
+            $I->seeElement('.listing');
+        } catch (Exception $e) {
+            $I->seeElement('.noItemsFound');
+        }
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function addCalendarEvent(AcceptanceTester $I)
+    {
+        $I->wantTo("Add calendar event");
+        $I->amOnPage('/calendar/viewcalendar.php?type=calendEdit&dateCalend=' . $this->today->format('Y-m-d'));
+        $I->submitForm('form', [
+            'shortname' => $this->eventName,
+            'subject' => 'My test event subject',
+            'description' => 'My test event description'
+        ]);
+        $I->see('Success : Addition succeeded');
+        $this->eventId = $I->grabFromCurrentUrl('~id=(\d+)~');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function editCalendarEvent(AcceptanceTester $I)
+    {
+        $I->wantTo("Edit calendar event");
+        $I->amOnPage('/calendar/viewcalendar.php?type=dayList&dateCalend=' . $this->today->format('Y-m-d'));
+        $I->seeElement('.listing');
+        $I->see($this->eventName);
+        $I->click(Locator::contains('a', $this->eventName));
+        $I->seeElement('.content');
+        $I->see('Subject :');
+        $I->see('Description :');
+        $I->see('Short name');
+        $I->amOnPage('/calendar/viewcalendar.php?type=calendEdit&dateCalend=' . $this->today->format('Y-m-d') . '&id=' . $this->eventId);
+        $I->see('Edit: ' . $this->eventName);
+        $I->submitForm('form', [
+            'shortname' => $this->eventName . ' - edited',
+            'subject' => 'My test event subject - edited',
+            'description' => 'My test event description - edited'
+        ]);
+        $I->see('Success : Modification succeeded');
+        $I->amOnPage('/calendar/viewcalendar.php?dateCalend=' . $this->today->format('Y-m-d') . '&type=calendDetail&msg=update&id=' . $this->eventId);
+        $I->see($this->eventName . " - edited");
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function deleteCalendarEvent(AcceptanceTester $I)
+    {
+        $I->wantTo('Delete calendar event');
+        $I->amOnPage('/calendar/viewcalendar.php?dateCalend=' . $this->today->format('Y-m-d') . '&type=dayList');
+        $I->seeElement('.listing');
+        $I->see($this->eventName);
+        $I->click(Locator::contains('a', $this->eventName));
+        $I->seeElement('.content');
+        $I->see('Subject :');
+        $I->see('Description :');
+        $I->see('Short name');
+        $I->amOnPage('/calendar/deletecalendar.php?id=' . $this->eventId);
+        $I->see("Delete Calendar");
+        $I->seeElement('.content');
+        $I->see('#' . $this->eventId);
+        $I->see($this->eventName);
+        $I->click('Delete');
+        $I->see('Success : Deletion succeeded');
     }
 }
--- tests/acceptance/loggedIn/NewsdeskCest.php
+++ tests/acceptance/loggedIn/NewsdeskCest.php
@@ -2,8 +2,20 @@
 namespace loggedIn;
 use \AcceptanceTester;
 
+/**
+ * Class NewsdeskCest
+ * @package loggedIn
+ *
+ * Tests performed as a general user
+ */
 class NewsdeskCest
 {
+    private $postId;
+    private $commentId;
+
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/general/login.php');
@@ -12,11 +24,16 @@ class NewsdeskCest
         $I->click('input[type="submit"]');
     }
 
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _after(AcceptanceTester $I)
     {
     }
 
-    // tests
+    /**
+     * @param AcceptanceTester $I
+     */
     public function listPosts(AcceptanceTester $I)
     {
         $I->wantTo('See a list of Newsdesk posts');
@@ -24,6 +41,9 @@ class NewsdeskCest
         $I->see('News list');
     }
 
+    /**
+     * @param AcceptanceTester $I
+     */
     public function viewPost(AcceptanceTester $I)
     {
         $I->wantTo('View a newsdesk post');
@@ -32,5 +52,44 @@ class NewsdeskCest
         $I->click('.listing tr:nth-child(2) td:nth-child(2) a');
         $I->see('Details');
         $I->see('Comments');
+        $this->postId = $I->grabFromCurrentUrl('~id=(\d+)~');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function addComment(AcceptanceTester $I)
+    {
+        $I->wantTo('Add a comment to a news post');
+        $I->amOnPage('/newsdesk/viewnews.php?id=' . $this->postId);
+        $I->see('Newsdesk');
+        $I->see('Comments');
+        $I->amOnPage('/newsdesk/editmessage.php?postid=' . $this->postId);
+        $I->see('Add a comment to the News Article');
+        $I->submitForm('form', [
+            'comment' => 'Codeception comment'
+        ]);
+        $I->see('Success : Addition succeeded', ['css' => '.message']);
+        $I->see('Codeception comment', ['css' => '#clPrc']);
+        $this->commentId = preg_replace("/[^0-9s]/", "", $I->grabAttributeFrom('#clPrc table.listing tr:last-child img', 'name'));
+
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function editComment(AcceptanceTester $I)
+    {
+        $I->wantTo('Edit my news post comment');
+        $I->amOnPage('/newsdesk/viewnews.php?id=' . $this->postId);
+        $I->see('Codeception comment', ['css' => '#clPrc']);
+        $I->amGoingTo('Navigate to the edit view');
+        $I->amOnPage('/newsdesk/editmessage.php?postid=' . $this->postId . '&id=' . $this->commentId);
+        $I->see('Edit the comment of the News Article :');
+        $I->submitForm('form', [
+            'comment' => 'Codeception comment - edited'
+        ]);
+        $I->see('Success : Modification succeeded');
+        $I->see('Codeception comment - edited', ['css' => '#clPrc']);
     }
 }
