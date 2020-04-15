04b73fdf0da56d784aea49c60129f546e738c6e9
Jeff Sittler <mindblender@gmail.com>
2020-04-15 14:56:21 -0700

    test(Clients): Updated ClientsCest
    Added viewClient test

--- tests/acceptance/loggedIn/ClientsCest.php
+++ tests/acceptance/loggedIn/ClientsCest.php
@@ -1,9 +1,13 @@
 <?php
 namespace loggedIn;
 use \AcceptanceTester;
+use Exception;
 
 class ClientsCest
 {
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/general/login.php');
@@ -13,11 +17,16 @@ class ClientsCest
     }
 
 
+    /**
+     * @param AcceptanceTester $I
+     */
     public function _after(AcceptanceTester $I)
     {
     }
 
-    // tests
+    /**
+     * @param AcceptanceTester $I
+     */
     public function listClients(AcceptanceTester $I)
     {
         $I->wantTo('See a list of Clients');
@@ -25,8 +34,22 @@ class ClientsCest
         $I->seeInTitle('List Clients');
         try {
             $I->seeElement('.listing');
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $I->seeElement('.noItemsFound');
         }
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function viewClient(AcceptanceTester $I)
+    {
+        $I->wantTo('View details about a client');
+        $I->amOnPage('/clients/listclients.php');
+        $I->seeInTitle('List Clients');
+        $I->amGoingTo('select the first client in the list and navigate to it');
+        $I->dontSeeElement('.noItemsFound');
+        $I->click('.listing tr:nth-child(2) td:nth-child(2) a');
+        $I->seeElement('.content');
     }
 }
