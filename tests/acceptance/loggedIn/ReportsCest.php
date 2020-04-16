eecab62b717a9bd4f1be9d95c97b453b8d41cef7
Jeff Sittler <mindblender@gmail.com>
2020-04-15 22:30:00 -0700

    test(Reports): Added additional tests for Reports
    Created:
    noReports
    createReport
    saveReport
    listReports
    viewSavedReport
    deleteReport

--- tests/acceptance/loggedIn/ReportsCest.php
+++ tests/acceptance/loggedIn/ReportsCest.php
@@ -4,6 +4,10 @@ use \AcceptanceTester;
 
 class ReportsCest
 {
+
+    private $reportName = 'Codeception Report';
+    private $reportId;
+
     public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/general/login.php');
@@ -12,21 +16,111 @@ class ReportsCest
         $I->click('input[type="submit"]');
     }
 
-
     public function _after(AcceptanceTester $I)
     {
     }
 
-    // tests
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function noReports(AcceptanceTester $I)
+    {
+        $I->wantTo('See no Reports listed');
+        $I->amOnPage('/reports/listreports.php');
+        $I->seeInTitle('My Reports');
+        $I->seeElement('.noItemsFound');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function createReport(AcceptanceTester $I)
+    {
+        $I->wantTo('Create a Report');
+        $I->amOnPage('/reports/createreport.php');
+        $I->see('Create Report', ['css' => 'h1.heading']);
+        $I->seeElement('form', ['name' => 'customsearchForm']);
+        $I->click('input[type="submit"]');
+        $I->amOnPage('/reports/resultsreport.php');
+        $I->seeInTitle('Report Results');
+        $I->see('Report Results', ['css' => 'h1.heading']);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function saveReport(AcceptanceTester $I)
+    {
+        $I->wantTo('Save a Report');
+        $I->amOnPage('/reports/createreport.php');
+        $I->see('Create Report', ['css' => 'h1.heading']);
+        $I->seeElement('form', ['name' => 'customsearchForm']);
+        $I->submitForm('form[name=customsearchForm]', [
+            'Save'
+        ]);
+
+        $I->amOnPage('/reports/resultsreport.php');
+        $I->seeInTitle('Report Results');
+        $I->see('Report Results', ['css' => 'h1.heading']);
+
+        $I->amGoingTo('enter a report name and save');
+        $I->submitForm('form[name=save_reportForm]', [
+            'report_name' => $this->reportName,
+            'filterOrganization' => 'ALL',
+            'filterProject' => 'ALL',
+            'filterAssignedTo' => 'ALL',
+            'filterStatus' => 'ALL',
+            'filterPriority' => 'ALL',
+            'Save'
+        ]);
+
+        $I->see('Success : Created report');
+        $I->see($this->reportName, ['css' => 'table.listing']);
+        $I->seeInCurrentUrl('/reports/listreports.php?msg=addReport');
+
+        // Since there should only be one report, grab its ID from the row checkbox so we can use it for the delete report test
+        $this->reportId = preg_replace("/[^0-9s]/", "", $I->grabAttributeFrom('table.listing tr:last-child img', 'name'));
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
     public function listReports(AcceptanceTester $I)
     {
         $I->wantTo('See a list of Reports');
         $I->amOnPage('/reports/listreports.php');
         $I->seeInTitle('My Reports');
-        try {
-            $I->seeElement('.listing');
-        } catch (\Exception $e) {
-            $I->seeElement('.noItemsFound');
-        }
+        $I->seeElement('.listing');
+        $I->see($this->reportName, ['css' => 'table.listing a']);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function viewSavedReport(AcceptanceTester $I)
+    {
+        $I->wantTo('View a saved Report');
+        $I->amOnPage('/reports/listreports.php');
+        $I->seeInTitle('My Reports');
+        $I->seeElement('.listing');
+        $I->see($this->reportName, ['css' => 'table.listing a']);
+        $I->click($this->reportName);
+        $I->seeInCurrentUrl('/reports/resultsreport.php?id=' . $this->reportId);
+        $I->see($this->reportName, ['css' => '.breadcrumbs']);
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function deleteReport(AcceptanceTester $I)
+    {
+        $I->wantTo('Delete a report');
+        $I->amOnPage('/reports/deletereports.php?id=' . $this->reportId);
+        $I->seeInTitle('Delete Report');
+        $I->see('#' . $this->reportId);
+        $I->see('Codeception Report', ['css' => '.content td']);
+        $I->click('input[type="submit"]');
+        $I->see('Success : Deleted reports');
+        $I->seeElement('.noItemsFound');
     }
 }
