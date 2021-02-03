44d76832f29d9300ae4a582a8b14ca3f8b3bf456
Jeff Sittler <mindblender@gmail.com>
2021-02-03 15:26:38 -0700

    refactor(Login): Moved langSelected to HEREDOC
    Changed the code from individual lines to a HEREDOC format for the
    language selection

--- general/login.php
+++ general/login.php
@@ -273,43 +273,44 @@ $block1->heading($setTitle . " : " . $strings["login"]);
 $block1->openContent();
 $block1->contentTitle($strings["please_login"]);
 
-$selectLanguage = "<select name='languageForm'>";
-$selectLanguage .= "<option value='$langDefault'>Default (" . $langValue["$langDefault"] . ")</option>";
-$selectLanguage .= "
-	<option value='ar'>Arabic</option>
-	<option value='az'>Azerbaijani</option>
-	<option value='pt-br'>Brazilian Portuguese</option>
-	<option value='bg'>Bulgarian</option>
-	<option value='ca'>Catalan</option>
-	<option value='zh'>Chinese simplified</option>
-	<option value='zh-tw'>Chinese traditional</option>
-	<option value='cs-iso'>Czech (iso)</option>
-	<option value='cs-win1250'>Czech (win1250)</option>
-	<option value='da'>Danish</option>
-	<option value='nl'>Dutch</option>
-	<option value='en'>English</option>
-	<option value='et'>Estonian</option>
-	<option value='fr'>French</option>
-	<option value='de'>German</option>
-	<option value='hu'>Hungarian</option>
-	<option value='is'>Icelandic</option>
-	<option value='in'>Indonesian</option>
-	<option value='it'>Italian</option>
-	<option value='ko'>Korean</option>
-	<option value='lv'>Latvian</option>
-	<option value='ja'>Japanese</option>
-	<option value='no'>Norwegian</option>
-	<option value='pl'>Polish</option>
-	<option value='pt'>Portuguese</option>
-	<option value='ro'>Romanian</option>
-	<option value='ru'>Russian</option>
-	<option value='sk-win1250'>Slovak (win1250)</option>
-	<option value='es'>Spanish</option>
-	<option value='tr'>Turkish</option>
-	<option value='uk'>Ukrainian</option>
-";
 
-$selectLanguage .= "</select>";
+$selectLanguage = <<<SELECT_LANG
+<select name='languageForm'>";
+    <option value="{$langDefault}">{$langValue["$langDefault"]} (Default)</option>
+	<option value="ar">Arabic</option>
+	<option value="az">Azerbaijani</option>
+	<option value="pt-br">Brazilian Portuguese</option>
+	<option value="bg">Bulgarian</option>
+	<option value="ca">Catalan</option>
+	<option value="zh">Chinese simplified</option>
+	<option value="zh-tw">Chinese traditional</option>
+	<option value="cs-iso">Czech (iso)</option>
+	<option value="cs-win1250">Czech (win1250)</option>
+	<option value="da">Danish</option>
+	<option value="nl">Dutch</option>
+	<option value="en">English</option>
+	<option value="et">Estonian</option>
+	<option value="fr">French</option>
+	<option value="de">German</option>
+	<option value="hu">Hungarian</option>
+	<option value="is">Icelandic</option>
+	<option value="in">Indonesian</option>
+	<option value="it">Italian</option>
+	<option value="ko">Korean</option>
+	<option value="lv">Latvian</option>
+	<option value="ja">Japanese</option>
+	<option value="no">Norwegian</option>
+	<option value="pl">Polish</option>
+	<option value="pt">Portuguese</option>
+	<option value="ro">Romanian</option>
+	<option value="ru">Russian</option>
+	<option value="sk-win1250">Slovak (win1250)</option>
+	<option value="es">Spanish</option>
+	<option value="tr">Turkish</option>
+	<option value="uk">Ukrainian</option>
+</select>
+SELECT_LANG;
+
 
 $block1->contentRow($strings["language"], $selectLanguage);
 
