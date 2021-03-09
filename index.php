c5b1b4614fb9d8d9736c8f5bb7a77b1ed3c68f5c
Jeff Sittler <mindblender@gmail.com>
2021-03-09 14:01:14 -0700

    feat(Error): Added Fatal Error view

--- index.php
+++ index.php
@@ -3,22 +3,29 @@
 #Status page: 0
 #Path by root: index.php
 
+
 /**
  * Do Setup Check
  */
-if (!file_exists("includes/settings.php")) {
-    header('Location: installation/setup.php');
-} else {
-    $checkSession = "false";
-    $indexRedirect = "true";
+try {
+    if (!file_exists("includes/settings.php")) {
+        header('Location: installation/setup.php');
+    } else {
+        $checkSession = "false";
+        $indexRedirect = "true";
 
-    include_once('includes/library.php');
+        include_once('includes/library.php');
 
-    //case session fails
-    if ($session == "false") {
-        phpCollab\Util::headerFunction("general/login.php?session=false");
-    } //default case
-    else {
-        phpCollab\Util::headerFunction("general/login.php");
+        //case session fails
+        if ($session == "false") {
+            phpCollab\Util::headerFunction("general/login.php?session=false");
+        } //default case
+        else {
+            phpCollab\Util::headerFunction("general/login.php");
+        }
     }
+} catch (Exception $exception) {
+    require_once dirname(__FILE__) . "/views/fatal_error.php";
+    error_log("FATAL ERROR: " . $exception . "\n");
+    error_log("FATAL ERROR: " . $exception . "\n", 3, "./logs/phpcollab.log");
 }
---
+++ views/fatal_error.php
@@ -0,0 +1,30 @@
+<?php
+
+echo <<<EXCEPTION
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>phpCollab Error</title>
+    <meta name="robots" content="none" />
+    
+    <link rel="icon" type="image/png" sizes="32x32" href="../public/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../public/images/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../public/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="../public/site.webmanifest">
+    
+    <script type="text/javascript" src="../javascript/general.js"></script>
+    <script type="text/JavaScript" src="../javascript/overlib_mini.js"></script>
+    <link rel="stylesheet" href="../public/css/all.min.css">
+    <link rel="stylesheet" href="../public/css/error_page.css">
+</head>
+<body id="error_page">
+<h2>Oooof!</h2>
+<p>phpCollab appears to have fallen down.</p>
+<p>Please check the log files to find out what happened.</p>
+</div>
+</body>
+
+EXCEPTION;
