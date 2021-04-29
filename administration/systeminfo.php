a0ad936822d79bbfcf76539dfa9b752a2f11865f
Jeff Sittler <mindblender@gmail.com>
2021-04-28 20:24:05 -0700

    refactor(Administration): Changed flags from 1/0 to On/Off

--- administration/systeminfo.php
+++ administration/systeminfo.php
@@ -38,7 +38,7 @@ if ($session->get("connectedUsers")) {
 }
 
 $block1->contentRow("File Management",
-    $fileManagement . " (default max file size $maxFileSize $byteUnits[0]) " . Util::convertSize($maxFileSize));
+    ['Off', 'On'][$fileManagement] . " (default max file size $maxFileSize) " . Util::convertSize($maxFileSize));
 
 if ($mkdirMethod == "FTP") {
     $mkdirMethodMore = " (Path to root with mentioned account: \"$ftpRoot\")";
@@ -46,8 +46,8 @@ if ($mkdirMethod == "FTP") {
 
 $block1->contentRow("Create folder method", $mkdirMethod . $mkdirMethodMore);
 $block1->contentRow("Theme", THEME);
-$block1->contentRow("Product Site Publish", $sitePublish);
-$block1->contentRow("Notifications", $notifications);
+$block1->contentRow("Product Site Publish", ['Off', 'On'][$sitePublish]);
+$block1->contentRow("Notifications", ['Off', 'On'][$notifications]);
 $block1->contentRow("Root", $root);
 
 if ($useLDAP == "true") {
@@ -98,32 +98,35 @@ if ($include_path == "") {
 $block1->contentRow("include_path", $include_result);
 
 $register_globals = ini_get('register_globals');
-if ($register_globals == "1") {
-    $register_result = "On";
-} else {
-    $register_result = "Off";
-}
+//if ($register_globals == "1") {
+//    $register_result = "On";
+//} else {
+//    $register_result = "Off";
+//}
 
-$block1->contentRow("register_globals", $register_result);
+//$block1->contentRow("register_globals", $register_result);
+$block1->contentRow("register_globals", ['Off', 'On'][$register_globals]);
 
 $safemodeTest = ini_get('safe_mode');
-if ($safemodeTest == "1") {
-    $safe_mode_result = "On";
-} else {
-    $safe_mode_result = "Off";
-}
+//if ($safemodeTest == "1") {
+//    $safe_mode_result = "On";
+//} else {
+//    $safe_mode_result = "Off";
+//}
 
-$block1->contentRow("safe_mode", $safe_mode_result);
+$block1->contentRow("safe_mode", ['Off', 'On'][$safemodeTest]);
+//$block1->contentRow("safe_mode", $safe_mode_result);
 
-$notificationsTest = function_exists('mail');
+$mailTest = function_exists('mail');
 
-if ($notificationsTest == "true") {
-    $mail_result = "On";
-} else {
-    $mail_result = "Off";
-}
+//if ($notificationsTest == "true") {
+//    $mail_result = "On";
+//} else {
+//    $mail_result = "Off";
+//}
 
-$block1->contentRow("Mail", $mail_result);
+$block1->contentRow("Mail", ['Off', 'On'][$mailTest]);
+//$block1->contentRow("Mail", $mail_result);
 
 $gdlibraryTest = function_exists('imagecreate');
 
@@ -149,13 +152,21 @@ if ($matches[1] != "") {
 $block1->contentRow("SMTP", ini_get('SMTP'));
 $block1->contentRow("upload_max_filesize", ini_get('upload_max_filesize'));
 $block1->contentRow("session.name", session_name());
-$block1->contentRow("session.save_path", session_save_path());
+//xdebug_var_dump( session_save_path() );
+if (!empty(session_save_path())) {
+    $block1->contentRow("session.save_path", session_save_path());
+}
 $block1->contentRow("HTTP_HOST", $request->server->get('HTTP_HOST'));
 
 $block1->contentRow("SERVER_NAME", $request->server->get('SERVER_NAME'));
 $block1->contentRow("SERVER_PORT", $request->server->get('SERVER_PORT'));
 $block1->contentRow("SERVER_SOFTWARE", $request->server->get('SERVER_SOFTWARE'));
 $block1->contentRow("SERVER_OS", PHP_OS);
+
+$block1->contentTitle($strings["system_info_file_sizes"]);
+$block1->contentRow("Post Max Size", ini_get('post_max_size'));
+$block1->contentRow("Upload Max Size", ini_get('upload_max_filesize'));
+
 
 $block1->closeContent();
 
